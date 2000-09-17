140feacade4450bf9e52345e9b34135d7af4c3cf
René Fonseca <rene.m.fonseca@gmail.com>
2000-09-17 22:19:28 +0000

    Added IOException.cpp

---
+++ base/IOException.cpp
@@ -0,0 +1,12 @@
+/***************************************************************************
+    copyright   : (C) 2000 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#include "Exception.h"
+
+IOException::IOException() {
+}
+
+IOException::~IOException() {
+}

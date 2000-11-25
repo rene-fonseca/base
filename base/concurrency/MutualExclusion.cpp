796a1d128c5a16ce928022894fa856e4ba3cdef9
René Fonseca <rene.m.fonseca@gmail.com>
2000-11-25 16:43:38 +0000

    Added WIN32 support to MutualExclusion.

--- base/concurrency/MutualExclusion.cpp
+++ base/concurrency/MutualExclusion.cpp
@@ -4,52 +4,94 @@
  ***************************************************************************/
 
 #include "MutualExclusion.h"
-#include <errno.h>
 
-MutualExclusion::MutualExclusion() throw(Construct) {
-#ifdef __cygwin__
-  if (pthread_mutex_init(&mutex, NULL) != 0) {
-    throw Construct();
+#ifndef __win32__
+  #include <errno.h>
+#endif // __win32__
+
+MutualExclusion::MutualExclusion() throw(ResourceException) {
+#ifdef __win32__
+  __try {
+    InitializeCriticalSection(&criticalSection);
+  } __except(STATUS_NO_MEMORY) {
+    throw ResourceException("Unable to initialize MutualExclusion");
   }
-#else
+#else // __win32__
   pthread_mutexattr_t attributes;
   if (pthread_mutexattr_init(&attributes) != 0) {
-    throw Construct();
+    throw ResourceException("Unable to initialize MutualExclusion");
   }
   if (pthread_mutexattr_settype(&attributes, PTHREAD_MUTEX_ERRORCHECK) != 0) {
     pthread_mutexattr_destroy(&attributes); // should never fail
-    throw Construct();
+    throw ResourceException("Unable to initialize MutualExclusion");
   }
   if (pthread_mutex_init(&mutex, &attributes) != 0) {
     pthread_mutexattr_destroy(&attributes); // should never fail
-    throw Construct();
+    throw ResourceException("Unable to initialize MutualExclusion");
   }
   pthread_mutexattr_destroy(&attributes); // should never fail
+#endif // __win32__
 }
 
-void MutualExclusion::lock() const throw(MutualExclusion::Lock) {
-  if (pthread_mutex_lock(&mutex) != 0) {
-    throw MutualExclusion::Lock();
+void MutualExclusion::lock() const throw(Exception) {
+#ifdef __win32__
+  __try {
+    EnterCriticalSection(&criticalSection);
+  } __except(STATUS_INVALID_HANDLE) {
+    throw Exception("Unable to acquire lock on MutualExclusion");
   }
+#else
+  int result pthread_mutex_lock(&mutex);
+  if (result == 0) {
+    return;
+  } else if (result == EDEADLK) {
+    return;
+  } else {
+    throw Exception("Unable to acquire lock on MutualExclusion");
+  }
+#endif // __win32__
 }
 
-bool MutualExclusion::tryLock() const throw(MutualExclusion::Lock) {
+bool MutualExclusion::tryLock() const throw(Exception) {
+#ifdef __win32__
+  BOOL result;
+  __try {
+    result = TryEnterCriticalSection(&criticalSection);
+  } __except(STATUS_INVALID_HANDLE) {
+    throw Exception("Unable to attempt to acquire lock on MutualExclusion");
+  }
+  return result;
+#else
   int result = pthread_mutex_trylock(&mutex);
   if (result == 0) {
     return true;
   } else if (result == EBUSY) {
     return false;
   } else {
-    throw MutualExclusion::Lock();
+    throw Exception("Unable to attempt to acquire lock on MutualExclusion");
   }
+#endif // __win32__
 }
 
-void MutualExclusion::unlock() const throw(MutualExclusion::Unlock) {
-  if (pthread_mutex_unlock(&mutex) != 0) {
-    throw MutualExclusion::Unlock();
+void MutualExclusion::unlock() const throw(Exception) {
+#ifdef __win32__
+  LeaveCriticalSection(&criticalSection);
+#else
+  int result = pthread_mutex_unlock(&mutex);
+  if (result == 0) {
+    return;
+  } else if (result == EPERM) {
+    throw Exception("Unable to unlock MutualExclusion");
+  } else {
+    throw Exception("Unable to unlock MutualExclusion");
   }
+#endif // __win32__
 }
 
 MutualExclusion::~MutualExclusion() throw() {
+#ifdef __win32__
+  DeleteCriticalSection(&criticalSection);
+#else
   pthread_mutex_destroy(&mutex); // lets just hope that this doesn't fail
+#endif // __win32__
 }
--- base/concurrency/MutualExclusion.h
+++ base/concurrency/MutualExclusion.h
@@ -9,63 +9,68 @@
 #include <config.h>
 #include "base/Object.h"
 #include "base/Exception.h"
-#include "base/Construct.h"
-#include <pthread.h>
+#include "base/ResourceException.h"
+
+#ifdef __win32__
+  #include <Windows.h>
+#else
+  #include <pthread.h>
+#endif // __win32__
 
 /**
-  This class implements a mutual exclusion semaphore used to protect shared
+  This class implements a mutual exclusion object used to protect shared
   data structures from concurrent modifications, implementing critical sections
-  and monitors. Please note that the lock/unlock mechanism is considered const.
+  and monitors. Please note that the lock/unlock mechanism is considered a
+  non-modifying property of a class.
 
   @author René Møller Fonseca
-  @version 1.0
+  @version 1.1
 */
 
 class MutualExclusion : public Object {
 protected:
 
- /** Internal mutex representation. */
- mutable pthread_mutex_t mutex;
+#ifdef __win32__
+  /** Internal mutex representation. */
+  mutable CRITICAL_SECTION criticalSection;
+#else
+  /** Internal mutex representation. */
+  mutable pthread_mutex_t mutex;
+#endif // __win32__
 public:
 
-  /** Group of exceptions thrown directly by the MutualExclusion class. */
-  class MutualExclusionException : public Exception {
-  };
-  /** Thrown on failure to lock the MutualExclusion object. */
-  class Lock : public MutualExclusionException {
-  };
-  /** Thrown on failure to unlock the MutualExclusion object. */
-  class Unlock : public MutualExclusionException {
-  };
-
   /**
-    Initializes the mutex object in unlocked state.
+    Initializes a mutual exclusion object in the unlocked state. Throws
+    'ResourceException' if unable to initialize the object.
   */
-  MutualExclusion() throw(Construct);
+  MutualExclusion() throw(ResourceException);
 
   /**
-    Requests the mutex. The calling thread is suspended if the mutex has been
-    locked by another thread. Throws the exception
-    'MutualExclusionException:Lock' on failure.
+    Requests a lock on this mutual exclusion object. The calling thread is
+    suspended if the mutex has been locked by another thread. Does nothing if
+    the calling thread already has a lock on the object. Throws 'Exception'
+    on undefined failure.
   */
-  void lock() const throw(Lock);
+  void lock() const throw(Exception);
 
   /**
-    Tries to lock the mutex. Throws the exception
-    'MutualExclusionException:Lock' on failure.
+    Tries to lock this mutual exclusion object. Throws 'Exception' on
+    undefined failure.
 
-    @return True if the mutex was locked successfully.
+    @return True if the mutual exclusion was locked successfully or already was locked by the calling thread.
   */
-  bool tryLock() const throw(Lock);
+  bool tryLock() const throw(Exception);
 
   /**
-    This method unlocks the mutex. Throws the exception
-    'MutualExclusionException:Unlock' on failure.
+    This method unlocks this mutual exclusion object. Throws 'Exception' on
+    undefined failure. The calling thread must have a lock on this mutual
+    exlusion object prior to invocation otherwise the behaviour is undefined.
   */
-  void unlock() const throw(Unlock);
+  void unlock() const throw(Exception);
 
   /**
-    Destroys the mutex object.
+    Destroys the mutual exclusion object. The mutual exclusion must be
+    in the unlocked state prior to destruction.
   */
   ~MutualExclusion() throw();
 };

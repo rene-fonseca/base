d279b285f703ad227dd2a8996960ce26c002f50e
René Fonseca <rene.m.fonseca@gmail.com>
2001-03-01 20:51:00 +0000

    win32 bug fix.

--- base/mem/Allocator.cpp
+++ base/mem/Allocator.cpp
@@ -3,6 +3,7 @@
     email       : fonseca@mip.sdu.dk
  ***************************************************************************/
 
+#include <config.h>
 #include <base/mem/Allocator.h>
 
 #if defined(__win32__)
@@ -13,14 +14,27 @@
 
 void* AllocatorImpl::setSize(void* buffer, unsigned int size) throw(MemoryException) {
   // Allocator::setSize() checks if we really have to set the size
+  void* result;
 #if defined(__win32__)
-  // use HeapCreate, HeapReAlloc, and HeapDestroy with serialization enabled
-  void* result = (void*)LocalReAlloc((HLOCAL)buffer, size, 0);
+  // is serialization enabled for the heap object returned by GetProcessHeap
+  if (buffer) {
+    if (size) {
+      result = (void*)HeapReAlloc(GetProcessHeap(), 0, buffer, size);
+    } else {
+      if (!HeapFree(GetProcessHeap(), 0, buffer)) {
+        throw MemoryException("Unable to release memory");
+      }
+      result = 0;
+    }
+  } else {
+    result = (void*)HeapAlloc(GetProcessHeap(), 0, size);
+  }
+  //  result = (void*)LocalReAlloc((HLOCAL)buffer, size, 0);
 #else // __unix__
-  void* result = realloc(buffer, size);
+  result = realloc(buffer, size);
 #endif
   if ((!result) && (size != 0)) { // was memory allocated
-    throw MemoryException();
+    throw MemoryException("Unable to adjust allocated memory");
   }
   return result;
 }

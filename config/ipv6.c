9b224489a32e70f28f5c4e1602a0de425a5049a1
René Fonseca <rene.m.fonseca@gmail.com>
2001-03-08 20:02:27 +0000

    Added strict requirements for IPv6 support.

--- config/ipv6.c
+++ config/ipv6.c
@@ -14,8 +14,16 @@ int main() {
   struct sockaddr_in6 sa;
   struct addrinfo hint;
   struct addrinfo* ai;
+  char buffer[INET6_ADDRSTRLEN];
+  int handle;
+
   getaddrinfo(NULL, NULL, &hint, &ai);
   freeaddrinfo(NULL);
-  inet_pton(AF_INET6, NULL, NULL);
-  inet_ntop(AF_INET6, NULL, NULL, 0);
+  inet_pton(AF_INET6, "::", &ia);
+  inet_ntop(AF_INET6, &ia, buffer, sizeof(buffer));
+  handle = socket(PF_INET6, SOCK_STREAM, 0);
+  if (handle == -1) {
+    return 1;
+  }
+  return 0;
 }

3f97e888ca1cc2f4a7fbef1710c7986b3269f661
René Fonseca <rene.m.fonseca@gmail.com>
2001-04-01 16:31:58 +0000

    Added ThreadPool class.

---
+++ base/concurrency/ThreadPool.cpp
@@ -0,0 +1,152 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#include <base/features.h>
+#include <base/concurrency/ThreadPool.h>
+#include <base/collection/Functor.h>
+#include <base/Trace.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+//class QueuedJobProvider : public JobProvider, public Queue<Job*> {
+//public:
+//
+//  QueueJobProvider() {}
+//};
+
+void ThreadPool::Wrapper::run() throw() {
+  pool->run();
+}
+
+
+
+void ThreadPool::run() throw() {
+  Thread* context = Thread::getThread();
+  Runnable* job;
+
+  while (!context->isTerminated()) {
+    semaphore.wait(); // wait for job or termination event
+
+    threadLock.exclusiveLock(); // synchronize threads in the pool
+
+    if (context->isTerminated()) { // has thread already been terminated
+      threadLock.releaseLock();
+      break; // leave
+    }
+
+    if (terminateOne) { // should the thread be terminated
+      context->terminate();
+      terminatedThread = context;
+      termination.post(); // signal the thread that requested the termination
+      terminateOne = false;
+      threadLock.releaseLock();
+      break; // leave
+    }
+
+    if (provider->isEmpty()) { // is job available
+      threadLock.releaseLock();
+      continue; // try again
+    }
+
+    job = provider->pop();
+    threadLock.releaseLock();
+
+    job->run();
+
+//    threadLock.exclusiveLock();
+//    if (!terminatedOne && !terminate && context->isTerminated()) { // has thread been terminated by job
+//      // what should we do if the job terminates the thread
+//    }
+//    threadLock.releaseLock();
+  }
+}
+
+ThreadPool::ThreadPool(JobProvider* provider) throw() :
+  runnable(this), provider(provider), desiredThreads(0), terminated(false), terminateOne(false) {
+}
+
+ThreadPool::ThreadPool(JobProvider* provider, unsigned int threads) throw() :
+  runnable(this), provider(provider), desiredThreads(0), terminated(false), terminateOne(false) {
+  setThreads(threads);
+}
+
+unsigned int ThreadPool::getThreads() const throw() {
+  SynchronizeShared();
+  return desiredThreads;
+}
+
+void ThreadPool::setThreads(unsigned int value) throw(ThreadPoolException) {
+  SynchronizeExclusively();
+
+  if (terminated) {
+    ThreadPoolException("Thread pool has been terminated");
+  }
+
+  if (value != desiredThreads) {
+
+    if (value > desiredThreads) { // should we added thread to the pool
+
+      threadLock.exclusiveLock();
+      unsigned int count = value - desiredThreads; // number of threads to add
+      while (count--) {
+        Thread* thread = new Thread(&runnable);
+        pool.add(thread);
+        thread->start();
+      }
+      desiredThreads = value;
+      threadLock.releaseLock();
+
+    } else if (value < desiredThreads) { // should we remove threads
+      threadLock.exclusiveLock();
+
+      unsigned int count = desiredThreads - value; // number of threads to terminate
+      desiredThreads = value;
+
+      while (count--) { // threads are terminated one by one
+        terminateOne = true; // tell random thread to terminate
+        semaphore.post(); // wake one thread
+
+        threadLock.releaseLock();
+        termination.wait(); // wait for a thread to be terminated
+        threadLock.exclusiveLock();
+
+        terminatedThread->join(); // wait for thread to finalize
+        delete terminatedThread;
+        pool.remove(terminatedThread);
+      }
+
+      threadLock.releaseLock();
+    }
+  }
+}
+
+void ThreadPool::terminate() throw() {
+  SynchronizeExclusively();
+  terminated = true;
+  forEach(pool, invokeMember(&Thread::terminate));
+}
+
+void ThreadPool::join() throw() {
+  // threads should not be signaled here
+  SynchronizeExclusively();
+  forEach(pool, invokeMember(&Thread::join));
+}
+
+void ThreadPool::post() throw() {
+  semaphore.post();
+}
+
+ThreadPool::~ThreadPool() throw() {
+  TRACE_MEMBER();
+  terminate();
+  join();
+
+  Set<Thread*>::Enumeration enu(pool);
+  while (enu.hasNext()) {
+    delete *enu.next();
+  }
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/concurrency/ThreadPool.h
@@ -0,0 +1,159 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_CONCURRENCY__THREAD_POOL_H
+#define _DK_SDU_MIP__BASE_CONCURRENCY__THREAD_POOL_H
+
+#include <base/Object.h>
+#include <base/collection/Set.h>
+#include <base/concurrency/Thread.h>
+#include <base/concurrency/MutualExclusion.h>
+#include <base/concurrency/Semaphore.h>
+#include <base/concurrency/Synchronize.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+class JobProvider {
+public:
+
+  virtual bool isEmpty() const = 0;
+
+  virtual Runnable* pop() = 0;
+};
+
+
+
+/**
+  Pool of threads used to run 'm' jobs using 'n' threads. The implementation is
+  MT-safe.
+
+  @author René Møller Fonseca
+  @version 1.0
+*/
+
+class ThreadPool : public Synchronizeable<MutualExclusion> {
+public:
+
+  /**
+    Exception thrown directly by the ThreadPool class.
+  */
+  class ThreadPoolException : public Exception {
+  public:
+
+    ThreadPoolException() {}
+
+    ThreadPoolException(const char* message) : Exception(message) {}
+  };
+private:
+
+  /**
+    Wrapper runnable used by ThreadPool to run the jobs. All the threads of the
+    thread pool share one runnable object.
+
+    @author René Møller Fonseca
+    @version 1.0
+  */
+
+  class Wrapper : public Runnable {
+  private:
+
+    /** The thread pool. */
+    ThreadPool* pool;
+  public:
+
+    /**
+      Initialize the runnable with the specified thread pool.
+
+      @param pool The thread pool.
+    */
+    inline Wrapper(ThreadPool* pool) throw() : pool(pool) {}
+
+    /**
+      Run the object.
+    */
+    void run() throw();
+  };
+
+  friend Wrapper;
+  typedef MutualExclusion LOCK;
+
+  /** Runnable. */
+  Wrapper runnable;
+  /** Job provider. */
+  JobProvider* provider;
+  /** The number of desired threads. */
+  unsigned int desiredThreads;
+  /** Specifies that the thread pool has been terminated. */
+  bool terminated;
+  /** Specifies that a thread should be terminated. */
+  bool terminateOne;
+  /** The last terminated thread. */
+  Thread* terminatedThread;
+  /** Collection holding the thread objects. */
+  Set<Thread*> pool;
+  /** Synchronization object. */
+  MutualExclusion threadLock;
+  /** Synchronization object used to wake threads in the pool. */
+  Semaphore semaphore;
+  /** Synchronization object used to signal thread termination. */
+  Semaphore termination;
+private:
+
+  void run() throw();
+public:
+
+  /**
+    Initializes thread pool with no threads.
+
+    @param provider The job provider.
+  */
+  ThreadPool(JobProvider* provider) throw();
+
+  /**
+    Initializes thread pool with specified number of threads.
+
+    @param provider The job provider.
+    @param threads The initial number of threads in the pool.
+  */
+  ThreadPool(JobProvider* provider, unsigned int threads) throw();
+
+  /**
+    Returns the desired number of threads of the pool.
+  */
+  unsigned int getThreads() const throw();
+
+  /**
+    Sets the desired number of threads of the pool. Blocks until accomplished.
+    Throws 'ThreadPoolException' is the pool has been terminated.
+  */
+  void setThreads(unsigned int value) throw(ThreadPoolException);
+
+  /**
+    Asks all the threads to terminate. Jobs that have not been started are not
+    executed.
+  */
+  void terminate() throw();
+
+  /**
+    Waits for all the threads to terminate. All waiting jobs of the pool are
+    executed first unless the pool has been explicitly terminated.
+  */
+  void join() throw();
+
+  /**
+    The job provider is required to invoke this function when a job becomes
+    available.
+  */
+  void post() throw();
+
+  /**
+    Destroys the thread pool.
+  */
+  ~ThreadPool() throw();
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

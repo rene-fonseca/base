3afd262c6bb4b16e51851f4415d3a5875207d359
René Fonseca <rene.m.fonseca@gmail.com>
2001-04-20 16:25:15 +0000

    Added BitSet.

---
+++ base/collection/BitSet.cpp
@@ -0,0 +1,218 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#include <base/collection/BitSet.h>
+#include <base/string/StringOutputStream.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+void BitSet::zeroExtend(unsigned int size) throw(MemoryException) {
+  if (size > this->size) {
+    unsigned int count = getNumberOfElements(size);
+    if (this->size == 0) {
+      this->elements->setSize(count);
+      fill<unsigned long>(getElements(), count, 0);
+    } else {
+      unsigned int oldCount = getNumberOfElements(this->size);
+      if (oldCount < count) {
+        this->elements->setSize(count);
+        fill<unsigned long>(getElements() + oldCount, count - oldCount, 0); // does not overwrite bits in use
+      }
+    }
+    this->size = size;
+  }
+}
+
+BitSet::BitSet() throw() : elements(new ReferenceCountedCapacityAllocator<unsigned long>()), size(0) {
+}
+
+BitSet::BitSet(unsigned int s, bool value) throw() :
+  elements(new ReferenceCountedCapacityAllocator<unsigned long>(getNumberOfElements(size))),
+  size(s) {
+  fill<unsigned long>(getElements(), getNumberOfElements(size), value ? ~(unsigned long)0 : 0);
+  reinitialize();
+}
+
+BitSet& BitSet::operator=(const BitSet& eq) throw() {
+  if (&eq != this) { // protect against self assignment
+    elements = eq.elements;
+    size = eq.size;
+  }
+  return *this;
+}
+
+bool BitSet::getAt(unsigned int index) throw(OutOfRange) {
+  assert(index < size, OutOfRange());
+  return (getElements()[getElementIndex(index)]) & getBitMask(index);
+}
+
+void BitSet::setAt(unsigned int index, bool value) throw(OutOfRange) {
+  assert(index < size, OutOfRange());
+  if (value) {
+    getElements()[getElementIndex(index)] |= getBitMask(index); // set
+  } else {
+    getElements()[getElementIndex(index)] &= ~getBitMask(index); // reset
+  }
+}
+
+BitSet& BitSet::set() throw() {
+  fill<unsigned long>(getElements(), getNumberOfElements(size), ~(unsigned long)0);
+  reinitialize();
+  return *this;
+}
+
+BitSet& BitSet::set(unsigned int index) throw(OutOfRange) {
+  assert(index < size, OutOfRange());
+  getElements()[getElementIndex(index)] |= getBitMask(index);
+  return *this;
+}
+
+BitSet& BitSet::reset() throw() {
+  fill<unsigned long>(getElements(), getNumberOfElements(size), 0);
+  // no need to reinitialize
+  return *this;
+}
+
+BitSet& BitSet::reset(unsigned int index) throw(OutOfRange) {
+  assert(index < size, OutOfRange());
+  getElements()[getElementIndex(index)] &= ~getBitMask(index);
+  return *this;
+}
+
+BitSet& BitSet::flip() throw() {
+  transform<unsigned long>( // TAG: check this
+    getElements(),
+    getNumberOfElements(size),
+    bind2Second(BitwiseExclusiveOr<unsigned long>(), ~(unsigned long)0)
+  );
+  reinitialize();
+  return *this;
+}
+
+BitSet& BitSet::flip(unsigned int index) throw(OutOfRange) {
+  assert(index < size, OutOfRange());
+  getElements()[getElementIndex(index)] ^= getBitMask(index);
+  return *this;
+}
+
+BitSet& BitSet::operator&=(const BitSet& value) throw() {
+  // zeroExtend is not required
+  transformByBinary(
+    getElements(),
+    value.getElements(),
+    getNumberOfElements(minimum(size, value.size)),
+    BitwiseAnd<unsigned long>()
+  );
+  return *this;
+}
+
+BitSet& BitSet::operator|=(const BitSet& value) throw() {
+  zeroExtend(value.getSize());
+  transformByBinary(
+    getElements(),
+    value.getElements(),
+    getNumberOfElements(minimum(size, value.size)),
+    BitwiseOr<unsigned long>()
+  );
+  return *this;
+}
+
+BitSet& BitSet::operator^=(const BitSet& value) throw() {
+  zeroExtend(value.getSize());
+  transformByBinary(
+    getElements(),
+    value.getElements(),
+    getNumberOfElements(minimum(size, value.size)),
+    BitwiseExclusiveOr<unsigned long>()
+  );
+  return *this;
+}
+
+BitSet& BitSet::operator<<=(unsigned int count) throw() {
+  if (count == 0) {
+    return *this;
+  }
+
+  unsigned int oldSize = size;
+  setSize(size + count); // possible copy on write
+
+  unsigned long* writeHead = getElements() + getElementIndex(count);
+  const unsigned long* readHead = getElements(); // must be called after possible copy on write
+  unsigned int mostSignificant = count % (sizeof(unsigned long) * 8);
+  unsigned int leastSignificant = sizeof(unsigned long) * 8 - mostSignificant;
+
+  if (mostSignificant == 0) { // can we do a fast move without binary shifts
+    move<unsigned long>(writeHead, readHead, getNumberOfElements(oldSize));
+  } else {
+    const unsigned long* end = readHead + getNumberOfElements(oldSize);
+    unsigned long previous = 0;
+    while (readHead <= end) {
+      unsigned long temp = *readHead;
+      *writeHead = (previous >> leastSignificant) | (temp << mostSignificant);
+      previous = temp;
+      ++writeHead;
+      ++readHead;
+    }
+  }
+
+  fill<unsigned long>(getElements(), getElementIndex(count), 0); // reinitialize beginning
+  return *this;
+}
+
+BitSet& BitSet::operator>>=(unsigned int count) throw() {
+  if (count == 0) {
+    return *this;
+  }
+
+  unsigned long* writeHead = getElements(); // possible copy on write
+  const unsigned long* readHead = writeHead + getElementIndex(count) + 1;
+  unsigned int leastSignificant = count % (sizeof(unsigned long) * 8);
+  unsigned int mostSignificant = sizeof(unsigned long) * 8 - leastSignificant;
+
+  if (leastSignificant == 0) { // can we do a fast move without binary shifts
+    move<unsigned long>(writeHead, readHead, getNumberOfElements(size));
+  } else {
+    const unsigned long* end = writeHead + getNumberOfElements(size);
+    unsigned long previous = *readHead++;
+    while (readHead <= end) { // TAG: possible segmentation fault problem
+      unsigned long temp = *readHead;
+      *writeHead = (previous >> leastSignificant) | (temp << mostSignificant);
+      previous = temp;
+      ++writeHead;
+      ++readHead;
+    }
+  }
+
+  setSize(size - count);
+  return *this;
+}
+
+void BitSet::removeAll() throw() {
+  elements = new ReferenceCountedCapacityAllocator<unsigned long>(); // no need to copy
+  size = 0;
+}
+
+FormatOutputStream& operator<<(FormatOutputStream& stream, const BitSet& value) {
+  StringOutputStream buffer;
+
+  const unsigned long* current = value.getElements() + value.getSize();
+  unsigned long count = value.getSize();
+  unsigned long mask = ((unsigned long)1) << (value.getSize() % (sizeof(unsigned long) * 8)); // most significant first
+
+  while (count) {
+    unsigned long value = *current;
+    --current;
+    while (mask) { // continue until all bits have been written
+      buffer << ((value & mask) ? '1' : '0');
+      mask >>= 1;
+      --count;
+    }
+    mask = ((unsigned long)1) << (sizeof(unsigned long) * 8 - 1); // most significant first
+  }
+
+  return stream << buffer.getString();
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/collection/BitSet.h
@@ -0,0 +1,265 @@
+/***************************************************************************
+    copyright   : (C) 2000 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_COLLECTION__BIT_SET_H
+#define _DK_SDU_MIP__BASE_COLLECTION__BIT_SET_H
+
+#include <base/mem/ReferenceCountedCapacityAllocator.h>
+#include <base/mem/ReferenceCountedObjectPointer.h>
+#include <base/collection/Collection.h>
+#include <base/collection/Enumeration.h>
+#include <base/collection/InvalidEnumeration.h>
+#include <base/OutOfRange.h>
+#include <base/string/FormatOutputStream.h>
+#include <base/Functor.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Bit set. Ordered sequence of boolean values. Offen used to represent a set of flags.
+
+  @short Set of bits.
+  @author René Møller Fonseca
+  @version 1.0
+*/
+
+class BitSet : public Collection {
+public:
+
+  /**
+    Reference to a single bit within a BitSet.
+  */
+  class Reference {
+  private:
+    friend class BitSet;
+
+    BitSet& bitset; // use reference to avoid 'copy on write'
+    unsigned int index;
+    inline Reference(BitSet& b, unsigned int i) : bitset(b), index(i) {}
+  public:
+    inline Reference& operator=(bool value) throw(OutOfRange) {bitset.setAt(index, value); return *this;}
+    inline operator bool() throw(OutOfRange) {return bitset.getAt(index);}
+  };
+private:
+
+  /** The elements of the bit set. */
+  ReferenceCountedObjectPointer<ReferenceCountedCapacityAllocator<unsigned long> > elements;
+  /** The number of bits in the bit set. */
+  unsigned int size;
+protected:
+
+  /**
+    Returns the elements of the internal array for modifying access.
+  */
+  inline unsigned long* getElements() throw(MemoryException) {
+    elements.copyOnWrite();
+    return elements->getElements();
+  }
+
+  /**
+    Returns the elements of the internal array for non-modifying access.
+  */
+  inline const unsigned long* getElements() const throw() {
+    return elements->getElements();
+  }
+
+  /**
+    Returns the number of required elements to hold the specified number of bits.
+  */
+  inline unsigned int getNumberOfElements(unsigned int size) throw() {
+    return (size + sizeof(unsigned long) * 8 - 1)/(sizeof(unsigned long) * 8);
+  }
+
+  /**
+    Returns the index of the internal element holding the bit at the specified index.
+  */
+  inline unsigned int getElementIndex(unsigned int index) throw() {
+    return index/(sizeof(unsigned long) * 8);
+  }
+
+  /**
+    Returns the bit mask for the specified index.
+  */
+  inline unsigned long getBitMask(unsigned int index) throw() {
+    return ((unsigned long)1) << (index % (sizeof(unsigned long) * 8));
+  }
+
+  /**
+    Zero-extends the bit set to the specified size.
+  */
+  void zeroExtend(unsigned int size) throw(MemoryException);
+
+  /**
+    Resets any unused bits.
+  */
+  inline void reinitialize() throw() {
+    unsigned long mask = getBitMask(size) - 1; // TAG: big endian problem
+    if ((size > 0) && (mask != 0)) { // are some of the bits unused
+      getElements()[getNumberOfElements(size) - 1] &= mask; // reset unused bits
+    }
+  }
+
+  /**
+    Sets the number of bits in the bit set.
+  */
+  inline void setSize(unsigned int size) throw(MemoryException) {
+    elements->setSize(getNumberOfElements(size));
+    reinitialize();
+  }
+public:
+
+  /**
+    Initializes an empty bit set.
+  */
+  BitSet() throw();
+
+  /**
+    Initializes array with the specified number of elements.
+
+    @param size The initial number of bits.
+    @param value The initial state of the bits.
+  */
+  BitSet(unsigned int size, bool value) throw();
+
+  /**
+    Initializes bit set from other bit set.
+  */
+  BitSet(const BitSet& copy) throw(MemoryException) : elements(copy.elements), size(copy.size) {}
+
+  /**
+    Assignment of bit set to bit set.
+  */
+  BitSet& operator=(const BitSet& eq) throw();
+
+  /**
+    Returns the number of bit in the bit set.
+  */
+  inline unsigned int getSize() const throw() {return size;}
+
+  /**
+    Returns true if the bit set is empty.
+  */
+  inline bool isEmpty() const throw() {return size == 0;}
+
+  /**
+    Returns the bit state at the specified index. Throws 'OutOfRange' if the index is invalid.
+
+    @param index The index of the element.
+  */
+  bool getAt(unsigned int index) throw(OutOfRange);
+
+  /**
+    Sets the bit state at the specified index. Throws 'OutOfRange' if the index is invalid.
+
+    @param index The index of the element.
+    @param value The desired value.
+  */
+  void setAt(unsigned int index, bool value) throw(OutOfRange);
+
+  /**
+    Sets (sets to true) all the states of the bit set.
+  */
+  BitSet& set() throw();
+
+  /**
+    Sets (sets to true) the state at the specified index. Throws 'OutOfRange' if the index is invalid.
+  */
+  BitSet& set(unsigned int index) throw(OutOfRange);
+
+  /**
+    Resets (set to false) all the states of the bit set.
+  */
+  BitSet& reset() throw();
+
+  /**
+    Resets (sets to false) the state at the specified index. Throws 'OutOfRange' if the index is invalid.
+  */
+  BitSet& reset(unsigned int index) throw(OutOfRange);
+
+  /**
+    Inverts all the states of the bit set.
+  */
+  BitSet& flip() throw();
+
+  /**
+    Inverts the state at the specified index. Throws 'OutOfRange' if the index is invalid.
+  */
+  BitSet& flip(unsigned int index) throw(OutOfRange);
+
+  /**
+    Binary AND of the bit sets.
+  */
+  BitSet& operator&=(const BitSet& value) throw();
+
+  /**
+    Binary OR of the bit sets.
+  */
+  BitSet& operator|=(const BitSet& value) throw();
+
+  /**
+    Binary EXCLUSIVE OR of the bit sets.
+  */
+  BitSet& operator^=(const BitSet& value) throw();
+
+  /**
+    Binary left shift.
+
+    @param count The number of bits to shift.
+  */
+  BitSet& operator<<=(unsigned int count) throw();
+
+  /**
+    Binary right shift.
+
+    @param count The number of bits to shift.
+  */
+  BitSet& operator>>=(unsigned int count) throw();
+
+  /**
+    Returns an inverted bit set.
+  */
+  inline BitSet operator~() const throw() {
+    return BitSet(*this).flip();
+  }
+
+  /**
+    Returns a left-shifted bit set.
+  */
+  inline BitSet operator<<(unsigned int count) const throw() {
+    return BitSet(*this) <<= count;
+  }
+
+  /**
+    Returns a right-shifted bit set.
+  */
+  inline BitSet operator>>(unsigned int count) const throw() {
+    return BitSet(*this) >>= count;
+  }
+
+  /**
+    Returns reference to the bit at the specified index. Throws 'OutOfRange' if the index is invalid.
+
+    @param index The index of the element.
+  */
+  inline Reference operator[](unsigned int index) throw(OutOfRange) {
+    return Reference(*this, index);
+  }
+
+  /**
+    Removes all the bits.
+  */
+  void removeAll() throw();
+
+  friend FormatOutputStream& operator<<(FormatOutputStream& stream, const BitSet& value);
+};
+
+/**
+  Writes a string representation of a bit set to a format stream.
+*/
+FormatOutputStream& operator<<(FormatOutputStream& stream, const BitSet& value);
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

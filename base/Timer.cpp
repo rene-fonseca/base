2a195d50b21deae6d05f1ac682bd26c5dd8c1419
René Fonseca <rene.m.fonseca@gmail.com>
2001-05-07 16:27:55 +0000

    Added Timer class.

---
+++ base/Timer.cpp
@@ -0,0 +1,85 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#include <base/features.h>
+#include <base/Timer.h>
+
+#if defined(__win32__)
+  #include <windows.h>
+#else // __UNIX__
+  #include <sys/time.h>
+#endif
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+Timer::Timer() throw() : stopTime(0) {
+  start();
+}
+
+inline void Timer::start() throw() {
+#if defined(__win32__)
+  ASSERT(sizeof(LARGE_INTEGER) == sizeof(long long));
+  QueryPerformanceCounter((LARGE_INTERGER*)&startTime);
+#else // __unix__
+  struct timeval temp;
+  gettimeofday(&temp, 0);
+  startTime = 1000000l * temp.tv_sec + temp.tv_usec;
+#endif
+}
+
+inline void Timer::stop() throw() {
+#if defined(__win32__)
+  ASSERT(sizeof(LARGE_INTEGER) == sizeof(long long));
+  QueryPerformanceCounter((LARGE_INTERGER*)&stopTime);
+#else // __unix__
+  struct timeval temp;
+  gettimeofday(&temp, 0);
+  stopTime = 1000000l * temp.tv_sec + temp.tv_usec;
+#endif
+}
+
+long long Timer::getStartTime() const throw() {
+#if defined(__win32__)
+  LARGE_INTEGER frequency; // ticks per second
+  QueryPerformanceFrequency(&frequency); // ignore any error
+  return startTime * 1000000./frequency.QuadPart;
+#else // __unix__
+  return startTime;
+#endif
+}
+
+long long Timer::getStopTime() const throw() {
+#if defined(__win32__)
+  LARGE_INTEGER frequency; // ticks per second
+  QueryPerformanceFrequency(&frequency); // ignore any error
+  return stopTime * 1000000./frequency.QuadPart;
+#else // __unix__
+  return stopTime;
+#endif
+}
+
+long long Timer::getMicroseconds() const throw() {
+#if defined(__win32__)
+  LARGE_INTEGER frequency; // ticks per second
+  QueryPerformanceFrequency(&frequency); // ignore any error
+  return (stopTime - startTime) * 1000000./frequency.QuadPart;
+#else // __unix__
+  return stopTime - startTime;
+#endif
+}
+
+FormatOutputStream& operator<<(FormatOutputStream& stream, const Timer& value) throw(IOException) {
+  long long microseconds = value.getMicroseconds();
+  long long seconds = microseconds/1000000;
+  long long minutes = seconds/60;
+  long long hours = minutes/60;
+  return stream << hours << ":" << minutes%60 << ":" << seconds%60 << "." << microseconds%1000000;
+}
+
+void TimeScope::dump() const throw(IOException) {
+  fout << "Elapsed time (H:M:S.microseconds): " << timer << ENDL;
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/Timer.h
@@ -0,0 +1,99 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__TIMER_H
+#define _DK_SDU_MIP__BASE__TIMER_H
+
+#include <base/Object.h>
+#include <base/string/FormatOutputStream.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Timer wrapper class.
+
+  @short Timer.
+  @author René Møller Fonseca
+*/
+
+class Timer : public Object {
+private:
+
+  /** The start time in platform specific resolution. */
+  long long startTime;
+  /** The stop time in platform specific resolution. */
+  long long stopTime;
+public:
+
+  /**
+    Initializes the timer. The timer is started.
+  */
+  Timer() throw();
+
+  /**
+    Starts the timer.
+  */
+  void start() throw();
+
+  /**
+    Stops the timer.
+  */
+  void stop() throw();
+
+  /**
+    Returns the start time in microseconds.
+  */
+  long long getStartTime() const throw();
+
+  /**
+    Returns the stop time in microseconds.
+  */
+  long long getStopTime() const throw();
+
+  /**
+    Returns the number of elapsed microseconds.
+  */
+  long long getMicroseconds() const throw();
+};
+
+/**
+  Writes a string representation of the elapsed time of the specified timer.
+*/
+FormatOutputStream& operator<<(FormatOutputStream& stream, const Timer& value) throw(IOException);
+
+
+
+class TimeScope : public Object {
+private:
+
+  Timer timer;
+public:
+
+  inline TimeScope() throw() {}
+
+  inline void start() throw() {timer.start();}
+
+  inline void stop() throw() {timer.stop();}
+
+  void dump() const throw(IOException);
+
+  inline ~TimeScope() throw(IOException) {
+    stop();
+    dump();
+  }
+};
+
+/**
+  Times the current scope. Writes the elapsed time to fout when the scope is exited.
+*/
+#define timeScope() Timer _dk_sdu_mip__base__timer
+
+#define startScopeTimer() _dk_sdu_mip__base__timer.start()
+
+#define stopScopeTimer() _dk_sdu_mip__base__timer.stop()
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

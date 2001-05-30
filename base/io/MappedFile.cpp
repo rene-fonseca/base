5fdc41f4b7a9419dce9250f0f33982fd29cf679e
René Fonseca <rene.m.fonseca@gmail.com>
2001-05-30 20:09:25 +0000

    Added file region mapping class.

---
+++ base/io/MappedFile.cpp
@@ -0,0 +1,135 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#include <base/features.h>
+#include <base/io/MappedFile.h>
+#include <base/Base.h>
+#include <base/Functor.h>
+
+#if defined(__win32__)
+  #include <windows.h>
+#else // __unix__
+  #include <sys/types.h>
+  #include <sys/stat.h>
+  #include <sys/time.h>
+  #include <unistd.h>
+  #include <fcntl.h>
+  #include <errno.h>
+  #include <limits.h>
+  #include <string.h> // required by FD_SET on solaris
+  #include <sys/mman.h>
+
+  #undef assert
+#endif
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+MappedFile::MappedFileImpl::MappedFileImpl(const File& f, const FileRegion& r, bool w) throw(FileException) :
+  file(f), region(r), writeable(w) {
+  assert(region.getOffset() >= 0, FileException("Unable to map file region"));
+#if defined(__win32__)
+  HANDLE handle = CreateFileMapping((HANDLE)getHandle(file), NULL, writeable ? PAGE_READWRITE : PAGE_READONLY, 0, 0, NULL);
+  assert(handle != NULL, FileException("Unable to map file region"));
+
+  void* address = MapViewOfFile(
+    handle,
+    writeable ? FILE_MAP_WRITE : FILE_MAP_READ,
+    ((LARGE_INTEGER*)&offset)->HighPart,
+    ((LARGE_INTEGER*)&offset)->LowPart,
+    region.getSize()
+  );
+
+  CloseHandle(handle); // this should not fail
+#else
+  void* address;
+  #if defined(_DK_SDU_MIP__BASE__LARGE_FILE_SYSTEM)
+    address = mmap64(0, region.getSize(), writeable ? (PROT_READ | PROT_WRITE) : PROT_READ, MAP_SHARED, getHandle(file), region.getOffset());
+  #else
+    assert((region.getOffset() >= 0) && (region.getOffset() <= INT_MAX), FileException("Unable to map file region"));
+    address = mmap(0, region.getSize(), writeable ? (PROT_READ | PROT_WRITE) : PROT_READ, MAP_SHARED, getHandle(file), region.getOffset());
+  #endif
+  assert(address != (void*)-1, FileException("Unable to map file region"));
+#endif
+  bytes = address;
+}
+
+void MappedFile::MappedFileImpl::flush() const throw(FileException) {
+#if defined(__win32__)
+  if (!FlushViewOfFile(bytes, 0)) {
+    throw FileException("Unable to flush");
+  }
+#else // __unix__
+  if (msync(bytes, region.getSize(), MS_SYNC)) {
+    throw FileException("Unable to flush");
+  }
+#endif
+}
+
+MappedFile::MappedFileImpl::~MappedFileImpl() throw(FileException) {
+#if defined(__win32__)
+  if (!UnmapViewOfFile(bytes)) {
+    throw FileException("Unable to unmap file");
+  }
+#else // __unix__
+  if (munmap(bytes, region.getSize())) {
+    throw FileException("Unable to unmap file");
+  }
+#endif
+}
+
+unsigned int MappedFile::getGranularity() throw() {
+#if defined(__win32__)
+  SYSTEM_INFO info;
+  GetSystemInfo(&info);
+  return info.dwAllocationGranularity;
+#else // __unix__
+  ASSERT(sysconf(_SC_PAGE_SIZE) <= UINT_MAX); // this should never happen
+  return sysconf(_SC_PAGE_SIZE);
+#endif
+}
+
+MappedFile::MappedFile(const File& file, const FileRegion& region, bool writeable) throw(FileException) : map(0) {
+  map = new MappedFileImpl(file, region, writeable);
+//  assert(r.getOffset() >= 0, FileException("Unable to map file"));
+//#if defined(__win32__)
+//  HANDLE handle = CreateFileMapping((HANDLE)f.fd->getHandle(), NULL, writeable ? PAGE_READWRITE : PAGE_READONLY, 0, 0, NULL);
+//  if (handle == NULL) {
+//    throw FileException("Unable to map file");
+//  }
+//
+//  void* address = MapViewOfFile(
+//    handle,
+//    writeable ? FILE_MAP_WRITE : FILE_MAP_READ,
+//    ((LARGE_INTEGER*)&offset)->HighPart,
+//    ((LARGE_INTEGER*)&offset)->LowPart,
+//    size
+//  );
+//
+//  CloseHandle(handle); // this should not fail
+//
+//  map = new MappedFileImpl(f, address, size);
+//#else
+//  void* address;
+//#if defined(_DK_SDU_MIP__BASE__LARGE_FILE_SYSTEM)
+//  address = mmap64(0, r.getSize(), writeable ? (PROT_READ | PROT_WRITE) : PROT_READ, MAP_SHARED, f.fd->getHandle(), r.getOffset());
+//#else
+//  assert((r.getOffset() >= 0) && (r.getOffset() <= INT_MAX), FileException("Unable to map file"));
+//  address = mmap(0, r.getSize(), writeable ? (PROT_READ | PROT_WRITE) : PROT_READ, MAP_SHARED, f.fd->getHandle(), r.getOffset());
+//#endif
+//  if (address == (void*)-1) {
+//    throw FileException("Unable to map file");
+//  }
+//  map = new MappedFileImpl(f, address, r);
+//#endif
+}
+
+MappedFile& MappedFile::operator=(const MappedFile& eq) throw() {
+  if (&eq == this) {
+    map = eq.map;
+  }
+  return *this;
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/io/MappedFile.h
@@ -0,0 +1,111 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_IO__MAPPED_FILE_H
+#define _DK_SDU_MIP__BASE_IO__MAPPED_FILE_H
+
+#include <base/Object.h>
+#include <base/io/File.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  This class is used to map a specified file region into the address space of the process.
+
+  @short File region mapper.
+  @author René Møller Fonseca
+  @version 1.0
+*/
+
+class MappedFile : public Object {
+private:
+
+  class MappedFileImpl : public ReferenceCountedObject {
+  private:
+
+    File file; // ensure that the file is not closed before map has been closed - may not be required
+    FileRegion region;
+    bool writeable;
+    void* bytes;
+  public:
+
+    MappedFileImpl(const File& file, const FileRegion& region, bool writeable) throw(FileException);
+
+    inline void* getBytes() const throw() {return bytes;}
+
+    inline File& getFile() throw() {return file;}
+
+    inline const FileRegion& getRegion() const throw() {return region;}
+
+    inline bool isWriteable() throw() {return writeable;}
+
+    void flush() const throw(FileException);
+
+    ~MappedFileImpl() throw(FileException);
+  };
+
+  /** The internal mapping representation. */
+  ReferenceCountedObjectPointer<MappedFileImpl> map;
+protected:
+
+  /**
+    Returns the handle of the specified file.
+  */
+  inline static int getHandle(File& file) {return file.fd->getHandle();}
+public:
+
+  /**
+    Returns the required granularity of the file region offset.
+  */
+  static unsigned int getGranularity() throw();
+
+  /**
+    Initializes a file mapping.
+
+    @param file The file to be mapped into memory.
+    @param region The file region to be mapped. The offset of the region must honour the granularity returned by getGranularity.
+    @param writeable Specifies that write access is required. Default is false.
+  */
+  MappedFile(const File& file, const FileRegion& region, bool writeable = false) throw(FileException);
+
+  /**
+    Initializes mapping from other mapping.
+  */
+  inline MappedFile(const MappedFile& copy) throw() : map(copy.map) {}
+
+  /**
+    Assignment of mapping by mapping.
+  */
+  MappedFile& operator=(const MappedFile& eq) throw();
+
+  /**
+    Returns the mapped bytes. Do not use the mapping outside the requested
+    mapping range.
+  */
+  inline byte* getBytes() const throw() {return static_cast<byte*>(map->getBytes());}
+
+  /**
+    Returns the mapped file region.
+  */
+  inline const FileRegion& getRegion() const throw() {return map->getRegion();}
+
+  /**
+    Flushes the mapping.
+  */
+  inline void flush() const throw(FileException) {map->flush();}
+
+  /**
+    Maps the specified file region.
+
+    @region The file region to be mapped.
+  */
+  inline void remap(const FileRegion& region) throw(FileException) {
+    map = new MappedFileImpl(map->getFile(), region, map->isWriteable());
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

43d653f50f0e5188fb1ab23de4d74d07d35756dc
René Fonseca <rene.m.fonseca@gmail.com>
2001-06-06 07:57:04 +0000

    Updated/added testing apps.

--- testsuite/daemon.cpp
+++ testsuite/daemon.cpp
@@ -8,33 +8,23 @@
 #include <base/string/FormatOutputStream.h>
 #include <base/Application.h>
 #include <base/Type.h>
+#include <base/concurrency/Thread.h>
+#include <base/Daemon.h>
 
 using namespace base;
 
-void readFile(const String& filename) {
-  fout << "Initializing file input stream" << ENDL;
-  FileInputStream file(filename, 0);
-
-  fout << "Binding file input stream to format input stream" << ENDL;
-  FormatInputStream format(file);
-
-  while (format.available()) {
-    char ch;
-    format >> ch;
-    fout << ch;
+int daemon() {
+  for (unsigned int i = 0; i < 1000000000; ++i) {
   }
+  return 0;
 }
 
 int entry() {
-  fout << "Testing implementation of the FileInputStream" << ENDL;
-
-  const Array<String> arguments = Application::getApplication()->getArguments();
-  if (arguments.getSize() != 1) {
-    fout << Application::getApplication()->getName() << " filename" << ENDL;
-    return 0; // stop
+  fout << "Testing implementation of the Daemon..." << ENDL;
+  if (Daemon::daemonize()) {
+    return daemon();
   }
-
-  readFile(arguments[0]);
+  fout << "Forked" << ENDL;
   return 0;
 }
 
---
+++ testsuite/xml.cpp
@@ -0,0 +1,163 @@
+/***************************************************************************
+    copyright   : (C) 2001 by René Møller Fonseca
+    email       : fonseca@mip.sdu.dk
+ ***************************************************************************/
+
+#include <base/string/FormatOutputStream.h>
+#include <base/io/File.h>
+#include <base/xml/XMLParser.h>
+#include <base/Type.h>
+#include <base/collection/Stack.h>
+#include <base/collection/Array.h>
+
+using namespace base;
+
+class Callback : public XMLParser::Callback {
+public:
+
+  void startDocument() {
+    fout << "XML - startDocument" << EOL;
+  }
+
+  void endDocument() {
+    fout << "XML - endDocument" << EOL;
+  }
+
+  void startElement(const String& name, const XMLPropertyHash& attributes) {
+    fout << "XML - startElement: " << name << " - {";
+    XMLPropertyHash::ReadOnlyEnumeration enu(attributes);
+    while (enu.hasNext()) {
+      const XMLProperty* attribute = enu.next()->getValue();
+      fout << "{" << attribute->getName() << ";" << attribute->getValue() << "}";
+      if (enu.hasNext()) {
+        fout << ", ";
+      }
+    }
+    fout << "}" << EOL;
+  }
+
+  void endElement(const String& n) {
+    fout << "XML - endElement: " << n << EOL;
+  }
+
+  void characters(const String& s) {
+    fout << "XML - characters: " << s << EOL;
+  }
+
+  void comment(const String& s) {
+    fout << "XML - comment: " << s << EOL;
+  }
+
+  void warning(const String& s) {
+    fout << "XML - warning: " << s << EOL;
+  }
+
+  void error(const String& s) {
+    fout << "XML - error: " << s << EOL;
+  }
+
+  void fatalError(const String& s) {
+    fout << "XML - fatalError: " << s << EOL;
+  }
+};
+
+class StripCallback : public XMLParser::Callback {
+private:
+
+  bool ignore;
+  Stack<bool> oldIgnores;
+  Array<bool> a;
+  unsigned int i;
+  unsigned int chapter;
+public:
+
+  StripCallback() throw() : ignore(false), chapter(1) {
+  }
+
+  void startElement(const String& name, const XMLPropertyHash& attributes) {
+//    oldIgnores.push(ignore);
+    a.append(ignore);
+    ignore = false;
+    if (name == String("title")) {
+    } else if (name == String("chapter")) {
+      fout << "  " << chapter++ << ". ";
+    } else if (name == String("para")) {
+    } else { // unknown entry
+      ignore = true;
+    }
+  }
+
+  void endElement(const String& name) {
+    if (!ignore) {
+      fout << EOL;
+    }
+    ignore = a[a.getSize() - 1];
+    a.remove(a.getSize() - 1);
+  }
+
+  void characters(const String& s) {
+    if (!ignore) {
+      fout << s;
+    }
+  }
+
+  void warning(const String& s) {
+    ferr << "XML - warning: " << s << EOL;
+  }
+
+  void error(const String& s) {
+    ferr << "XML - error: " << s << EOL;
+  }
+
+  void fatalError(const String& s) {
+    ferr << "XML - fatalError: " << s << EOL;
+  }
+};
+
+
+
+void test(const String& filename) {
+  fout << "Testing XML classes..." << ENDL;
+
+  Allocator<char> buffer;
+
+  {
+    fout << "Reading XML file into buffer..." << ENDL;
+    File file(filename, File::READ, File::EXCLUSIVE);
+    buffer.setSize(file.getSize());
+    file.read(buffer.getElements(), file.getSize());
+  }
+
+  fout << ">>>" << ENDL;
+  for (unsigned int i = 0; i < buffer.getSize(); ++i) {
+    fout << buffer.getElements()[i];
+  }
+  fout << "<<<" << ENDL;
+
+  fout << "Parsing..." << ENDL;
+  Callback callback;
+  XMLParser parser(&callback);
+  parser.parse(buffer.getElements(), buffer.getSize());
+  parser.terminate();
+}
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 2) {
+    fout << "xml filename" << ENDL;
+    return 0; // stop
+  }
+
+  String filename = argv[1];
+
+  try {
+    test(filename);
+  } catch(Exception& e) {
+    ferr << "Catched exception - " << getTypename(e) << ": " << e.getMessage() << ENDL;
+    return 1;
+  } catch(...) {
+    ferr << "Unknown exception" << ENDL;
+    return 1;
+  }
+  return 0;
+}

be8595f2bfea984af9316ccd9ca66130bfeec333
René Fonseca <rene.m.fonseca@gmail.com>
2001-07-25 19:17:07 +0000

    Added functionality.

--- base/string/FormatInputStream.cpp
+++ base/string/FormatInputStream.cpp
@@ -20,32 +20,140 @@ _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 FileDescriptorInputStream standardInputStream(FileDescriptor::getStandardInput());
 FormatInputStream fin(standardInputStream);
 
-FormatInputStream::FormatInputStream(InputStream& in, unsigned int size) throw(BindException) :
-  BufferedInputStream(in, size) {
+FormatInputStream::FormatInputStream(InputStream& in) throw(BindException) :
+  FilterInputStream(in),
+  buffer(WINDOW_SIZE),
+  head(buffer.getBeginIterator()),
+  tail(head) {
 }
 
-char FormatInputStream::getCharacter() throw(IOException) {
-  char buffer;
-  read(&buffer, sizeof(buffer));
-  return buffer;
+bool FormatInputStream::overwriteFromSource() throw(IOException) {
+  FilterInputStream::wait(); // blocking wait for more input
+  unsigned int available = FilterInputStream::available();
+  if (available == 0) { // TAG: what about eof
+    return false;
+  }
+  unsigned int size = minimum<unsigned int>(WINDOW_SIZE, available);
+  head = buffer.getBeginIterator();
+  tail = head;
+  FilterInputStream::read(head.getValue(), size); // will not block
+  head += size;
+  return true;
+}
+
+bool FormatInputStream::appendFromSource() throw(InvalidFormat, IOException) {
+  FilterInputStream::wait(); // blocking wait for more input
+  unsigned int available = FilterInputStream::available();
+  if (available == 0) { // TAG: what about eof
+    return false;
+  }
+  unsigned int size = minimum<unsigned int>(buffer.getEndReadIterator() - head, available);
+  assert(size > 0, InvalidFormat("Window size exceeded"));
+  FilterInputStream::read(head.getValue(), size); // will not block
+  head += size;
+  return true;
+}
+
+void FormatInputStream::appendToString(String& result, ReadIterator end) throw(InvalidFormat) {
+  assert(result.getLength() + (end - tail) <= MAXIMUM_LINE_SIZE, InvalidFormat("Maximum length of line exceeded"));
+  for (; tail < end; ++tail) { // TAG: need alternative
+    result += *tail;
+  }
+}
+
+String FormatInputStream::getLine() throw(InvalidFormat, IOException) {
+  String result;
+  while (true) { // find new line (i.e. any of the substrings "\n", "\r", "\n\r", or "\r\n")
+    ReadIterator i = tail;
+    for (; i < head; ++i) {
+      char ch = *i;
+      if (ch == '\n') {
+        appendToString(result, i);
+        ++tail;
+        if ((tail == head) && !overwriteFromSource()) {
+          return result; // eof reached
+        }
+        if (*tail == '\r') {
+          ++tail;
+        }
+        return result;
+      } else if (ch == '\r') {
+        appendToString(result, i);
+        ++tail;
+        if ((tail == head) && !overwriteFromSource()) {
+          return result; // eof reached
+        }
+        if (*tail == '\n') {
+          ++tail;
+        }
+        return result;
+      }
+    }
+    appendToString(result, i);
+    if (!overwriteFromSource()) {
+      return result; // eof reached
+    }
+  }
+}
+
+unsigned int FormatInputStream::read(char* buffer, unsigned int size, bool nonblocking) throw(IOException) {
+  unsigned int bytesRead = 0; // number of bytes that have been copied into external buffer
+  while (true) {
+    // copy from internal to external buffer - no overlap
+    unsigned int bytesToCopy = minimum<unsigned int>(size, head - tail);
+    copy(buffer + bytesRead, tail.getValue(), bytesToCopy);
+    tail += bytesToCopy;
+    bytesRead += bytesToCopy;
+    size -= bytesToCopy;
+    if (size == 0) { // have we read all the bytes we were asked to read
+      break;
+    }
+    if (!overwriteFromSource()) { // refill buffer
+      break; // eof has been reached
+    }
+  }
+  return bytesRead;
 }
 
 FormatInputStream::~FormatInputStream() throw(IOException) {
   TRACE_MEMBER();
 }
 
-FormatInputStream& operator>>(FormatInputStream& stream, bool& value) {
-//  char* buffer = stream.peek(5);
-  return stream;
-}
 
-FormatInputStream& operator>>(FormatInputStream& stream, char& value) {
+
+FormatInputStream& operator>>(FormatInputStream& stream, char& value) throw(IOException) {
   value = stream.getCharacter();
   return stream;
 }
 
-FormatInputStream& operator>>(FormatInputStream& stream, char*& value) {
-//  char* buffer = stream.readUntil("\n");
+FormatInputStream& operator>>(FormatInputStream& stream, String& value) throw(IOException) {
+  value = stream.getLine();
+  return stream;
+}
+
+FormatInputStream& operator>>(FormatInputStream& stream, unsigned int& value) throw(InvalidFormat, IOException) {
+  value = 0;
+  char ch;
+  do {
+    ch = stream.getCharacter();
+  } while (String::Traits::isSpace(ch));
+
+  assert((ch >= '0') && (ch <= '9'), InvalidFormat("Not an unsigned int"));
+  do {
+    ch -= '0';
+    if ((ch >= 0) && (ch <= 9)) {
+      if ((value < UnsignedInt::MAXIMUM/10) || ((value == UnsignedInt::MAXIMUM/10) && (ch <= UnsignedInt::MAXIMUM%10))) {
+        value = ch + 10 * value;
+        try {
+          ch = stream.getCharacter(); // TAG: must accept eof
+        } catch (EndOfFile& e) {
+          break;
+        }
+        continue;
+      }
+    }
+    throw InvalidFormat("Not an unsigned int");
+  } while (!String::Traits::isSpace(ch));
   return stream;
 }
 
--- base/string/FormatInputStream.h
+++ base/string/FormatInputStream.h
@@ -16,31 +16,85 @@
 
 #include <base/io/BufferedInputStream.h>
 #include <base/io/BindException.h>
+#include <base/io/EndOfFile.h>
+#include <base/string/InvalidFormat.h>
+#include <base/string/String.h>
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
 /**
-  Format input stream. This class provides...
+  Format input stream. This class is not MT-safe.
 
+  @see FormatOutputStream
+  @short Reader of formatted input.
   @author René Møller Fonseca
   @version 1.0
 */
 
-class FormatInputStream : public BufferedInputStream {
+class FormatInputStream : public FilterInputStream {
+public:
+
+  /** Specifies the maximum length of a line. */
+  static const unsigned int MAXIMUM_LINE_SIZE = 1024;
+  /** Specifies the window size. */
+  static const unsigned int WINDOW_SIZE = 4096;
+protected:
+
+  typedef Allocator<String::Character>::Iterator Iterator;
+  typedef Allocator<String::Character>::ReadIterator ReadIterator;
+
+  /** The internal buffer. */
+  Allocator<String::Character> buffer;
+  /** The current write position. */
+  Iterator head;
+  /** The current read position. */
+  ReadIterator tail;
+
+  /**
+    Fills the internal buffer with bytes from the input stream. Removes any characters already in the buffer.
+  */
+  bool overwriteFromSource() throw(IOException);
+
+  /**
+    Fills the internal buffer with bytes from the input stream without removing any characters already in the buffer.
+  */
+  bool appendFromSource() throw(InvalidFormat, IOException);
+
+  /** Santas little helper. */
+  void appendToString(String& result, ReadIterator end) throw(InvalidFormat);
 public:
 
   /**
     Initializes the format input stream.
 
     @param in The input stream.
-    @param size The size of the buffer. Default is given by DEFAULT_BUFFER_SIZE.
   */
-  FormatInputStream(InputStream& in, unsigned int size = DEFAULT_BUFFER_SIZE) throw(BindException);
+  FormatInputStream(InputStream& in) throw(BindException);
 
   /**
-    Read a character from the stream.
+    Reads one character from the stream.
   */
-  char getCharacter() throw(IOException);
+  inline char getCharacter() throw(IOException) {
+    if (tail == head) {
+      assert(overwriteFromSource(), EndOfFile());
+    }
+    return *tail++;
+  }
+
+  /**
+    Reads one line from the stream.
+  */
+  String getLine() throw(InvalidFormat, IOException);
+
+  /**
+    Fills the specified buffer with bytes from the stream. Blocks if asked to read more bytes than available.
+
+    @param buffer The buffer to receive the bytes.
+    @param size The size of the buffer.
+    @param nonblocking Specifies that the method may not block. Default is false.
+    @return The actual number of bytes read from the stream.
+  */
+  unsigned int read(char* buffer, unsigned int size, bool nonblocking = false) throw(IOException);
 
   /**
     Destroy format input stream.
@@ -54,18 +108,29 @@ public:
 */
 extern FormatInputStream fin;
 
-FormatInputStream& operator>>(FormatInputStream& stream, bool& value);
-FormatInputStream& operator>>(FormatInputStream& stream, char& value);
-FormatInputStream& operator>>(FormatInputStream& stream, char*& value);
-//FormatInputStream& operator>>(FormatInputStream& stream, short int value);
-//FormatInputStream& operator>>(FormatInputStream& stream, unsigned short int value);
-//FormatInputStream& operator>>(FormatInputStream& stream, int value);
-//FormatInputStream& operator>>(FormatInputStream& stream, unsigned int value);
-//FormatInputStream& operator>>(FormatInputStream& stream, long long int value);
-//FormatInputStream& operator>>(FormatInputStream& stream, unsigned long long int value);
-//FormatInputStream& operator>>(FormatInputStream& stream, float value);
-//FormatInputStream& operator>>(FormatInputStream& stream, double value);
-//FormatInputStream& operator>>(FormatInputStream& stream, long double value);
+/**
+  Reads one character from the format input stream.
+*/
+FormatInputStream& operator>>(FormatInputStream& stream, char& value) throw(IOException);
+
+/**
+  Reads one line from the format input stream.
+*/
+FormatInputStream& operator>>(FormatInputStream& stream, String& value) throw(IOException);
+
+/**
+  Reads unsigned int from the format input stream.
+*/
+FormatInputStream& operator>>(FormatInputStream& stream, unsigned int& value) throw(InvalidFormat, IOException);
+
+//FormatInputStream& operator>>(FormatInputStream& stream, short int& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, unsigned short int& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, int& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, long long int& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, unsigned long long int& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, float& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, double& value);
+//FormatInputStream& operator>>(FormatInputStream& stream, long double& value);
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE
 

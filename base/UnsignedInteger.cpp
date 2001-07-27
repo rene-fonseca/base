4b07b4768ac679ae1634bfec4676f5fe5eeabf2e
René Fonseca <rene.m.fonseca@gmail.com>
2001-07-27 16:49:00 +0000

    Added UnsignedInteger class.

---
+++ base/UnsignedInteger.cpp
@@ -0,0 +1,92 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/UnsignedInteger.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+unsigned int UnsignedInteger::parse(const String& str, unsigned int accept) throw(InvalidFormat) {
+  unsigned int base = 10; // default integer base
+  String::ReadIterator i = str.getBeginReadIterator();
+  const String::ReadIterator end = str.getEndReadIterator();
+
+  assert(i < end, InvalidFormat("Not an integer")); // do not accept empty strings
+
+  switch (accept) {
+  case BIN:
+    base = 2;
+    break;
+  case OCT:
+    base = 8;
+    break;
+  case DEC:
+    base = 10;
+    break;
+  case HEX:
+    base = 16;
+    break;
+  default:
+    accept |= PREFIX; // ambiguous unless integer base is determined by prefix
+  }
+
+  if (accept & PREFIX) {
+    // determine base by looking at prefix (0b: binary, 0x: hex, 0: octal, other: decimal)
+    char first = *i;
+    if (*i == '0') { // is binary, octal, or hex
+      ++i;
+      if (i < end) { // has second char
+        char second = ASCIITraits::toLower(*i);
+        if (second == 'b') {
+          ++i;
+          base = 2;
+        } else if (second == 'x') {
+          ++i;
+          base = 16;
+        } else {
+          --i; // important 'cause we want to accept "0"
+          base = (accept & OCT) ? 8 : 10; // default is octal within this content (but use DEC if disallowed)
+        }
+      } else {
+        --i; // important 'cause we want to accept "0"
+        base = (accept & OCT) ? 8 : 10; // default is octal within this content (but use DEC if disallowed)
+      }
+    }
+
+    // check if base is allowed
+    assert(
+      ((base == 2) && (accept & BIN)) ||
+      ((base == 8) && (accept & OCT)) ||
+      ((base == 10) && (accept & DEC)) ||
+      ((base == 16) && (accept & HEX)),
+      InvalidFormat("Not an integer")
+    );
+  }
+
+  unsigned int highLimit = MAXIMUM/base;
+  unsigned int lowLimit = MAXIMUM%base;
+  unsigned int temp = 0;
+  while (i < end) {
+    char ch = *i++;
+    unsigned char digitValue = ASCIITraits::digitToValue(ch); // unspecified for non-digit
+    assert(
+      ASCIITraits::isHexDigit(ch) &&
+      (digitValue < base) &&
+      ((temp < highLimit) || ((temp == highLimit) && (digitValue <= lowLimit))),
+      InvalidFormat("Not an integer")
+    );
+    temp = temp * base + digitValue;
+  }
+  return temp;
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/UnsignedInteger.h
@@ -0,0 +1,128 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__UNSIGNED_INTEGER_H
+#define _DK_SDU_MIP__BASE__UNSIGNED_INTEGER_H
+
+#include <base/Object.h>
+#include <base/Type.h>
+#include <base/string/String.h>
+#include <base/string/InvalidFormat.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Unsigned integer (32 bits or more).
+
+  @short Unsigned integer.
+  @author René Møller Fonseca
+  @version 1.0
+*/
+
+class UnsignedInteger : public Object {
+public:
+
+  /** True if the integer type is signed. */
+  static const bool SIGNED = false;
+  /** Specifies the minimum value. */
+  static const int MINIMUM = UnsignedInt::MINIMUM;
+  /** Specifies the maximum value. */
+  static const int MAXIMUM = UnsignedInt::MAXIMUM;
+
+  enum {
+    BIN = 1, /**< Specifies binary integer base (2). */
+    DEC = 2, /**< Specifies decimal integer base (10). */
+    OCT = 4, /**< Specifies octal integer base (8). */
+    HEX = 8, /**< Specifies hexadecimal integer base (16). */
+    PREFIX = 16, /**< Specifies that prefix is required. */
+    ANY=BIN|DEC|OCT|HEX|PREFIX /**< Specifies any supported base (integer base is derived from prefix). */
+  };
+protected:
+
+  /** The value. */
+  unsigned int value;
+public:
+
+  /**
+    Returns the value of the integer string representation.
+
+    @param str The string representation.
+    @param accept Specifies the integer bases to accept. Default is ANY.
+  */
+  static unsigned int parse(const String& str, unsigned int accept = ANY) throw(InvalidFormat);
+
+  /**
+    Initializes the integer as zero.
+  */
+  inline UnsignedInteger() throw() : value(0) {}
+
+  /**
+    Initializes the integer with the specified value. Implicit initialization allowed.
+
+    @param value The desired value.
+  */
+  UnsignedInteger(unsigned int value) throw();
+
+  /**
+    Initializes the integer from the specified string representation.
+
+    @param str The string representation.
+    @param accept Specifies the bases to accept. Default is ANY.
+  */
+  inline UnsignedInteger(const String& str, unsigned int accept = ANY) throw(InvalidFormat) : value(parse(str, accept)) {
+  }
+
+  /**
+    Copy constructor. Initializes a new Integer from other Integer object.
+  */
+  inline UnsignedInteger(const UnsignedInteger& copy) throw() : value(copy.value) {}
+
+  /**
+    Assignment of integer to this integer.
+  */
+  inline UnsignedInteger& operator=(const UnsignedInteger& eq) throw() {
+    value = eq.value;
+    return *this;
+  }
+
+  /**
+    Assignment of native type to this integer.
+  */
+  inline UnsignedInteger& operator=(unsigned int value) throw() {
+    this->value = value;
+    return *this;
+  }
+
+  /**
+    Gets the value of the integer.
+  */
+  inline unsigned int getValue() const throw() {return value;}
+
+  /**
+    Sets the value of the integer.
+
+    @param value The desired value.
+  */
+  inline void setValue(unsigned int value) throw() {this->value = value;}
+
+  /**
+    Casts integer to native type.
+  */
+  inline operator unsigned int() const throw() {return value;}
+};
+
+inline UnsignedInteger::UnsignedInteger(unsigned int v) throw() : value(v) {}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

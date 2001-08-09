688f1848a1e4b1113f0a370c401df30b3c2c4b94
René Fonseca <rene.m.fonseca@gmail.com>
2001-08-09 01:47:42 +0000

    Added pseudorandom number generator.

---
+++ base/mathematics/Random.cpp
@@ -0,0 +1,145 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/mathematics/Random.h>
+#include <base/concurrency/SpinLock.h>
+#include <base/Timer.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+namespace RandomImpl {
+
+  template<>
+  class Traits<Random::MT11213A> {
+  public:
+
+    static const unsigned int w = 32;
+    static const unsigned int n = 351;
+    static const unsigned int m = 175;
+    static const unsigned int r = 19;
+    static const unsigned int a = 0xe4bd75f5;
+    static const unsigned int u = 11;
+    static const unsigned int s = 7;
+    static const unsigned int b = 0x655e5280;
+    static const unsigned int t = 15;
+    static const unsigned int c = 0xffd58000;
+    static const unsigned int l = 17;
+  };
+
+  template<>
+  class Traits<Random::MT11213B> {
+  public:
+
+    static const unsigned int w = 32;
+    static const unsigned int n = 351;
+    static const unsigned int m = 175;
+    static const unsigned int r = 19;
+    static const unsigned int a = 0xccab8ee7;
+    static const unsigned int u = 11;
+    static const unsigned int s = 7;
+    static const unsigned int b = 0x31b6ab00;
+    static const unsigned int t = 15;
+    static const unsigned int c = 0xffe50000;
+    static const unsigned int l = 17;
+  };
+
+  template<>
+  class Traits<Random::MT19937> {
+  public:
+
+    static const unsigned int w = 32;
+    static const unsigned int n = 624;
+    static const unsigned int m = 397;
+    static const unsigned int r = 31;
+    static const unsigned int a = 0x9908b0df;
+    static const unsigned int u = 11;
+    static const unsigned int s = 7;
+    static const unsigned int b = 0x9d2c5680;
+    static const unsigned int t = 15;
+    static const unsigned int c = 0xefc60000;
+    static const unsigned int l = 18;
+  };
+
+  template<>
+  class Traits<Random::TT800> {
+  public:
+
+    static const unsigned int w = 32;
+    static const unsigned int n = 25;
+    static const unsigned int m = 7;
+    static const unsigned int r = 0;
+    static const unsigned int a = 0x8ebfd028;
+    static const unsigned int u = 0; // zero => disabled
+    static const unsigned int s = 7;
+    static const unsigned int b = 0x2b5b2500;
+    static const unsigned int t = 15;
+    static const unsigned int c = 0xdb8b0000;
+    static const unsigned int l = 16;
+  };
+
+  class Randomizer {
+  public:
+    inline Randomizer() throw() {Random::randomize();}
+  };
+
+  SpinLock lock; // before randomization!
+  Randomizer randomizer; // randomize during application initialization
+
+}; // end of namespace RandomImpl
+
+unsigned int Random::state[Random::Traits::n];
+unsigned int Random::nextWord; // initialized by randomize()
+
+void Random::randomize() throw() {
+  Timer timer;
+  long long seed = timer.getStartTime();
+  seed += 20010908014640; // magic date/time in UTC - 09/08/2001 01:46:40
+  seed &= 0xffffffff;
+  if (seed == 0) {
+    seed = 20010908014640;
+  }
+  randomize(seed);
+}
+
+void Random::randomize(unsigned int seed) throw() {
+  ASSERT((Traits::w == 32) && (sizeof(unsigned int) == 4));
+  RandomImpl::lock.exclusiveLock();
+    for (unsigned int i = 0; i < Traits::n; ++i) {
+      // assert: (seed != 0) && (seed < (1 << 32))
+      state[i] = seed;
+      seed *= 69069;
+    }
+    nextWord = 0;
+  RandomImpl::lock.releaseLock();
+}
+
+unsigned int Random::getInteger() throw() {
+  ASSERT((Traits::w == 32) && (sizeof(unsigned int) == 4));
+  unsigned int bits;
+  RandomImpl::lock.exclusiveLock();
+    unsigned int i = (nextWord + Traits::m < Traits::n) ? (nextWord + Traits::m) : (nextWord + Traits::m - Traits::n);
+    unsigned int y = state[i] & ~((1 << Traits::r) - 1) | state[i + 1] & ((1 << Traits::r) - 1);
+    bits = state[nextWord] = state[i] ^ (y >> 1) ^ ((y & 0x1) ? Traits::a : 0);
+    nextWord = ++nextWord % Traits::n;
+  RandomImpl::lock.releaseLock();
+
+  if (Traits::u != 0) {
+    bits ^= (bits >> Traits::u);
+  }
+  bits ^= (bits << Traits::s) & Traits::b;
+  bits ^= (bits << Traits::t) & Traits::c;
+  bits ^= (bits >> Traits::l);
+  return bits;
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/mathematics/Random.h
@@ -0,0 +1,82 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_MATHEMATICS__RANDOM_H
+#define _DK_SDU_MIP__BASE_MATHEMATICS__RANDOM_H
+
+#include <base/features.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+namespace RandomImpl {
+
+  template<unsigned int ID>
+  class Traits {
+  };
+
+};
+
+/**
+  Implementation of a pseudorandom number generator. The generator in described
+  in the paper "Mersenne Twister: A 623-dimensionally equidistributed uniform
+  pseudorandom number generator" by Makoto Matsumoto and Takuji Nishimura. The
+  implementation is MT-safe.
+
+  @short Random number generator
+  @author René Møller Fonseca
+  @version 1.0
+*/
+
+class Random {
+public:
+
+  /** The supported generators. */
+  enum Generator {MT11213A = 0, MT11213B = 1, MT19937 = 2, TT800 = 3};
+  /** Specifies the generator to be used. */
+  typedef RandomImpl::Traits<TT800> Traits;
+private:
+
+  /** The internal state of the random number generator. */
+  static unsigned int state[];
+  /** The internal state of the random number generator. */
+  static unsigned int nextWord;
+public:
+
+  /**
+    Initializes the internal state of the generator from the current time.
+  */
+  static void randomize() throw();
+
+  /**
+    Initializes the internal state of the generator.
+
+    @param seed The desired seed (generator does not work with zero).
+  */
+  static void randomize(unsigned int seed) throw();
+
+  /**
+    Returns a random integer (unsigned) utilizing the full range of the type.
+  */
+  static unsigned int getInteger() throw();
+
+  /**
+    Returns a random real number in the range [0;1].
+  */
+  inline static long double getLongDouble() throw() {
+    return (1.0L/0xffffffff) * getInteger();
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

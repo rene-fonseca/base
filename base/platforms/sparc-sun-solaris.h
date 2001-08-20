777cb496464c872dae8c55a3c1a99cb562ed3f97
René Fonseca <rene.m.fonseca@gmail.com>
2001-08-20 11:52:15 +0000

    Added platform descriptions.

---
+++ base/platforms/sparc-sun-solaris.h
@@ -0,0 +1,26 @@
+#define _DK_SDU_MIP__BASE__FLAVOUR _DK_SDU_MIP__BASE__UNIX
+#define _DK_SDU_MIP__BASE__OS _DK_SDU_MIP__BASE__SOLARIS
+#define _DK_SDU_MIP__BASE__ARCH _DK_SDU_MIP__BASE__SPARC
+#define _DK_SDU_MIP__BASE__ARCH_MINOR _DK_SDU_MIP__BASE__UNSPECIFIED
+
+#define _DK_SDU_MIP__BASE__BYTE_ORDER _DK_SDU_MIP__BASE__BIG_ENDIAN
+#define _DK_SDU_MIP__BASE__FLOAT_WORD_ORDER _DK_SDU_MIP__BASE__BIG_ENDIAN
+
+#define _DK_SDU_MIP__BASE__FLOAT _DK_SDU_MIP__BASE__IEEE_754_SINGLE_PRECISION
+#define _DK_SDU_MIP__BASE__DOUBLE _DK_SDU_MIP__BASE__IEEE_754_DOUBLE_PRECISION
+#define _DK_SDU_MIP__BASE__LONG_DOUBLE _DK_SDU_MIP__BASE__IEEE_QUADRUPLE_PRECISION
+
+
+
+#if defined(_DK_SDU_MIP__BASE__IMPLEMENTATION)
+
+// Select additionally reentrant object
+#define _REENTRANT 1
+
+// Specifies that the Solaris extensions are utilized
+#define __EXTENSIONS__ 1
+
+// Specifies that the UNIX 98 functionality is utilized
+#define _XOPEN_SOURCE 500
+
+#endif

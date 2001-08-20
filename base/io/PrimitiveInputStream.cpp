a9eb0e4bad978c1699c7e7c5e5eddab6333c2131
René Fonseca <rene.m.fonseca@gmail.com>
2001-08-20 11:55:41 +0000

    Now uses byte order functions.

--- base/io/PrimitiveInputStream.cpp
+++ base/io/PrimitiveInputStream.cpp
@@ -12,6 +12,7 @@
  ***************************************************************************/
 
 #include <base/io/PrimitiveInputStream.h>
+#include <base/ByteOrder.h>
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
@@ -20,176 +21,72 @@ PrimitiveInputStream::PrimitiveInputStream(InputStream& in) throw(BindException)
 }
 
 bool PrimitiveInputStream::readBoolean() throw(IOException) {
-  bool value;
-  read((char*)&value, sizeof(value));
-  return value;
+  char value;
+  read((char*)&value, sizeof(value)); // TAG: problem if size > 1
+  return value != 0;
 }
 
 char PrimitiveInputStream::readChar() throw(IOException) {
   char value;
-  read((char*)&value, sizeof(value));
+  read((char*)&value, sizeof(value)); // TAG: problem if size > 1
   return value;
 }
 
 short PrimitiveInputStream::readShortInteger() throw(IOException) {
   short value;
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[2]; // 16 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[1] = buffer[0];
-  p[0] = buffer[1];
-#endif
-  return value;
+  return ByteOrder::fromBigEndian<unsigned short>(value);
 }
 
 unsigned short PrimitiveInputStream::readUnsignedShortInteger() throw(IOException) {
   unsigned short value;
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[2]; // 16 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[1] = buffer[0];
-  p[0] = buffer[1];
-#endif
-  return value;
+  return ByteOrder::fromBigEndian(value);
 }
 
 int PrimitiveInputStream::readInteger() throw(IOException) {
   int value;
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[4]; // 32 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[3] = buffer[0];
-  p[2] = buffer[1];
-  p[1] = buffer[2];
-  p[0] = buffer[3];
-#endif
-  return value;
+  return ByteOrder::fromBigEndian<unsigned int>(value);
 }
 
 unsigned int PrimitiveInputStream::readUnsignedInteger() throw(IOException) {
   unsigned int value;
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[4]; // 32 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[3] = buffer[0];
-  p[2] = buffer[1];
-  p[1] = buffer[2];
-  p[0] = buffer[3];
-#endif
-  return value;
+  return ByteOrder::fromBigEndian(value);
 }
 
 long long PrimitiveInputStream::readLongInteger() throw(IOException) {
   long long value;
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[8]; // 64 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[7] = buffer[0];
-  p[6] = buffer[1];
-  p[5] = buffer[2];
-  p[4] = buffer[3];
-  p[3] = buffer[4];
-  p[2] = buffer[5];
-  p[1] = buffer[6];
-  p[0] = buffer[7];
-#endif
-  return value;
+  return ByteOrder::fromBigEndian<unsigned long long>(value);
 }
 
 unsigned long long PrimitiveInputStream::readUnsignedLongInteger() throw(IOException) {
   unsigned long long value;
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[8]; // 64 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[7] = buffer[0];
-  p[6] = buffer[1];
-  p[5] = buffer[2];
-  p[4] = buffer[3];
-  p[3] = buffer[4];
-  p[2] = buffer[5];
-  p[1] = buffer[6];
-  p[0] = buffer[7];
-#endif
-  return value;
+  return ByteOrder::fromBigEndian<unsigned long long>(value);
 }
 
 float PrimitiveInputStream::readFloat() throw(IOException) {
   float value;
-// float may have different order
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
+  // convert value from IEEE_754_SinglePrecision (big endian) and return
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[4]; // 32 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[3] = buffer[0];
-  p[2] = buffer[1];
-  p[1] = buffer[2];
-  p[0] = buffer[3];
-#endif
-  return value;
+  return value; // TAG: fix this
 }
 
 double PrimitiveInputStream::readDouble() throw(IOException) {
   double value;
-
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
+  // convert value from IEEE_754_DoublePrecision (big endian) and return
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[8]; // 64 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[7] = buffer[0];
-  p[6] = buffer[1];
-  p[5] = buffer[2];
-  p[4] = buffer[3];
-  p[3] = buffer[4];
-  p[2] = buffer[5];
-  p[1] = buffer[6];
-  p[0] = buffer[7];
-#endif
-  return value;
+  return value; // TAG: fix this
 }
 
 long double PrimitiveInputStream::readLongDouble() throw(IOException) {
   long double value;
-
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
+  // convert value from IEEE_QuadruplePrecision (big endian) and return
   read((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[10]; // 80 bits
-  read((char*)&buffer, sizeof(buffer));
-  p[9] = buffer[0];
-  p[8] = buffer[1];
-  p[7] = buffer[2];
-  p[6] = buffer[3];
-  p[5] = buffer[4];
-  p[4] = buffer[5];
-  p[3] = buffer[6];
-  p[2] = buffer[7];
-  p[1] = buffer[8];
-  p[0] = buffer[9];
-#endif
-  return value;
+  return value; // TAG: fix this
 }
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE
--- base/io/PrimitiveOutputStream.cpp
+++ base/io/PrimitiveOutputStream.cpp
@@ -12,6 +12,7 @@
  ***************************************************************************/
 
 #include <base/io/PrimitiveOutputStream.h>
+#include <base/ByteOrder.h>
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
@@ -28,143 +29,48 @@ void PrimitiveOutputStream::writeChar(char value) throw(IOException) {
 }
 
 void PrimitiveOutputStream::writeShortInteger(short value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[2]; // 16 bits
-  buffer[1] = p[0]; // most significant
-  buffer[0] = p[1]; // least significant
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  short temp = ByteOrder::toBigEndian<unsigned short>(value);
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeUnsignedShortInteger(unsigned short value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[2]; // 16 bits
-  buffer[1] = p[0]; // most significant
-  buffer[0] = p[1]; // least significant
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  unsigned short temp = ByteOrder::toBigEndian<unsigned short>(value);
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeInteger(int value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[4]; // 32 bits
-  buffer[3] = p[0]; // most significant
-  buffer[2] = p[1]; // second most significant
-  buffer[1] = p[2]; // second least significant
-  buffer[0] = p[3]; // least significant
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  unsigned short temp = ByteOrder::toBigEndian<unsigned int>(value);
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeUnsignedInteger(unsigned int value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[4]; // 32 bits
-  buffer[3] = p[0]; // most significant
-  buffer[2] = p[1]; // second most significant
-  buffer[1] = p[2]; // second least significant
-  buffer[0] = p[3]; // least significant
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  unsigned int temp = ByteOrder::toBigEndian(value);
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeLongInteger(long long value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[8]; // 64 bits
-  buffer[7] = p[0];
-  buffer[6] = p[1];
-  buffer[5] = p[2];
-  buffer[4] = p[3];
-  buffer[3] = p[4];
-  buffer[2] = p[5];
-  buffer[1] = p[6];
-  buffer[0] = p[7];
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  unsigned long long temp = ByteOrder::toBigEndian<unsigned long long>(value);
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeUnsignedLongInteger(unsigned long long value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[8]; // 64 bits
-  buffer[7] = p[0];
-  buffer[6] = p[1];
-  buffer[5] = p[2];
-  buffer[4] = p[3];
-  buffer[3] = p[4];
-  buffer[2] = p[5];
-  buffer[1] = p[6];
-  buffer[0] = p[7];
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  unsigned short temp = ByteOrder::toBigEndian(value);
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeFloat(float value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[4]; // 32 bits
-  buffer[3] = p[0];
-  buffer[2] = p[1];
-  buffer[1] = p[2];
-  buffer[0] = p[3];
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  float temp; // convert to IEEE_FloatPrecision (big endian)
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeDouble(double value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[8]; // 64 bits
-  buffer[7] = p[0];
-  buffer[6] = p[1];
-  buffer[5] = p[2];
-  buffer[4] = p[3];
-  buffer[3] = p[4];
-  buffer[2] = p[5];
-  buffer[1] = p[6];
-  buffer[0] = p[7];
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  double temp; // convert to IEEE_DoublePrecision (big endian)
+  write((char*)&temp, sizeof(temp));
 }
 
 void PrimitiveOutputStream::writeLongDouble(long double value) throw(IOException) {
-#if defined(_DK_SDU_MIP__BASE__BIG_ENDIAN)
-  write((char*)&value, sizeof(value));
-#else
-  char* p = (char*)&value;
-  char buffer[10]; // 80 bits
-  buffer[9] = p[0];
-  buffer[8] = p[1];
-  buffer[7] = p[2];
-  buffer[6] = p[3];
-  buffer[5] = p[4];
-  buffer[4] = p[5];
-  buffer[3] = p[6];
-  buffer[2] = p[7];
-  buffer[1] = p[8];
-  buffer[0] = p[9];
-  write((char*)&buffer, sizeof(buffer));
-#endif
+  long double temp; // convert to IEEE_QuadruplePrecision (big endian)
+  write((char*)&temp, sizeof(temp));
 }
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

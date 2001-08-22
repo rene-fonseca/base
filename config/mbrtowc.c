7a11b941c0d370d2d9781ee388b8203694a41478
René Fonseca <rene.m.fonseca@gmail.com>
2001-08-22 18:20:57 +0000

    Added test for mbrtowc.

---
+++ config/mbrtowc.c
@@ -0,0 +1,28 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+/*
+  Links if mbrtowc is available.
+*/
+
+#include <wchar.h>
+
+int main() {
+  wchar_t dest[256];
+  const char* src = "My string";
+  mbstate_t state;
+
+  size_t result = mbrtowc(dest, &src, 256, &state);
+
+  return 0;
+}

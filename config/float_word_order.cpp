d4b6e6d71e1328a0d34565adce167b7c4eea082e
René Fonseca <rene.m.fonseca@gmail.com>
2001-08-25 00:23:46 +0000

    Improved byte/word order tests.

--- config/float_word_order.cpp
+++ config/float_word_order.cpp
@@ -12,14 +12,16 @@
  ***************************************************************************/
 
 /*
-  Returns 1 if the word order is big endian otherwise 0 (little endian).
+  This program returns _DK_SDU_MIP__BASE__BIG_ENDIAN if the word order of the
+  architecture is big endian, _DK_SDU_MIP__BASE__LITTLE_ENDIAN if the word order
+  is little endian, and _DK_SDU_MIP__BASE__UNSPECIFIED if neither.
 */
 
 #include "floating.h"
 
 int main() {
-  // TAG: should check float, double, and long double
+  // TAG: should check long long (if double word), float, double, and long double?
   int floatWordOrder;
   checkFloat<long double>(floatWordOrder);
-  return (floatWordOrder == _DK_SDU_MIP__BASE__BIG_ENDIAN) ? 0 : 1;
+  return floatWordOrder;
 }

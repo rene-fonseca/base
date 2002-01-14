d753c577dcbe21921009de803525da6438e1e501
René Fonseca <rene.m.fonseca@gmail.com>
2002-01-14 13:28:18 +0000

    Added check for restrict qualifier.

---
+++ config/restrict.cpp
@@ -0,0 +1,34 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by René Møller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+/*
+  This program compiles if the compiler supports the restrict keyword.
+*/
+
+class MyClass {
+private:
+
+  char value;
+public:
+
+  void myFunction(char* restrict p, char* restirct q) {
+  }
+};
+
+int main() {
+  MyClass myClass;
+  char* restrict p = 0;
+  char* restrict q = 0;
+  myClass.myFunction(p, q);
+  return 0;
+}

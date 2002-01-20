87732a85d97828df0d8fc8001e2cea7cea24ba48
René Fonseca <rene.m.fonseca@gmail.com>
2002-01-20 21:31:33 +0000

    Added exception.

---
+++ base/UnexpectedFailure.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__UNEXPECTED_FAILURE_H
+#define _DK_SDU_MIP__BASE__UNEXPECTED_FAILURE_H
+
+#include <base/Exception.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Exception thrown on unexpected errors (this is an internal error).
+
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class UnexpectedFailure : public Exception {
+public:
+
+  /**
+    Initializes the exception object with no message.
+  */
+  UnexpectedFailure() throw();
+
+  /**
+    Initializes the exception object.
+
+    @param message The message.
+  */
+  UnexpectedFailure(const char* message) throw();
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

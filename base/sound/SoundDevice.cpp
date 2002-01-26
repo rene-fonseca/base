2932cef3bdec06b64686b601aa78f2c3bd5c6772
René Fonseca <rene.m.fonseca@gmail.com>
2002-01-26 19:59:41 +0000

    Added sound device wrapper.

---
+++ base/sound/SoundDevice.cpp
@@ -0,0 +1,108 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/sound/SoundInputStream.h>
+#include <base/UnexpectedFailure.h>
+#include <base/io/IOException.h>
+#include <base/sound/SoundDevice.h>
+
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  #define NO_STRICT
+  // I don't get it: in STRICT mode handles are of type int but in NO_STRICT
+  // mode the handles are of size void*. This is a problem on 64 bit platforms
+  // where int and void* may be of different sizes.
+  #include <windows.h>
+#elif (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__UNIX)
+  #include <sys/types.h> // open
+  #include <sys/stat.h> // open
+  #include <fcntl.h> // open
+  #include <unistd.h> // read
+  #include <errno.h> // errno
+  #include <limits.h> // SSIZE_MAX
+
+  #if (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__LINUX)
+    #include <sys/ioctl.h> // ioctl
+    #include <sys/soundcard.h> // ioctl
+  #elif (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__SOLARIS)
+    #include <stropts.h> // ioctl
+    #include <sys/conf.h> // ioctl
+    #include <sys/audio.h>
+    #include <sys/audioio.h>
+  #endif // os
+#endif // flavour
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+SoundDevice SoundDevice::soundDevice;
+
+SoundDevice::SoundDevice() throw() : access(0) {
+}
+
+void SoundDevice::reacquireAccess(unsigned int access) throw(NotSupported) {
+  // guarded externally
+  if (this->access != access) {
+    this->access = access;
+    deviceDescriptor.close(); // must close before reopening
+    if (access != 0) { // only open if required
+      int flags;
+      switch (access) {
+      case READ:
+        flags = O_RDONLY;
+        break;
+      case WRITE:
+        flags = O_WRONLY;
+        break;
+      case READ|WRITE:
+        flags = O_RDWR;
+        break;
+      }
+      OperatingSystem::Handle handle = ::open("/dev/audio", flags); // linux and solaris
+      assert(handle != OperatingSystem::INVALID_HANDLE, NotSupported());
+      deviceDescriptor.setHandle(handle);
+    }
+  }
+}
+
+void SoundDevice::acquireReadAccess() throw(IOException) {
+  exclusiveLock();
+  if (!isReadable()) {
+    reacquireAccess(access | READ);
+  }
+  releaseLock();
+}
+
+void SoundDevice::relinquishReadAccess() throw(IOException) {
+  exclusiveLock();
+  if (isReadable()) {
+    reacquireAccess(access & ~READ);
+  }
+  releaseLock();
+}
+
+void SoundDevice::acquireWriteAccess() throw(IOException) {
+  exclusiveLock();
+  if (!isWriteable()) {
+    reacquireAccess(access | WRITE);
+  }
+  releaseLock();
+}
+
+void SoundDevice::relinquishWriteAccess() throw(IOException) {
+  exclusiveLock();
+  if (isWriteable()) {
+    reacquireAccess(access & ~WRITE);
+  }
+  releaseLock();
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/sound/SoundDevice.h
@@ -0,0 +1,107 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_SOUND__SOUND_DEVICE_H
+#define _DK_SDU_MIP__BASE_SOUND__SOUND_DEVICE_H
+
+#include <base/Object.h>
+#include <base/OperatingSystem.h>
+#include <base/NotSupported.h>
+#include <base/concurrency/ReadWriteLock.h>
+#include <base/io/FileDescriptor.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Sound device (singleton).
+
+  @see SoundInputStream SoundOutputStream.
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class SoundDevice : public virtual Object, public virtual ReadWriteLock {
+private:
+
+  friend class SoundInputStream;
+  friend class SoundOutputStream;
+
+  /** The sound device handle. */
+  static SoundDevice soundDevice;
+
+  enum Access {READ = 1, WRITE = 2};
+
+  /** Handle to the sound device. */
+  FileDescriptor deviceDescriptor;
+  /** The current access to the device.*/
+  unsigned int access;
+
+  /**
+    Acquires the desired access to the device.
+  */
+  void reacquireAccess(unsigned int access) throw(NotSupported);
+public:
+
+  /**
+    Initializes the sound device.
+  */
+  SoundDevice() throw();
+
+  /**
+    Returns true if the device is readable.
+  */
+  inline bool isReadable() const throw() {
+    return (access & READ) != 0;
+  }
+
+  /**
+    Returns true if the device is writeable.
+  */
+  inline bool isWriteable() const throw() {
+    return (access & WRITE) != 0;
+  }
+
+  /**
+    Acquires read access to the device.
+  */
+  void acquireReadAccess() throw(IOException);
+
+  /**
+    Releases the read access to the device.
+  */
+  void relinquishReadAccess() throw(IOException);
+
+  /**
+    Acquires write access to the device.
+  */
+  void acquireWriteAccess() throw(IOException);
+
+  /**
+    Releases the write access to the device.
+  */
+  void relinquishWriteAccess() throw(IOException);
+
+  /**
+    Returns the handle associated with read access from device (may be identical to the write handle).
+  */
+  inline OperatingSystem::Handle getReadHandle() const throw() {return deviceDescriptor.getHandle();}
+
+  /**
+    Returns the handle associated with write access to device (may be identical to the read handle).
+  */
+  inline OperatingSystem::Handle getWriteHandle() const throw() {return deviceDescriptor.getHandle();}
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

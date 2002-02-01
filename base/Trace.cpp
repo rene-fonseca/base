82f505489ca63079aa7eac352e59dee01b937274
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-01 14:42:35 +0000

    Switched to OutputDebugString for WIN32.

--- base/Trace.cpp
+++ base/Trace.cpp
@@ -12,16 +12,34 @@
  ***************************************************************************/
 
 #include <base/Trace.h>
-#include <stdio.h>
+
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  #include <windows.h>
+  #include <string.h>
+  #include <stdio.h>
+#else // unix
+  #include <stdio.h>
+#endif // flavour
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
 void Trace::message(const char* message) {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  OutputDebugString(message);
+#else // unix
   fprintf(stderr, "TRACE %s\n", message); // fprintf must be MT-safe
+#endif // flavour
 }
 
 void Trace::member(const void* ptr, const char* message) {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  unsigned int length = strlen(message);
+  char buffer[sizeof("0x12345678 >> ") + length];
+  sprintf(buffer, "%08x >> %s", ptr, message); // sprintf must be MT-safe
+  OutputDebugString(buffer);
+#else // unix
   fprintf(stderr, "TRACE %08x >> %s\n", ptr, message); // fprintf must be MT-safe
+#endif // flavour
 }
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

0e0e09a39c5a5f91002a252ab13142b0c1fad220
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-03 01:59:47 +0000

    Added wrappers for the context of asynchronous operations.

---
+++ base/io/async/AsynchronousReadContext.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_IO_ASYNC__ASYNCHRONOUS_READ_CONTEXT_H
+#define _DK_SDU_MIP__BASE_IO_ASYNC__ASYNCHRONOUS_READ_CONTEXT_H
+
+#include <base/io/async/AsynchronousReadEventListener.h>
+#include <base/io/async/AsynchronousReadCompletion.h>
+#include <base/io/async/AsynchronousException.h>
+#include <base/mem/ReferenceCountedObjectPointer.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+class AsynchronousReadContext : public Object, public ReferenceCountedObject {
+protected:  
+
+  /** Self-reference used to protect the object from premature destruction. */
+  ReferenceCountedObjectPointer<AsynchronousReadContext> selfReference;
+  
+  inline AsynchronousReadContext() throw() : selfReference(this) {
+  }
+public:
+  
+  virtual AsynchronousReadCompletion getCompletion() const throw() = 0;
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif
---
+++ base/io/async/AsynchronousWriteContext.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_IO_ASYNC__ASYNCHRONOUS_WRITE_CONTEXT_H
+#define _DK_SDU_MIP__BASE_IO_ASYNC__ASYNCHRONOUS_WRITE_CONTEXT_H
+
+#include <base/io/async/AsynchronousWriteEventListener.h>
+#include <base/io/async/AsynchronousIOStream.h>
+#include <base/mem/ReferenceCountedObjectPointer.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+class AsynchronousWriteContext : public Object, public ReferenceCountedObject {
+protected:
+  
+  /** Self-reference used to protect the object from premature destruction. */
+  ReferenceCountedObjectPointer<AsynchronousWriteContext> selfReference;
+
+  inline AsynchronousWriteContext() throw() : selfReference(this) {
+  }
+public:
+
+  virtual AsynchronousWriteCompletion getCompletion() const throw() = 0;
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

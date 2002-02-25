fa193a00d3b78c4cef47f47159c8ca33a517cd6a
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-25 21:33:47 +0000

    Added simple file descriptor handle.

---
+++ base/io/Handle.h
@@ -0,0 +1,81 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_IO__HANDLE_H
+#define _DK_SDU_MIP__BASE_IO__HANDLE_H
+
+#include <base/mem/ReferenceCountedObjectPointer.h>
+#include <base/OperatingSystem.h>
+#include <base/io/IOException.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  A generic descriptor which serves as a handle to a source or sink of bytes
+  within the operatingsystem (e.g. file, socket and pipe). This class is
+  normally not used directly by the application.
+
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class Handle : public virtual ReferenceCountedObject {
+private:
+
+  /** Handle to resource. */
+  OperatingSystem::Handle handle;
+
+  /* Disable the default copy constructor. */
+  Handle(const Handle& copy);
+  /* Disable the default assignment operator. */
+  Handle& operator=(const Handle& eq);
+
+  /** Invalid handle. */
+  static Handle invalid;
+public:
+
+  /**
+    Returns an invalid handle.
+  */
+  static inline Handle* getInvalid() throw() {
+    return &invalid;
+  }
+  
+  /**
+    Initializes an invalid handle.
+  */
+  inline Handle() throw() : handle(OperatingSystem::INVALID_HANDLE) {}
+
+  /**
+    Initializes handle using the specified value.
+  */
+  explicit inline Handle(OperatingSystem::Handle _handle) throw() : handle(_handle) {}
+
+  /**
+    Returns the handle.
+  */
+  inline OperatingSystem::Handle getHandle() const throw() {return handle;}
+
+  /**
+    Returns true if the handle is valid.
+  */
+  inline bool isValid() const throw() {
+    return handle != OperatingSystem::INVALID_HANDLE;
+  }
+  
+  virtual ~Handle() throw(IOException);
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

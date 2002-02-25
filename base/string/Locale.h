2ae2cc0aaa79b0170079bd236e8298d9adfb4e36
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-25 21:22:00 +0000

    Preliminary changes.

--- base/string/Locale.h
+++ base/string/Locale.h
@@ -19,55 +19,128 @@
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
-/*
-  POSIX:
-    beginNegative = "-"
-    beginPositive = "+"
-    endNegative = ""
-    endPositive = ""
-    radix = "."
-    grouping = ""
-*/
-
-class NumericFormatSet {
+class NumericFormatSet : public Object {
 private:
-  char* decimalPoint;
-  char* thousandsseparator;
-  char* grouping;
+  
+  unsigned int groupingDigits; /**< The number of digits in a group (0 ~ disables grouping). */
+  char groupingSymbol; /**< The grouping symbol. */
+  char decimalSymbol; /**< Decimal point symbol. */
 public:
+
+  NumericFormatSet() throw() {
+    groupingDigits = 0;
+    groupingSymbol = ',';
+    decimalSymbol = '.';
+  }
+  
 };
 
-class MonetaryFormatSet {
+class MonetaryFormatSet : public Object {
 public:
 
-  typedef enum {PARENTHESES, PRECEDES_QUANTITY, SUCCEEDS_QUANTITY, PRECEDES_SYMBOL, SUCCEEDS_SYMBOL} SignPosition;
+  /** Relative currency positions. */
+  enum CurrencyPosition {
+    PRECEDES_QUANTITY, /**< Currency symbol immediately precedes the quantity (i.e. symbol and quantity are not separated). */
+    SUCCEEDS_QUANTITY, /**< Currency symbol immediately succeeds the quantity (i.e. symbol and quantity are not separated). */
+    PRECEDES_QUANTITY_WITH_SPACE, /**< Currency symbol precedes the quantity but is separated with a space. */
+    SUCCEEDS_QUANTITY_WITH_SPACE, /**< Currency symbol succeeds the quantity but is separated with a space. */
+  };
+
+  /** Relative sign positions. */
+  enum SignPosition {
+    PARENTHESES, /**< Quantity is contained within parentheses. */
+    SIGN_PRECEDES_QUANTITY, /**< Sign precedes quantity and currency symbol. */
+    SIGN_SUCCEEDS_QUANTITY, /**< Sign succeeds quantity and currency symbol. */
+    SIGN_PRECEDES_SYMBOL, /**< Sign immediately precedes the currency symbol. */
+    SIGN_SUCCEEDS_SYMBOL /**< Sign immediately succeeds the currency symbol. */
+  };
 private:
-  char* int_currencySymbol;
-  char* currencySymbol; /* Local currency symbol.  */
-  char* decimalPoint;    /* Decimal point character.  */
-  char* thousandsSeparator;    /* Thousands separator.  */
-  char* grouping;         /* Like `grouping' element (above).  */
-  char* positiveSign;        /* Sign for positive values.  */
-  char* negativeSign;        /* Sign for negative values.  */
-  int int_fractionalDigits;       /* Int'l fractional digits.  */
-  int fractionalDigits;      /* Local fractional digits.  */
-  /* 1 if currency_symbol precedes a positive value, 0 if succeeds.  */
-  bool positiveSymbolPrecedes;
-  /* 1 if a space separates currency_symbol from a positive value.  */
-  bool positiveSymbolSeparated;
-  /* 1 if currency_symbol precedes a negative value, 0 if succeeds.  */
-  bool negativeSymbolPrecedes;
-  /* 1 if a space separates currency_symbol from a negative value.  */
-  bool negativeSymbolSeparated;
-  /* Positive and negative sign positions:
-     0 Parentheses surround the quantity and currency_symbol.
-     1 The sign string precedes the quantity and currency_symbol.
-     2 The sign string succeeds the quantity and currency_symbol.
-     3 The sign string immediately precedes the currency_symbol.
-     4 The sign string immediately succeeds the currency_symbol.  */
-  //SignPosition positiveSign;
-  //SignPosition negativeSign;
+  
+  String currencySymbol; /**< Currency symbol. */
+  unsigned int fractionalDigits; /**< Number of fractional digits. */
+  unsigned int groupingDigits; /**< The number of digits in a group (0 ~ disables grouping). */
+  char groupingSymbol; /**< The grouping symbol. */
+  char decimalSymbol; /**< Decimal point symbol. */
+  struct {
+    /** The position of the currency symbol for negative quantities.*/
+    CurrencyPosition currencyPosition;
+    /** The position of the sign designator for negative quantities.*/
+    SignPosition signPosition;
+  } negative;
+  struct {
+    /** The position of the currency symbol for positive quantities.*/
+    CurrencyPosition currencyPosition;
+    /** The position of the sign designator for positive quantities.*/
+    SignPosition signPosition;
+  } positive;
 public:
+
+  MonetaryFormatSet() throw() {
+    currencySymbol = "";
+    fractionalDigits = 2;
+    groupingDigits = 0;
+    groupingSymbol = ',';
+    decimalSymbol = '.';
+    negative.currencyPosition = PRECEDES_QUANTITY_WITH_SPACE;
+    negative.signPosition = SIGN_SUCCEEDS_SYMBOL;
+    positive.currencyPosition = PRECEDES_QUANTITY_WITH_SPACE;    
+    positive.signPosition = SIGN_SUCCEEDS_SYMBOL;
+  }
+};
+
+class DateFormatSet : public Object {
+private:
+
+  /** The short names of days. */
+  String daysShort[7];
+  /** The long name of days. */
+  String daysLong[7];
+  /** The short names of months. */
+  String monthsShort[12];
+  /** The long name of months. */
+  String monthsLong[12];
+  
+  /** The order of day, month, and year. */
+  enum DateOrder {
+    DMY, /**< day-month-year. */
+    DYM, /**< day-year-month. */
+    MDY, /**< month-day-year. */
+    MYD, /**< month-year-day. */
+    YDM, /**< year-day-month. */
+    YMD /**< year-month-day. */
+  };
+  
+  enum DayFormat {
+    D, /**< One digit day (1-7). */
+    DD, /**< Two digit day (01-07). */
+    DDD, /**< Day is given by its short name. */
+    DDDD /**< Day is given by its long name. */
+  };
+
+  enum MonthFormat {
+    M, /**< Month is given by its number (1-12). */
+    MM, /**< Month given by two digits (01-12). */
+    MMM, /**< Month is given by short name. */
+    MMMM /**< Month is given by its long name. */
+  };
+  
+  enum YearFormat {
+    YY, /**< Two digit year. */
+    YYYY /**< Four digit year. */
+  };
+  
+  enum DateStandardFormat {
+    SHORT_DATE_FORMAT,
+    MIDDLE_DATE_FORMAT,
+    LONG_DATE_FORMAT
+  };
+
+  /** Date separator used with short formats. */
+  char separator;
+public:
+
+  DateFormatSet() throw() {
+  }
 };
 
 /**

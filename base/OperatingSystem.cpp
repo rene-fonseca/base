c6ebe66452f9fd19459322d55d8f2c6eda81591e
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-26 11:59:35 +0000

    Added operating system compatibility check for win32.

--- base/OperatingSystem.cpp
+++ base/OperatingSystem.cpp
@@ -12,10 +12,11 @@
  ***************************************************************************/
 
 #include <base/OperatingSystem.h>
-#include <base/Exception.h>
+#include <base/SystemLogger.h>
 
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   #include <windows.h>
+  #undef ERROR // protect against the evil programmers
 #endif // flavour
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
@@ -23,5 +24,28 @@ _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   ASSERTION(sizeof(OperatingSystem::Handle) == sizeof(HANDLE));
 #endif
+
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+namespace {
+
+  class IsCompatible {
+  public:
+
+    IsCompatible() throw() {
+      OSVERSIONINFO versionInfo;
+      versionInfo.dwOSVersionInfoSize = sizeof(versionInfo);
+      ::GetVersionEx(&versionInfo); // never fails
+      // only Windows NT 4.0 or later
+      bool compatible = (versionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT) && (versionInfo.dwMajorVersion >= 4);
+      if (!compatible) {
+        SystemLogger::write(SystemLogger::ERROR, "Opeating system is not supported.");
+      }
+    }
+    
+  };
+
+  IsCompatible isCompatible();
+};
+#endif // flavour
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

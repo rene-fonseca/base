815b0a85c415f1841a2b354663dcc75c444e5781
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-26 23:20:34 +0000

    Updated comment.

--- base/dl/LinkerException.h
+++ base/dl/LinkerException.h
@@ -19,7 +19,10 @@
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
 /**
-  @short Exception thrown by DynamicLinker.
+  This exception is used by the DynamicLinker class.
+  
+  @short Exception thrown by DynamicLinker
+  @ingroup exceptions
   @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
   @version 1.0
 */

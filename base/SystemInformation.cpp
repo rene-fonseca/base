a4b7d0d648b62542a16c0d30a912dd4c34729e81
René Fonseca <rene.m.fonseca@gmail.com>
2002-02-26 12:02:56 +0000

    Added system information class.

---
+++ base/SystemInformation.cpp
@@ -0,0 +1,92 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/SystemInformation.h>
+#include <base/Exception.h>
+#include <base/SystemLogger.h>
+
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  #include <windows.h>
+#else
+  #include <sys/utsname.h>
+#endif // flavour
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+String SystemInformation::getVendor() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  return MESSAGE("Microsoft");
+#else
+  struct utsname name;
+  int result = ::uname(&name);
+  if (result == 0) {
+    return String(name.sysname);
+    return String(name.nodename);
+  } else {
+    return MESSAGE("Unspecified");
+  }
+#endif
+}
+
+String SystemInformation::getSystem() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  OSVERSIONINFO versionInfo;
+  versionInfo.dwOSVersionInfoSize = sizeof(versionInfo);
+  ::GetVersionEx(&versionInfo); // never fails
+  if ((versionInfo.dwMajorVersion == 4) && (versionInfo.dwMinorVersion == 0)) { // earlier versions are not supported
+    return MESSAGE("Windows NT");
+  } else if ((versionInfo.dwMajorVersion == 5) && (versionInfo.dwMinorVersion == 0)) {
+    return MESSAGE("Windows 2000");
+  } else if ((versionInfo.dwMajorVersion == 5) && (versionInfo.dwMinorVersion == 1)) {
+    return MESSAGE("Windows XP");
+  } else {
+    return MESSAGE("Windows later than XP");
+  }
+#else
+  struct utsname name;
+  int result = ::uname(&name);
+  return String(name.sysname);
+#endif // flavour
+}
+
+String SystemInformation::getRelease() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  return MESSAGE("Unspecified");
+#else
+  struct utsname name;
+  int result = ::uname(&name);
+  return String(name.release);
+#endif
+}
+
+String SystemInformation::getVersion() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  return MESSAGE("Unspecified");
+#else
+  struct utsname name;
+  int result = ::uname(&name);
+  return String(name.version);
+#endif
+}
+
+String SystemInformation::getMachine() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+  return MESSAGE("Unspecified");
+#else
+  struct utsname name;
+  int result = ::uname(&name);
+  return String(name.machine);
+#endif
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/SystemInformation.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__SYSTEM_INFORMATION_H
+#define _DK_SDU_MIP__BASE__SYSTEM_INFORMATION_H
+
+#include <base/Object.h>
+#include <base/string/String.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  System information.
+
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class SystemInformation : public Object {
+public:
+  
+  /**
+    Returns the vendor name of the operating system.
+  */
+  static String getVendor() throw();
+
+  /**
+    Returns a description of the particular operating system.
+  */
+  static String getSystem() throw();
+
+  static String getRelease() throw();
+
+  static String getVersion() throw();
+
+  static String getMachine() throw();
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

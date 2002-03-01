32efc31191d3f1823ae50127f5b245bae53c49a5
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-01 18:48:05 +0000

    Now uses DynamicMemory class.

--- base/mem/Allocator.h
+++ base/mem/Allocator.h
@@ -14,11 +14,11 @@
 #ifndef _DK_SDU_MIP__BASE_MEM__ALLOCATOR_H
 #define _DK_SDU_MIP__BASE_MEM__ALLOCATOR_H
 
+#include <base/mem/DynamicMemory.h>
 #include <base/Functor.h>
 #include <base/mem/Heap.h>
 #include <base/mem/AllocatorEnumeration.h>
 #include <base/iterator/SequenceIterator.h>
-#include <new>
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 

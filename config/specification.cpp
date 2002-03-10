a90af92def87d231a2f7d3a8fa3f038e9997b0f0
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-10 22:50:58 +0000

    Added test for exception specification support.

---
+++ config/specification.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+/*
+  This program compiles if the compiler supports exception specification.
+*/
+
+class MyException {
+public:
+
+  MyException() throw() {
+  }
+};
+
+class MyClass {
+public:
+
+  MyClass() throw() {
+  }
+  
+  static void myStaticMethod() throw(MyException) {
+    throw MyException();
+  }
+
+  void myMethod() throw(MyException) {
+    throw MyException();
+  }
+};
+
+int main() {
+  MyClass object;
+  try {
+    object.myMethod();
+  } catch (MyException& e) {
+  }
+  return 0;
+}

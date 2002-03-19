5c7b33420fdee77263f7a06a9aad3fd4f701cff1
René Fonseca <rene.m.fonseca@gmail.com>
2002-03-19 18:11:05 +0000

    Implementation of temporary folder and file interface.

--- base/filesystem/FileSystem.cpp
+++ base/filesystem/FileSystem.cpp
@@ -13,6 +13,15 @@
 
 #include <base/filesystem/FileSystem.h>
 #include <base/concurrency/Thread.h>
+#include <base/NotImplemented.h>
+#include <base/Type.h>
+#include <base/collection/Map.h>
+#include <base/Application.h>
+#include <base/string/StringOutputStream.h>
+#include <base/security/User.h>
+#include <base/concurrency/Process.h>
+#include <base/net/InetAddress.h>
+#include <base/mathematics/Random.h>
 
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   #include <windows.h>
@@ -42,14 +51,14 @@ String FileSystem::getCurrentFolder() throw(FileSystemException) {
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   Allocator<char>* buffer = Thread::getLocalStorage();
   if (::GetCurrentDirectory(buffer->getSize(), buffer->getElements())) {
-    throw FileSystemException("Unable to get current folder");
+    throw FileSystemException("Unable to get current folder", Type::getType<FileSystem>());
   }
   return String(buffer->getElements());
 #else // unix
   Allocator<char>* buffer = Thread::getLocalStorage();
   ASSERT(buffer->getSize() > PATH_MAX);
   if (::getcwd(buffer->getElements(), buffer->getSize())) {
-    throw FileSystemException("Unable to get current folder");
+    throw FileSystemException("Unable to get current folder", Type::getType<FileSystem>());
   }
   return String(buffer->getElements());
 #endif // flavour
@@ -58,11 +67,11 @@ String FileSystem::getCurrentFolder() throw(FileSystemException) {
 void FileSystem::setCurrentFolder(const String& path) throw(FileSystemException) {
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   if (!::SetCurrentDirectory(path.getElements())) {
-    throw FileSystemException("Unable to set current folder");
+    throw FileSystemException("Unable to set current folder", Type::getType<FileSystem>());
   }
 #else // unix
   if (::chdir(path.getElements())) {
-    throw FileSystemException("Unable to set current folder");
+    throw FileSystemException("Unable to set current folder", Type::getType<FileSystem>());
   }
 #endif // flavour
 }
@@ -80,11 +89,11 @@ bool FileSystem::fileExists(const String& path) throw(FileSystemException) {
       if (S_ISREG(status.st_mode)) {
         return true;
       }
-      throw FileSystemException("Not a regular file");
+      throw FileSystemException("Not a regular file", Type::getType<FileSystem>());
     case ENOENT:
       return false;
     default:
-      throw FileSystemException("Unable to examine if file exists");
+      throw FileSystemException("Unable to examine if file exists", Type::getType<FileSystem>());
     }
   #else
     struct stat status;
@@ -94,11 +103,11 @@ bool FileSystem::fileExists(const String& path) throw(FileSystemException) {
       if (S_ISREG(status.st_mode)) {
         return true;
       }
-      throw FileSystemException("Not a regular file");
+      throw FileSystemException("Not a regular file", Type::getType<FileSystem>());
     case ENOENT:
       return false;
     default:
-      throw FileSystemException("Unable to examine if file exists");
+      throw FileSystemException("Unable to examine if file exists", Type::getType<FileSystem>());
     }
   #endif
 #endif // flavour
@@ -117,11 +126,11 @@ bool FileSystem::folderExists(const String& path) throw(FileSystemException) {
       if (S_ISDIR(status.st_mode)) {
         return true;
       }
-      throw FileSystemException("Not a folder");
+      throw FileSystemException("Not a folder", Type::getType<FileSystem>());
     case ENOENT:
       return false;
     default:
-      throw FileSystemException("Unable to examine if folder exists");
+      throw FileSystemException("Unable to examine if folder exists", Type::getType<FileSystem>());
     }
   #else
     struct stat status;
@@ -131,11 +140,11 @@ bool FileSystem::folderExists(const String& path) throw(FileSystemException) {
       if (S_ISDIR(status.st_mode)) {
         return true;
       }
-      throw FileSystemException("Not a folder");
+      throw FileSystemException("Not a folder", Type::getType<FileSystem>());
     case ENOENT:
       return false;
     default:
-      throw FileSystemException("Unable to examine if folder exists");
+      throw FileSystemException("Unable to examine if folder exists", Type::getType<FileSystem>());
     }
   #endif
 #endif // flavour
@@ -145,11 +154,11 @@ void FileSystem::removeFile(const String& path) throw(FileSystemException) {
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
 //  ::SetFileAttributes(file, FILE_ATTRIBUTE_NORMAL);
   if (!::DeleteFile(path.getElements())) {
-    throw FileSystemException("Unable to remove file");
+    throw FileSystemException("Unable to remove file", Type::getType<FileSystem>());
   }
 #else // unix
   if (unlink(path.getElements())) {
-    throw FileSystemException("Unable to remove file");
+    throw FileSystemException("Unable to remove file", Type::getType<FileSystem>());
   }
 #endif // flavour
 }
@@ -157,11 +166,11 @@ void FileSystem::removeFile(const String& path) throw(FileSystemException) {
 void FileSystem::removeFolder(const String& path) throw(FileSystemException) {
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   if (!::RemoveDirectory(path.getElements())) {
-    throw FileSystemException("Unable to remove folder");
+    throw FileSystemException("Unable to remove folder", Type::getType<FileSystem>());
   }
 #else // unix
   if (rmdir(path.getElements())) {
-    throw FileSystemException("Unable to remove file");
+    throw FileSystemException("Unable to remove file", Type::getType<FileSystem>());
   }
 #endif // flavour
 }
@@ -169,45 +178,120 @@ void FileSystem::removeFolder(const String& path) throw(FileSystemException) {
 void FileSystem::makeFolder(const String& path) throw(FileSystemException) {
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
   if (!::CreateDirectory(path.getElements(), 0)) { // use default security descriptor
-    throw FileSystemException("Unable to make folder");
+    throw FileSystemException("Unable to make folder", Type::getType<FileSystem>());
   }
 #else // unix
   if (mkdir(path.getElements(), 0)) {
-    throw FileSystemException("Unable to make folder");
+    throw FileSystemException("Unable to make folder", Type::getType<FileSystem>());
   }
 #endif // flavour
 }
 
-/*
-String FileSystem::getTempFolder() throw(FileSystemException) {
-//#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
-//  // TAG:  check buffer with ASSERT
-//  Allocator<char> buffer = Thread::getLocalStorage();
-//  unsigned int length = ::GetTempPath(buffer->getSize(), buffer->getBytes());
-//  return String(buffer->getBytes(), length);
-//#else // unix
-//  // get environment variable: TMP
-//  return String("/tmp");
-//#endif // flavour
-}
-
-String FileSystem::getTempFileName() throw(FileException) {
-  // TAG: prefix ~ name of application
-  // TAG: options: use application name, use host name, use user name, use process id, use time?
-  // TAG: options: overwrite file on creation, exclusive?
-  // TAG: options: remove file on object destruction, overwrite file on destruction?
-  // TAG: format of name: app-host-id-time.tmp
-  // TAG: should return file object created with the appropriate options
-
-// String tempFile;
-// StringOutputStream stream(tempFile);
-// stream << User::getUser() << "- " << InetAddress::getHostname() << "-" << Process::getProcess().getId() << "-" << ".tmp";
-
+String FileSystem::getTempFolder(TemporaryFolder folder) throw() {
+  // TAG: need to expand variables (win32) (e.g. set TMP=%HOME%\temp)
+  switch (folder) {
+  case USER_SPECIFIED:
+    {
+      const Map<String, String> environment = Application::getApplication()->getEnvironment();
+      if (environment.isKey(MESSAGE("TMP"))) {
+        return environment[MESSAGE("TMP")];
+      } else if (environment.isKey(MESSAGE("TEMP"))) {
+        return environment[MESSAGE("TEMP")];
+      }
+    }
+  case MACHINE_NONPERSISTENT:
 #if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
-  ::GetTempFileName(getTempFolder()->getBytes(), , 0, buffer->getBytes());
+    return MESSAGE("C:\\temp"); // TAG: fixme - use same drive as windows directory
 #else // unix
+    return MESSAGE("/tmp");
 #endif // flavour
+  case MACHINE_PERSISTENT:
+#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+    return MESSAGE("C:\\temp"); // TAG: fixme - use same drive as windows directory
+#else // unix
+    return MESSAGE("/var/tmp");
+#endif // flavour
+  }
 }
+
+String FileSystem::getTempFileName(unsigned int options) throw() {
+  bool first = true;
+  StringOutputStream stream;
+  if (options & PREFIX) {
+    stream << '~';
+  }
+  if (options & USER) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    stream << User::getCurrentUser().getName();
+  }
+  if (options & APPLICATION) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    stream << Application::getApplication()->getFormalName();
+  }
+  if (options & PROCESS) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    stream << Process::getProcess().getId();
+  }
+  if (options & HOST) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    stream << InetAddress::getLocalHost();
+  }
+  if (options & COUNTER) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    stream << HEX << ZEROPAD << counter++;
+  }
+  if (options & TIME) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    stream << HEX << ZEROPAD << Date();
+  }
+  if (options & RANDOM) {
+    if (!first) {
+      stream << '-';
+    }
+    first = false;
+    unsigned int random = Random::getInteger();
+    stream << HEX << ZEROPAD << random;
+  }
+  if (options & SUFFIX) {
+    stream << MESSAGE(".tmp");
+  }
+  stream << FLUSH;
+  return stream.getString();
+}
+
+File FileSystem::getTempFile(unsigned int options) throw(IOException) {
+  throw NotImplemented(Type::getType<FileSystem>());
+/*
+  unsigned int attempts = 16;
+  while (attempts--) {
+    // change name policy if to many attempts
+    String path = FileSystem::combine(getTempFolder(), getTempFileName());
+    File file;
+    try {
+      file = File(path, opts);
+      return file;
+    } catch (FileNotFound& e) {
+    }
+  }
 */
+}
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

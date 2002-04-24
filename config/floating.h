b69c315cf7be0530007787ad3c23e2e9e550240c
René Fonseca <rene.m.fonseca@gmail.com>
2002-04-24 16:03:42 +0000

    Updated copyright notice.

--- config/floating.h
+++ config/floating.h
@@ -2,7 +2,7 @@
     The Base Framework
     A framework for developing platform independent applications
 
-    Copyright (C) 2001 by René Møller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001-2002 by René Møller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of

f1ea29264d32397a3131e2e3c062d621bc72a314
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-12 17:16:57 +0000

    Updated copyright.

--- testsuite/Queue.cpp
+++ testsuite/Queue.cpp
@@ -2,7 +2,7 @@
     The Base Framework (Test Suite)
     A framework for developing platform independent applications
 
-    Copyright (C) 2001 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2001-2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of

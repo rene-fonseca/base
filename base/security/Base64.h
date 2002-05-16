afbd36ad2bd38f1a7d17ab4d5b3bac4f90bef5c8
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-16 21:08:33 +0000

    Optimization and added format output operator.

--- base/security/Base64.h
+++ base/security/Base64.h
@@ -16,37 +16,85 @@
 
 #include <base/Object.h>
 #include <base/string/String.h>
+#include <base/string/FormatOutputStream.h>
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
 /**
-  Base64 encoding (see RFC 2045).
-  
+  Base64 encoding (see RFC 2045 section 6.8).
+
+  @ingroup security
   @short Base64 encoding support.
   @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
-  @version 1.0
+  @version 1.1
 */
 
 class Base64 : public Object {
 public:
 
-  /** Digits of the Base64 encoding. */
-  static const char DIGITS[];
-  /** The special character used to denote unused digits. */
-  static const char UNUSED = '=';
+  /**
+    Base64 encode descriptor.
+    
+    @short Base64 encode descriptor.
+    @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    @version 1.0
+  */
+  class Descriptor : public Object {
+  private:
 
+    /** The bytes to be encoded. */
+    const char* buffer;
+    /** The size of the buffer. */
+    unsigned int size;
+  public:
+
+    /**
+      Initializes descriptor.
+      
+      @param buffer The bytes to be encoded.
+      @param size The size of the buffer.
+    */
+    Descriptor(const char* buffer, unsigned int size) throw();
+    
+    /**
+      Returns the size of the buffer.
+    */
+    inline unsigned int getSize() const throw() {
+      return size;
+    }
+    
+    
+    /**
+      Returns the bytes to be encoded.
+    */
+    inline const char* getBytes() const throw() {
+      return buffer;
+    }
+  };
+  
+  /** Lookup table for mapping digits into values. */
+  static const char VALUES[256];
+  /** Digits of the Base64 encoding. */
+  static const char DIGITS[64];
+  /** The special character used to denote unused digits. */
+  static const char PAD = '=';
+  
   /**
     Returns the digit for the specified value. The value returned is undefined
     if the value is not in the range [0;63].
   */
-  static inline char valueToDigit(unsigned int value) throw() {return DIGITS[value & 0x3f];}
-
+  static inline char valueToDigit(unsigned int value) throw() {
+    return DIGITS[value & 0x3f];
+  }
+  
   /**
     Returns the value [0;63] of the specified digit.
 
     @return -1 if not a valid Base64 digit.
   */
-  static int valueToDigit(char digit) throw();
+  static inline int valueToDigit(char digit) throw() {
+    return VALUES[static_cast<unsigned char>(digit)];
+  }
 
   /**
     Returns the Base64 encoded data.
@@ -56,6 +104,15 @@ public:
   */
   static String encode(const byte* buffer, unsigned int size) throw();
 };
+
+inline Base64::Descriptor::Descriptor(const char* _buffer, unsigned int _size) throw()
+  : buffer(_buffer), size(_size) {
+}
+
+/**
+  Writes the buffer to the format output stream as described in RFC 2045.
+*/
+FormatOutputStream& operator<<(FormatOutputStream& stream, const Base64::Descriptor& value) throw(IOException);
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE
 

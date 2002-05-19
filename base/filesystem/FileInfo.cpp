f25a6a815f2bc77a73890165f8bc9987205f60db
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-19 02:09:15 +0000

    Implementation of security support.

--- base/filesystem/FileInfo.cpp
+++ base/filesystem/FileInfo.cpp
@@ -27,7 +27,7 @@
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
-FileInfo::FileInfo(const String& _path) throw(FileSystemException) : path(_path) {
+FileInfo::FileInfo(const String& _path) throw(FileSystemException) : path(_path), mode(0) {
 #if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
   bool error = false;
   HANDLE file = ::CreateFile(path.getElements(), // file name
@@ -104,31 +104,164 @@ FileInfo::FileInfo(const String& _path) throw(FileSystemException) : path(_path)
   SECURITY_DESCRIPTOR* securityDescriptor;
   PSID ownerSID;
   PSID groupSID;
+  PACL acl;
   error |= ::GetSecurityInfo(file,
                              SE_FILE_OBJECT,
-                             OWNER_SECURITY_INFORMATION|GROUP_SECURITY_INFORMATION,
+                             OWNER_SECURITY_INFORMATION|GROUP_SECURITY_INFORMATION|DACL_SECURITY_INFORMATION,
                              &ownerSID,
                              &groupSID,
-                             0,
+                             &acl,
                              0,
                              &securityDescriptor) != ERROR_SUCCESS;
   ::CloseHandle(file);
-  if (!error) {
-    owner = User(ownerSID);
-    group = Group(groupSID);
-    ::LocalFree(securityDescriptor);
-  }
-  
-  static const long long fileTimeOffset = 116444736000000000ULL;
+  assert(!error, FileSystemException("Not a file", this));
+
+  owner = User((const void*)ownerSID);
+  group = Group((const void*)groupSID);
+  ::LocalFree(securityDescriptor);
+
   ULARGE_INTEGER temp;
   temp.LowPart = information.nFileSizeLow;
   temp.HighPart = information.nFileSizeHigh;
   size = temp.QuadPart;
-  mode = PrimitiveTraits<unsigned int>::MAXIMUM;
+  const long long fileTimeOffset = 116444736000000000ULL;
   access = (*pointer_cast<const long long*>(&information.ftLastAccessTime) - fileTimeOffset)/10000000;
   modification = (*pointer_cast<const long long*>(&information.ftLastWriteTime) - fileTimeOffset)/10000000;
   change = (*pointer_cast<const long long*>(&information.ftCreationTime) - fileTimeOffset)/10000000;
   links = information.nNumberOfLinks;
+  
+// #if 0 // disabled
+//   SECURITY_DESCRIPTOR_CONTROL* control;
+//   DWORD revision;
+// 	if (::GetSecurityDescriptorControl(securityDescriptor, &control, &revision)) {
+// 		throw FileSystemException("Unable to get secirity descriptor control", this);
+// 	}
+//   ::GetSecurityDescriptorLength(securityDescriptor);
+  
+//   fout << '{' << length << ' ' << revision << HEX << (unsigned int)control << '}';
+  
+//   ULONG numberOfEntries;
+//   PEXPLICIT_ACCESS entries;
+//   error |= ::GetExplicitEntriesFromAcl(acl, &numberOfEntries, &entries) != ERROR_SUCCESS;
+//   if (error) {
+//     ::LocalFree(securityDescriptor);
+//     throw FileSystemException("Unable to get ACL", this);
+//   }
+
+// //   ::GetSecurityDescriptorOwner();
+// //   ::GetSecurityDescriptorGroup();
+
+// 	if (::GetSecurityDescriptorDacl( psd, &daclPresent, &dacl, &daclDefaulted) == 0) {
+//     throw FileSystemException("Unable to get DACL", this);
+// 	}
+
+//   ACL_SIZE_INFORMATION information;
+//   if (::GetAclInformation(acl, &information, sizeof(information), AclSizeInformation) == 0) {
+//     throw FileSystemException("Unable to get ACL information", this);
+// 	}
+
+//   fout << '{' << aci.AceCount << ' ' << aci.AclBytesInUse << ' ' << aci.AclBytesFree << '}';
+  
+// 	for (unsigned int i = 0; i < aci.AceCount; ++i) {
+//     ACE_HEADER* ace;
+//     ::GetAce(acl, i, (void**)&ace);
+//     fout << "ace{entry=" << i " acetype=" << ace->AceType << ' ' << '}';
+    
+
+// 	printf( "%sACE type: %s (%lu)\n",type, (DWORD) ace->AceType );
+
+// //	  User(psid).getName();
+
+//   switch (ace->AceType) {
+//   case ACCESS_ALLOWED_ACE_TYPE:
+//     type = "ACCESS_ALLOWED_ACE_TYPE";
+//     psid = &((ACCESS_ALLOWED_ACE*)ace)->SidStart;
+//     break;
+//   case ACCESS_DENIED_ACE_TYPE:
+//     type = "ACCESS_DENIED_ACE_TYPE";
+//     psid = &((ACCESS_DENIED_ACE*)ace)->SidStart;
+//     break;
+//   default:
+//     Trace::message("default");
+//   }
+// #endif // disable
+
+
+  
+// 		case ACCESS_ALLOWED_OBJECT_ACE_TYPE:
+// 			type = "ACCESS_ALLOWED_OBJECT_ACE_TYPE";
+// 			psid = &( (ACCESS_ALLOWED_OBJECT_ACE *) ace )->SidStart;
+// 			break;
+// 		case ACCESS_DENIED_OBJECT_ACE_TYPE:
+// 			type = "ACCESS_DENIED_OBJECT_ACE_TYPE";
+// 			psid = &( (ACCESS_DENIED_OBJECT_ACE *) ace )->SidStart;
+// 			break;
+//       }
+  
+//   PEXPLICIT_ACCESS entry = entries;
+//   for (int i = 0; i < numberOfEntries; ++i) {
+//     fout << "{" << i << ' ' << "permissions:" << HEX << entry->grfAccessPermissions << ' '
+//          << "access mode:" << entry->grfAccessMode << ' '
+//          << "inherit:" << HEX << entry->grfInheritance << ' '
+//          << "form:" << entry->Trustee.TrusteeForm << ' '
+//          << "type:" << entry->Trustee.TrusteeType << '}';
+//     ++entry;
+//   }
+//   ::LocalFree(entry);
+  
+  ACCESS_MASK accessMask = 0;
+  TRUSTEE trustee;
+  trustee.pMultipleTrustee = 0;
+  trustee.MultipleTrusteeOperation = NO_MULTIPLE_TRUSTEE;
+  trustee.TrusteeForm = TRUSTEE_IS_SID;
+  trustee.TrusteeType = TRUSTEE_IS_UNKNOWN;
+  
+  // trustee.ptstrName = (char*)ownerSID;
+  ::BuildTrusteeWithSid(&trustee, ownerSID);
+  ::GetEffectiveRightsFromAcl(acl, &trustee, &accessMask); // ERROR_SUCCESS
+  if (accessMask & FILE_READ_DATA == FILE_READ_DATA) { // mode reset above
+    mode |= FileInfo::RUSR;
+  }
+  if (accessMask & (FILE_WRITE_DATA|FILE_APPEND_DATA) == (FILE_WRITE_DATA|FILE_APPEND_DATA)) {
+    mode |= FileInfo::WUSR;
+  }
+  if (accessMask & FILE_EXECUTE == FILE_EXECUTE) {
+    mode |= FileInfo::XUSR;
+  }
+  
+//  trustee.ptstrName = (char*)groupSID;
+  ::BuildTrusteeWithSid(&trustee, groupSID);
+  ::GetEffectiveRightsFromAcl(acl, &trustee, &accessMask); // ERROR_SUCCESS
+  if (accessMask & FILE_READ_DATA == FILE_READ_DATA) {
+    mode |= FileInfo::RGRP;
+  }
+  if (accessMask & (FILE_WRITE_DATA|FILE_APPEND_DATA) == (FILE_WRITE_DATA|FILE_APPEND_DATA)) {
+    mode |= FileInfo::WGRP;
+  }
+  if (accessMask & FILE_EXECUTE == FILE_EXECUTE) {
+    mode |= FileInfo::XGRP;
+  }
+
+  static const char EVERYONE_SID[1 + 1 + 6 + 4] = {
+    1, // revision
+    1, // number of sub authorities
+    0, 0, 0, 0, 0, 1, // SECURITY_WORLD_SID_AUTHORITY
+    0, 0, 0, 0 // SECURITY_WORLD_RID
+  };
+  
+//   //trustee.ptstrName = (char*)&EVERYONE_SID;
+  ::BuildTrusteeWithSid(&trustee, (void*)&EVERYONE_SID);
+  ::GetEffectiveRightsFromAcl(acl, &trustee, &accessMask); // ERROR_SUCCESS
+  if (accessMask & FILE_READ_DATA == FILE_READ_DATA) {
+    mode |= FileInfo::ROTH;
+  }
+  if (accessMask & (FILE_WRITE_DATA|FILE_APPEND_DATA) == (FILE_WRITE_DATA|FILE_APPEND_DATA)) {
+    mode |= FileInfo::WOTH;
+  }
+  if (accessMask & FILE_EXECUTE == FILE_EXECUTE) {
+    mode |= FileInfo::XOTH;
+  }
+
 #else // unix
   #if defined(_DK_SDU_MIP__BASE__LARGE_FILE_SYSTEM)
   #if (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__GNULINUX)
@@ -157,19 +290,47 @@ FileInfo::FileInfo(const String& _path) throw(FileSystemException) : path(_path)
   #else
     struct stat64 status;
   #endif // GNU Linux
-    if (stat64(path.getElements(), (struct stat64*)&status) || (!S_ISREG(status.st_mode))) {
+    if (::stat64(path.getElements(), (struct stat64*)&status) || (!S_ISREG(status.st_mode))) {
       throw FileSystemException("Not a file", this);
     }
   #else
     struct stat status;
-    if (stat(path.getElements(), &status) || (!S_ISREG(status.st_mode))) {
+    if (::stat(path.getElements(), &status) || (!S_ISREG(status.st_mode))) {
       throw FileSystemException("Not a file", this);
     }
   #endif
+    
+  if (status.st_mode & S_IRUSR) { // mode reset above
+    mode |= FileInfo::RUSR;
+  }
+  if (status.st_mode & S_IWUSR) {
+    mode |= FileInfo::WUSR;
+  }
+  if (status.st_mode & S_IXUSR) {
+    mode |= FileInfo::XUSR;
+  }
+  if (status.st_mode & S_IRGRP) {
+    mode |= FileInfo::RGRP;
+  }
+  if (status.st_mode & S_IWGRP) {
+    mode |= FileInfo::WGRP;
+  }
+  if (status.st_mode & S_IXGRP) {
+    mode |= FileInfo::XGRP;
+  }
+  if (status.st_mode & S_IROTH) {
+    mode |= FileInfo::ROTH;
+  }
+  if (status.st_mode & S_IWOTH) {
+    mode |= FileInfo::WOTH;
+  }
+  if (status.st_mode & S_IXOTH) {
+    mode |= FileInfo::XOTH;
+  }
+  
   size = status.st_size;
-  mode = status.st_mode;
-  owner = User((const void*)status.st_uid);
-  group = Group((const void*)status.st_gid);
+  owner = User((const void*)(ptrdiff_t)status.st_uid);
+  group = Group((const void*)(ptrdiff_t)status.st_gid);
   access = status.st_atime;
   modification = status.st_mtime;
   change = status.st_ctime;

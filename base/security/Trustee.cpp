a8759c9cddf3464c3285178b6e9a4be284942cf1
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-19 02:13:11 +0000

    Added trustee.

---
+++ base/security/Trustee.cpp
@@ -0,0 +1,315 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/platforms/features.h>
+#include <base/security/Trustee.h>
+#include <base/concurrency/Thread.h>
+#include <base/string/StringOutputStream.h>
+#include <base/NotImplemented.h>
+
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  #include <windows.h>
+  #include <aclapi.h>
+#else // unix
+  #include <sys/types.h>
+  #include <grp.h>
+  #include <pwd.h>
+  #include <unistd.h>
+#endif // flavor
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+Trustee::Trustee() throw() : type(UNSPECIFIED), integralId(PrimitiveTraits<unsigned long>::MAXIMUM), id(0) {
+}
+
+Trustee::Trustee(TrusteeType type, const void* _id) throw(OutOfDomain) {
+  if (_id == 0) {
+    type = Trustee::UNSPECIFIED;
+    integralId = PrimitiveTraits<unsigned long>::MAXIMUM;
+    return;
+  }
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  assert(::IsValidSid((PSID)_id) != 0, OutOfDomain("Invalid trustee", this));
+  DWORD size = ::GetLengthSid((PSID)_id);
+  integralId = 0;
+  id = new ReferenceCountedAllocator<char>(size);
+  copy<char>((char*)id->getElements(), (const char*)_id, size);
+#else // unix
+  assert(
+    ((type == Trustee::USER) || (type == Trustee::GROUP) || (type == Trustee::EVERYONE)) &&
+    ((unsigned long)_id <= PrimitiveTraits<uid_t>::MAXIMUM),
+    OutOfDomain("Invalid trustee", this)
+  );
+  integralId = (unsigned long)_id;
+#endif // flavor
+}
+
+Trustee::Trustee(const Trustee& copy) throw() : type(copy.type), integralId(copy.integralId), id(copy.id) {
+}
+
+Trustee& Trustee::operator=(const Trustee& eq) throw() {
+  type = eq.type;
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  id = eq.id; // dont care about integralId
+#else // unix
+  integralId = eq.integralId; // dont care about id
+#endif // flavor
+  return *this;
+}
+
+bool Trustee::operator==(const Trustee& eq) throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  if (!((id.isValid()) && (eq.id.isValid()))) {
+    return !id.isValid() && !eq.id.isValid();
+  }
+  return (id->getSize() == eq.id->getSize()) && (compare(id->getElements(), eq.id->getElements(), id->getSize()) == 0);
+#else // unix
+  return integralId == eq.integralId; // id is dont-care
+#endif
+}
+
+Trustee::Trustee(const String& name) throw(TrusteeException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  SID_NAME_USE sidType;
+  char sid[4096];
+  DWORD size = sizeof(sid);
+  assert(::LookupAccountName(0,
+                             name.getElements(),
+                             &sid,
+                             &size,
+                             0,
+                             0,
+                             &sidType) != 0,
+         TrusteeException("Unable to lookup name", this)
+  );
+  assert(sidType != SidTypeInvalid, TrusteeException("Not a trustee", this));
+  DWORD sidSize = ::GetLengthSid((PSID)sid);
+  integralId = PrimitiveTraits<unsigned long>::MAXIMUM;
+  id = new ReferenceCountedAllocator<char>(sidSize);
+  copy<char>((char*)id->getElements(), (const char*)sid, sidSize);
+#else // unix
+  id = 0;
+  //long sysconf(_SC_GETPW_R_SIZE_MAX);
+  //long sysconf(_SC_GETGR_R_SIZE_MAX);
+  char buffer[4096];
+  struct passwd pw;
+  struct passwd* entry;
+  int result = ::getpwnam_r(name.getElements(), &pw, buffer, sizeof(buffer), &entry);
+  if (result == 0) {
+    type = Trustee::USER;
+    integralId = entry->pw_uid;
+  } else {
+  #if defined(_DK_SDU_MIP__BASE__HAVE_GETGRNAM_R)
+    struct group grp;
+    struct group* entry;
+    int result = ::getgrnam_r(name.getElements(), &grp, buffer, sizeof(buffer), &entry);
+  #else
+    #warning Trustee::Trustee(const String& name) uses non-reentrant getgrnam
+    struct group* entry = ::getgrnam(name.getElements());
+    int result = entry ? 0 : -1;
+  #endif
+    if (result == 0) {
+      type = Trustee::GROUP;
+      integralId = entry->gr_gid;
+    } else {
+      if (name == "other") {
+        type = Trustee::EVERYONE;
+        integralId = 0;
+      } else {
+        throw TrusteeException("Not a trustee", this);
+      }
+    }
+  }
+#endif // flavor
+}
+
+bool Trustee::isInitialized() const throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return id.isValid();
+#else // unix
+  return integralId == PrimitiveTraits<unsigned long>::MAXIMUM;
+#endif // flavor
+}
+
+Trustee::TrusteeType Trustee::getType() const throw(TrusteeException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  assert(id.isValid(), TrusteeException("Invalid", this));
+  if (type == UNSPECIFIED) {
+    DWORD nameSize = 0;
+    DWORD domainNameSize = 0;
+    SID_NAME_USE sidType = SidTypeInvalid;
+    assert(::LookupAccountSid(0,
+                              (PSID)id->getElements(), // must be valid
+                              0,
+                              &nameSize,
+                              0,
+                              &domainNameSize,
+                              &sidType) == ERROR_INSUFFICIENT_BUFFER,
+           TrusteeException("Unable to lookup name", this)
+    );
+    type = ((sidType == SidTypeGroup) || (sidType == SidTypeWellKnownGroup)) ? Trustee::GROUP : Trustee::USER;
+  }
+  return type;
+#else // unix
+  ASSERT((type == Trustee::USER) || (type == Trustee::GROUP) || (type == Trustee::EVERYONE));
+  return type;
+#endif // flavor
+}
+
+bool Trustee::isMemberOf(const Trustee& trustee) const throw(TrusteeException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  if (!id.isValid()) {
+    return false;
+  }
+  if (type == UNSPECIFIED) {
+    DWORD nameSize = 0;
+    DWORD domainNameSize = 0;
+    SID_NAME_USE sidType = SidTypeInvalid;
+    ::LookupAccountSid(0, (PSID)id->getElements(), // must be valid
+                       0, &nameSize,
+                       0, &domainNameSize,
+                       &sidType); // does function set sidType even when it fails
+    // expected error: ERROR_INSUFFICIENT_BUFFER
+    ASSERT(sidType != SidTypeInvalid);
+    type = ((sidType == SidTypeGroup) || (sidType == SidTypeWellKnownGroup)) ? Trustee::GROUP : Trustee::USER;
+  }
+  if (type != Trustee::GROUP) {
+    return false;
+  }
+#else // unix
+  ASSERT((type == Trustee::USER) || (type == Trustee::GROUP) || (type == Trustee::EVERYONE));
+  if (type == Trustee::EVERYONE) {
+    return true;
+  }
+#endif // flavor
+  if (type == Trustee::GROUP) {
+    // check if member
+    return true; // TAG: implement me
+    #warning bool Trustee::isMemberOf(const Trustee& trustee) const throw(TrusteeException) not implemented
+  }
+  return false;
+}
+
+// TAG: select full name domain/user with option: LOCAL prefix?, BUILTIN prefix (no)?
+String Trustee::getName() const throw(TrusteeException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  if (!id.isValid()) {
+    return MESSAGE("unknown");
+  }
+  SID_NAME_USE sidType;
+  char name[4096]; // TAG: what is the maximum size
+  DWORD nameSize = sizeof(name);
+  char domainName[64]; // TAG: what is the maximum size
+  DWORD domainNameSize = sizeof(domainName);
+  assert(::LookupAccountSid(0, (PSID)id->getElements(), // must be valid
+                            name, &nameSize,
+                            domainName, &domainNameSize,
+                            &sidType) != 0,
+         TrusteeException("Unable to lookup name", this)
+  );
+  if (domainName[0] != 0) {
+    return String(domainName) + MESSAGE("\\") + String(name);
+  } else {
+    return String(name); // TAG: does nameSize hold length of name
+  }
+#else // unix
+
+  switch (type) {
+  case EVERYONE:
+    return MESSAGE("other");
+  case GROUP:
+    {
+#if defined(_DK_SDU_MIP__BASE__HAVE_GETGRNAM_R)
+      //long sysconf(_SC_GETGR_R_SIZE_MAX);
+      Allocator<char>* buffer = Thread::getLocalStorage();
+      struct group grp;
+      struct group* entry;
+      int result = ::getgrgid_r((gid_t)integralId, &grp, buffer->getElements(), buffer->getSize(), &entry);
+      assert(result == 0, TrusteeException(this));
+      return String(entry->gr_name);
+#else
+      //long sysconf(_SC_GETGR_R_SIZE_MAX);
+      struct group* entry = ::getgrgid((gid_t)integralId);
+      assert(entry != 0, TrusteeException(this));
+      return String(entry->gr_name);
+#endif // HAVE_GETGRNAM_R
+    }
+  case USER:
+    {
+      Allocator<char>* buffer = Thread::getLocalStorage();
+      struct passwd pw;
+      struct passwd* entry;
+      int result = ::getpwuid_r((uid_t)integralId, &pw, buffer->getElements(), buffer->getSize(), &entry);
+      assert(result == 0, TrusteeException("Unable to lookup name", this));
+      return String(entry->pw_name);
+    }
+  }
+#endif // flavor
+}
+
+FormatOutputStream& operator<<(FormatOutputStream& stream, const Trustee& value) throw(IOException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  if (!value.id.isValid()) {
+    return stream << MESSAGE("unknown");
+  }
+  StringOutputStream s;
+  PSID sid = (PSID)value.id->getElements(); // must be valid
+  
+  // write prefix and revision number
+  s << MESSAGE("S-") << ((SID*)sid)->Revision << '-';
+  
+  // write identifier authority
+  PSID_IDENTIFIER_AUTHORITY identifier = ::GetSidIdentifierAuthority(sid);
+  if ((identifier->Value[0] == 0) && (identifier->Value[1] == 0)) {
+    s << DEC << static_cast<unsigned int>(identifier->Value[5]) +
+      (static_cast<unsigned int>(identifier->Value[4]) << 8) +
+      (static_cast<unsigned int>(identifier->Value[3]) << 16) +
+      (static_cast<unsigned int>(identifier->Value[2]) << 24);
+  } else {
+    s << HEX << ZEROPAD << PREFIX << static_cast<unsigned int>(identifier->Value[0])
+      << HEX << ZEROPAD << NOPREFIX << static_cast<unsigned int>(identifier->Value[1])
+      << HEX << ZEROPAD << NOPREFIX << static_cast<unsigned int>(identifier->Value[2])
+      << HEX << ZEROPAD << NOPREFIX << static_cast<unsigned int>(identifier->Value[3])
+      << HEX << ZEROPAD << NOPREFIX << static_cast<unsigned int>(identifier->Value[4])
+      << HEX << ZEROPAD << NOPREFIX << static_cast<unsigned int>(identifier->Value[5]);
+  }
+  
+  // write subauthorities
+  const unsigned int numberOfSubAuthorities = *::GetSidSubAuthorityCount(sid);
+  for (unsigned int i = 0; i < numberOfSubAuthorities; i++) {
+    s << '-' << *::GetSidSubAuthority(sid, i);
+  }
+  s << FLUSH;
+  return stream << s.getString();
+#else
+  unsigned long id = value.integralId;
+  if (id == PrimitiveTraits<unsigned long>::MAXIMUM) {
+    return stream << MESSAGE("unknown");
+  }
+  StringOutputStream s;
+  switch (value.getType()) {
+  case Trustee::EVERYONE:
+    s << 'G' << '-' << MESSAGE("other");
+    break;
+  case Trustee::GROUP:
+    s << 'G' << '-' << id << FLUSH;
+    break;
+  case Trustee::USER:
+    s << 'U' << '-' << id << FLUSH;
+    break;
+  }
+  return stream << s.getString();
+#endif
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/security/Trustee.h
@@ -0,0 +1,171 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_SECURITY__TRUSTEE_H
+#define _DK_SDU_MIP__BASE_SECURITY__TRUSTEE_H
+
+#include <base/security/Trustee.h>
+#include <base/string/String.h>
+#include <base/mem/ReferenceCountedAllocator.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  A trustee (user or group).
+  
+  @short Trustee
+  @ingroup security
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class Trustee : public Object {
+public:
+
+  /** The type of trustee. */
+  enum TrusteeType {
+    UNSPECIFIED, /**< Trustee is unspecified. */
+    USER, /**< Trustee is a user. */
+    GROUP, /**< Trustee is a group. */
+    CLASS, /**< Trustee is a class. */
+    EVERYONE /**< Everyone. */
+  };
+private:
+
+  /** Cached type of trustee. */
+  mutable TrusteeType type;
+  /** Identifier of the trustee represented as an integral. */
+  unsigned long integralId;
+  /** Opaque identifier of the trustee. */
+  ReferenceCountedObjectPointer<ReferenceCountedAllocator<char> > id;
+public:
+  
+  /**
+    This exception is raised by the Trustee class to indicate a non-existent trustee.
+
+    @short Trustee exception
+    @ingroup exceptions security
+    @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    @version 1.0
+  */
+  
+  class TrusteeException : public Exception {
+  public:
+    
+    /**
+      Initializes the exception object with no message.
+    */
+    inline TrusteeException() throw() {}
+
+    /**
+      Initializes the exception object.
+
+      @param message The message.
+    */
+    inline TrusteeException(const char* message) throw() : Exception(message) {}
+
+    /**
+      Initializes the exception object without an associated message.
+
+      @param type The identity of the type.
+    */
+    inline TrusteeException(Type type) throw() : Exception(type) {}
+
+    /**
+      Initializes the exception object.
+
+      @param message An NULL-terminated string (ASCII).
+      @param type The identity of the type.
+    */
+    inline TrusteeException(const char* message, Type type) throw() : Exception(message, type) {}
+  };
+  
+  /**
+    Initializes trustee as invalid.
+  */
+  Trustee() throw();
+  
+  /**
+    Initializes trustee by id. Raises OutOfDomain if the specified id is not
+    supported by the platform.
+  */
+  Trustee(TrusteeType type, const void* id) throw(OutOfDomain);
+  
+  /**
+    Initializes trustee from other trustee.
+  */
+  Trustee(const Trustee& copy) throw();
+  
+  /**
+    Assignment of trustee by trustee.
+  */
+  Trustee& operator=(const Trustee& eq) throw();
+  
+  /**
+    Initializes trustee by name.
+  */
+  Trustee(const String& name) throw(TrusteeException);
+  
+  /**
+    Returns true if the trustees are identical. The method returns true if both
+    trustees are invalid.
+  */
+  bool operator==(const Trustee& eq) throw();
+  
+  /**
+    Returns false if the trustees are not identical. The method returns false if
+    both trustees are invalid.
+  */
+  inline bool operator!=(const Trustee& eq) throw() {
+    return !(*this == eq);
+  }
+  
+  /**
+    Returns true if the trustee is initialized. This does not mean that the
+    trustee exists.
+  */
+  bool isInitialized() const throw();
+  
+  /**
+    Returns type of the trustee.
+  */
+  TrusteeType getType() const throw(TrusteeException);
+  
+  /**
+    Returns true if the specified trustee is a member of this trustee (if a
+    group) and otherwise returns false (also if trustee is currently INVALID).
+    Raises TrusteeException if group could not be enumerated.
+  */
+  bool isMemberOf(const Trustee& trustee) const throw(TrusteeException);
+  
+  /**
+    Returns the short name of the trustee.
+  */
+  String getName() const throw(TrusteeException);
+  
+  /**
+    Returns the full name of the trustee.
+  */
+  String getFullName() const throw(TrusteeException); 
+  
+  friend FormatOutputStream& operator<<(FormatOutputStream& stream, const Trustee& value) throw(IOException);
+};
+
+/**
+  Writes the trustee id to the format output stream.
+*/
+FormatOutputStream& operator<<(FormatOutputStream& stream, const Trustee& value) throw(IOException);
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

2ae3fdcf23f0feb5d74029776b305c57d1a67103
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-20 23:46:51 +0000

    Renamed FLAVOUR.

--- base/mem/DynamicMemory.cpp
+++ base/mem/DynamicMemory.cpp
@@ -15,7 +15,7 @@
 #include <base/mem/DynamicMemory.h>
 #include <base/OperatingSystem.h>
 
-#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
   #include <windows.h>
 #else // unix
   #include <stdlib.h>
@@ -23,7 +23,7 @@
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
-#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
 namespace internal {
   namespace specific {
     extern OperatingSystem::Handle processHeap;
@@ -33,7 +33,7 @@ namespace internal {
 
 void* DynamicMemory::allocate(MemorySize size) throw() {
   void* result;
-#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
   result = static_cast<void*>(::HeapAlloc(internal::specific::processHeap, 0, size));
 #else // unix
   result = ::malloc(size); // unspecified behavior if size is 0
@@ -42,7 +42,7 @@ void* DynamicMemory::allocate(MemorySize size) throw() {
 }
 
 bool DynamicMemory::release(void* memory) throw() {
-#if (_DK_SDU_MIP__BASE__FLAVOUR == _DK_SDU_MIP__BASE__WIN32)
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
   return (memory == 0) || (::HeapFree(internal::specific::processHeap, 0, memory) != 0);
 #else // unix
   ::free(memory); // works with 0 pointer

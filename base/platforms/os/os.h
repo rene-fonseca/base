0fc462a331d04bd4db9d7ede5a433875328a4ba1
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-20 23:51:32 +0000

    Added operating system wrapper header.

---
+++ base/platforms/os/os.h
@@ -0,0 +1,21 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_PLATFORMS_OS__OS_H
+#define _DK_SDU_MIP__BASE_PLATFORMS_OS__OS_H
+
+#if (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__CYGWIN)
+  #include <base/platforms/os/cygwin.cygwin.h>
+#endif
+
+#endif

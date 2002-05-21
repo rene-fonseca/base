928309cafee1cefab59e4758346a9413258b04ac
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-21 02:12:29 +0000

    Added Microsoft compiler header.

---
+++ base/platforms/compiler/msc/msc.h
@@ -0,0 +1,33 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_PLATFORMS_COMPILER_MSC__MSC_H
+#define _DK_SDU_MIP__BASE_PLATFORMS_COMPILER_MSC__MSC_H
+
+#define _DK_SDU_MIP__BASE__COMPILER _DK_SDU_MIP__BASE__MSC
+#define _DK_SDU_MIP__BASE__COMPILER_VERSION _MSC_VER
+#define _DK_SDU_MIP__BASE__COMPILER_NAMESPACE com::microsoft
+
+#define _DK_SDU_MIP__BASE__FUNCTION __FUNCDNAME__
+
+#define _DK_SDU_MIP__BASE__COMPILER_ABI _DK_SDU_MIP__BASE__ABI_MSC
+
+amespace com {
+  
+  namespace microsoft {
+    
+  };
+  
+};
+
+#endif

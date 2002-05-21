fca3fe3ef550bbe51435689d5c83e38e9a588bf4
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-21 01:45:00 +0000

    Added Solaris specific header and fixed bug.

--- base/platforms/os/os.h
+++ base/platforms/os/os.h
@@ -15,7 +15,9 @@
 #define _DK_SDU_MIP__BASE_PLATFORMS_OS__OS_H
 
 #if (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__CYGWIN)
-  #include <base/platforms/os/cygwin.cygwin.h>
+  #include <base/platforms/os/cygwin/cygwin.h>
+#elif (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__SOLARIS)
+  #include <base/platforms/os/solaris/solaris.h>
 #endif
 
 #endif

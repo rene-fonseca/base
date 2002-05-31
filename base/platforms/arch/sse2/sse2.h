cc040be9b262f973936fa0930f53a2f31e586413
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-31 10:30:25 +0000

    Added SSE support.

---
+++ base/platforms/arch/sse2/sse2.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+// TAG: watch out for accuracy
+// FAST/APPROX must be enabled explicitly
+
+#if !defined(_DK_SDU_MIP__BASE__BUILTIN_SQRTF)
+#  define _DK_SDU_MIP__BASE__BUILTIN_SQRTF builtin::sse2::invsqrtf
+#endif
+
+namespace base {
+
+  using namespace compiler::org::gnu;
+  
+  namespace arch {
+  
+    namespace sse2 {
+
+      //typedef double float64;
+
+      static inline sse128 myfunc(sse128 a, sse128 b) throw() {
+        sse128 result;
+        asm (
+          "addss %0,%1\n\t"
+          : "=&x" (result)
+          : "0" (a), "x" (b)
+        );
+        return result;
+      }
+      
+      inline float64 invsqrt(float64 value) throw() {
+        float64 result;
+        asm (
+          "rsqrtss %0,%0\n\t"
+          : "=x" (result)
+          : "0" (value)
+        );
+        return result;
+      }
+
+      inline float64 sqrt(float64 value) throw() {
+        float64 result;
+        asm (
+          "sqrtsd %0,%0\n\t"
+          : "=x" (result)
+          : "0" (value)
+        );
+        return result;
+      }
+    
+    }; // end of namespace sse2
+  
+  }; // end of namespace arch
+ 
+}; // end of namespace base

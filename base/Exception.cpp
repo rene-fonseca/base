bcc772438e778fc55e77d4e2fe4201e010a714f4
René Fonseca <rene.m.fonseca@gmail.com>
2002-05-31 09:56:06 +0000

    Added internal features header.

--- base/Exception.cpp
+++ base/Exception.cpp
@@ -11,6 +11,7 @@
     For the licensing terms refer to the file 'LICENSE'.
  ***************************************************************************/
 
+#include <base/platforms/features.h>
 #include <base/Exception.h>
 
 #if defined(_DK_SDU_MIP__BASE__EXCEPTION_V3MV)

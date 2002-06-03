20ea38c435da2112ad2e78e631f80817a4c33313
René Fonseca <rene.m.fonseca@gmail.com>
2002-06-03 20:32:27 +0000

    Reserved header for upcoming SSE3.

---
+++ base/platforms/arch/sse3/sse3.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+namespace base {
+  
+  namespace builtin {
+    
+    namespace sse3 {
+      
+    }; // end of namespace sse3
+    
+  }; // end of namespace builtin
+  
+}; // end of namespace base

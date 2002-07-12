1048d9c25a863ac1662aa4eb0795f3787427a282
René Fonseca <rene.m.fonseca@gmail.com>
2002-07-12 16:04:08 +0000

    Removed old code.

--- base/Application.cpp
+++ base/Application.cpp
@@ -70,7 +70,7 @@ public:
     // TAG: need runtime debug mode support (e.g. bool Trace::debug or with level support)
     Trace::message(stream.getString().getElements());
 #endif
-    exit(Application::EXIT_CODE_ERROR); // TAG: is abort() better
+    exit(Application::EXIT_CODE_INTERNAL_ERROR); // TAG: is abort() better
   }
 
   static void unexpectedExceptionHandler() {
@@ -112,60 +112,52 @@ public:
     SystemLogger::write(SystemLogger::ERROR, stream.getString().getElements());
     // TAG: only if ferr is valid
     ferr << stream.getString() << ENDL;
-#if !defined(_DK_SDU_MIP__BASE__DEBUG) // TAG: must be a setting in some operating mode class - e.g. setDebugMode(unsigned int flags)
-    // continue search for exception handler in debug mode
-    exit(Application::EXIT_CODE_ERROR); // TAG: is abort() or terminate() better
-#endif
+    exit(Application::EXIT_CODE_INTERNAL_ERROR); // TAG: is abort() or terminate() better
   }
 
   /* The application signal handler. */
 #if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
 #if 0 // disabled
-     // TAG: we should destroy window in destructor
-     StringOutputStream stream;
-     stream << MESSAGE("messageHandler: message=") << message << " primary=" << primaryParameter << " second=" << secondaryParameter << FLUSH;
+  // TAG: we should destroy window in destructor
+  StringOutputStream stream;
+  stream << MESSAGE("messageHandler: message=") << message << " primary=" << primaryParameter << " second=" << secondaryParameter << FLUSH;
   Trace::message(stream.getString().getElements());
-  Trace::message("Quit");
-  Trace::message("got WM_QUIT message");
-       if (::InSendMessage()) {
-         ::ReplyMessage(0);
-       }
-       if (Application::application) {
-         Application::application->terminate();
-       }
-       return 0;
-     case WM_CLOSE:
-       Trace::message("got WM_CLOSE message");
-       return 0;
-     case WM_DESTROY:
-       Trace::message("got WM_DESTROY message");
-       ::PostQuitMessage(Application::application->exitCode);
-       return 0;
-     case WM_QUERYENDSESSION:
-       Trace::message("got WM_QUERYENDSESSION message");
-       return TRUE; // do nothing but allow session to be closed
-     case WM_ENDSESSION: // TAG: I do NOT like this message
-       Trace::message("got WM_ENDSESSION message");
-       // wait for termination
-  Trace::message("got unspecified message");
+  if (::InSendMessage()) {
+    ::ReplyMessage(0);
+  }
+  if (Application::application) {
+    Application::application->terminate();
+  }
   return 0;
+  switch () {
+  case WM_CLOSE:
+    return 0;
+  case WM_DESTROY:
+    ::PostQuitMessage(Application::application->exitCode);
+  return 0;
+  case WM_QUERYENDSESSION:
+    return TRUE; // do nothing but allow session to be closed
+  case WM_ENDSESSION: // TAG: I do NOT like this message
+    // wait for termination
+    return 0;
+  }
   
   windowClass.lpszClassName = "mip.sdu.dk/~fonseca/base?message handler";
   OSVERSIONINFO versionInfo;
-   versionInfo.dwOSVersionInfoSize = sizeof(versionInfo);
-   ::GetVersionEx(&versionInfo); // never fails
-   bool isWindows2000OrLater = (versionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT) && (versionInfo.dwMajorVersion >= 5);
+  versionInfo.dwOSVersionInfoSize = sizeof(versionInfo);
+  ::GetVersionEx(&versionInfo); // never fails
+  bool isWindows2000OrLater = (versionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT) && (versionInfo.dwMajorVersion >= 5);
 //   OSVERSIONINFO versionInfo;
 //   versionInfo.dwOSVersionInfoSize = sizeof(versionInfo);
 //   ::GetVersionEx(&versionInfo); // never fails
 //   bool isWindows2000OrLater = (versionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT) && (versionInfo.dwMajorVersion >= 5);
 // we do not use message-only window 'cause it cannot be enumerated
   "mip.sdu.dk/~fonseca/base?message handler", // registered class name
-isWindows2000OrLater ? ((HWND)-3) : ((HWND)0), // no parent or owner window - (HWND(-3)) ~ HWND_MESSAGE
-         ((HWND)0), // no parent or owner window
-     //isWindows2000OrLater ? ((HWND)-3) : ((HWND)0), // (HWND(-3)) ~ HWND_MESSAGE
-DWORD dispatchResult;
-   LRESULT result2 = ::SendMessageTimeout(messageWindow, 10000+WM_QUIT, Application::EXIT_CODE_EXTERNAL, 0, SMTO_NORMAL, 3000, &dispatchResult);
+    isWindows2000OrLater ? ((HWND)-3) : ((HWND)0), // no parent or owner window - (HWND(-3)) ~ HWND_MESSAGE
+    ((HWND)0), // no parent or owner window
+                                        //isWindows2000OrLater ? ((HWND)-3) : ((HWND)0), // (HWND(-3)) ~ HWND_MESSAGE
+                                        DWORD dispatchResult;
+  LRESULT result2 = ::SendMessageTimeout(messageWindow, 10000+WM_QUIT, Application::EXIT_CODE_EXTERNAL, 0, SMTO_NORMAL, 3000, &dispatchResult);
 #endif // disabled
   
   static BOOL WINAPI signalHandler(DWORD signal) throw() {

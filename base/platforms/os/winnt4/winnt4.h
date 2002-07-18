e1044cbb74a64e08f8afefedfee43a153f5ce425
René Fonseca <rene.m.fonseca@gmail.com>
2002-07-18 02:14:20 +0000

    Added winnt4 specific header.

---
+++ base/platforms/os/winnt4/winnt4.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_PLATFORMS_OS_WINNT4__WINNT4_H
+#define _DK_SDU_MIP__BASE_PLATFORMS_OS_WINNT4__WINNT4_H
+
+#include <base/features.h>
+
+#if !defined(_DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2L)
+#  if defined(_DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2)
+#    define _DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2L _DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2
+#  elif defined(_DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2F)
+#    define _DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2L _DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2F
+#  else
+#    define _DK_SDU_MIP__BASE__COMPILER_ISOC_LOG2L isoc::log2
+#  endif
+#endif
+
+#undef _DK_SDU_MIP__BASE__COMPILER_ISOC_FLOORL
+#define _DK_SDU_MIP__BASE__COMPILER_ISOC_FLOORL isoc::floor
+
+#endif

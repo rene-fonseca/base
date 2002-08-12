906f3f76d15e87ba93b1078774a1fd142cdfaf82
René Fonseca <rene.m.fonseca@gmail.com>
2002-08-12 19:55:44 +0000

    Added comments.

--- base/SystemInformation.h
+++ base/SystemInformation.h
@@ -22,6 +22,7 @@ _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 /**
   System information.
 
+  @short System information
   @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
   @version 1.0
 */
@@ -35,14 +36,23 @@ public:
   static String getVendor() throw();
 
   /**
-    Returns a description of the particular operating system.
+    Returns a description of the operating system.
   */
   static String getSystem() throw();
 
+  /**
+    Returns the release of the operating system.
+  */
   static String getRelease() throw();
 
+  /**
+    Returns the version of the operating system.
+  */
   static String getVersion() throw();
 
+  /**
+    Returns the machine.
+  */
   static String getMachine() throw();
 };
 

8854cb615082664fc9177a66cca06e6b96f1628a
René Fonseca <rene.m.fonseca@gmail.com>
2002-08-12 15:56:36 +0000

    Added C++ compiler tests.

---
+++ config/gcc.cpp
@@ -0,0 +1,20 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+/*
+  This program compiles if the compiler is GNU C++ Compiler compatible.
+*/
+
+#if (!defined(__GNUC__) || !defined(__GNUC_MINOR__) || !defined(__cplusplus))
+#  error Not GNU C++ Compiler
+#endif

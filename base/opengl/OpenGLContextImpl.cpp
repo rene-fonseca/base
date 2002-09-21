d31267ed718d7849d930c79df0a0bc391bd40013
René Fonseca <rene.m.fonseca@gmail.com>
2002-09-21 07:20:48 +0000

    Added OpenGLContextImpl.

---
+++ base/opengl/OpenGLContextImpl.cpp
@@ -0,0 +1,248 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/platforms/features.h>
+#include <base/opengl/OpenGLContextImpl.h>
+#include <base/ui/WindowImpl.h>
+#include <base/platforms/backend/WindowImpl.h>
+#include <base/concurrency/SpinLock.h>
+
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+#  include <base/platforms/os/unix/GDI.cpp>
+#else // unix
+#  include <base/platforms/os/unix/GLX.cpp>
+#endif // flavor
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+void OpenGLContextImpl::loadModule() throw(OpenGLException) {
+  // TAG: never release ui component?
+  assert(
+    Backend<WindowImpl>::loadModule(true),
+    OpenGLException("Unable to load module", Type::getType<OpenGLContextImpl>())
+  );
+  static SpinLock spinLock;
+  spinLock.exclusiveLock();
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  bool success = native::GDI::import();
+#else // unix
+  bool success = native::GLX::import();
+#endif // flavor
+  spinLock.releaseLock();
+  assert(
+    success,
+    OpenGLException("Unable to load module", Type::getType<OpenGLContextImpl>())
+  );
+}
+
+OpenGLContextImpl::OpenGLContextImpl() throw(OpenGLException)
+  : version(0),
+    openGL() {
+}
+
+String OpenGLContextImpl::getGLClientVendor() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return String(); // TAG: fixme
+#else // unix
+  return native::GLX::glXGetClientString((Display*)Backend<WindowImpl>::getDisplay(), GLX_VENDOR);
+#endif // flavor
+}
+
+String OpenGLContextImpl::getGLClientRelease() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return String(); // TAG: fixme
+#else // unix
+  return native::GLX::glXGetClientString((Display*)Backend<WindowImpl>::getDisplay(), GLX_VERSION);
+#endif // flavor
+}
+
+String OpenGLContextImpl::getGLClientExtensions() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return String(); // TAG: fixme
+#else // unix
+  return native::GLX::glXGetClientString((Display*)Backend<WindowImpl>::getDisplay(), GLX_EXTENSIONS);
+#endif // flavor
+}
+
+String OpenGLContextImpl::getGLServerVendor() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return String(); // TAG: fixme
+#else // unix
+  void* screenHandle = 0; // TAG: fixme move method to OpenGLWindowContext?
+  return native::GLX::glXQueryServerString(
+    (Display*)Backend<WindowImpl>::getDisplay(),
+    ::XScreenNumberOfScreen((Screen*)screenHandle),
+    GLX_VENDOR
+  );
+#endif // flavor
+}
+
+String OpenGLContextImpl::getGLServerRelease() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return String(); // TAG: fixme
+#else // unix
+  void* screenHandle = 0; // TAG: fixme move method to OpenGLWindowContext?
+  return native::GLX::glXQueryServerString(
+    (Display*)Backend<WindowImpl>::getDisplay(),
+    ::XScreenNumberOfScreen((Screen*)screenHandle),
+    GLX_VERSION
+  );
+#endif // flavor
+}
+
+String OpenGLContextImpl::getGLServerExtensions() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return String(); // TAG: fixme
+#else // unix
+  void* screenHandle = 0; // TAG: fixme move method to OpenGLWindowContext?
+  return native::GLX::glXQueryServerString(
+    (Display*)Backend<WindowImpl>::getDisplay(),
+    ::XScreenNumberOfScreen((Screen*)screenHandle),
+    GLX_EXTENSIONS
+  );
+#endif // flavor
+}
+
+bool OpenGLContextImpl::isDirect() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return true; // TAG: unless pixmap
+#else // unix
+  return native::GLX::glXIsDirect(
+    (Display*)Backend<WindowImpl>::getDisplay(),
+    (GLXContext)renderingContextHandle
+  ) == True;
+#endif // flavor
+}
+
+bool OpenGLContextImpl::hasCurrent() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return native::GDI::wglGetCurrentContext() != 0;
+#else // unix
+  return native::GLX::glXGetCurrentContext() != 0;
+#endif // flavor
+}
+
+bool OpenGLContextImpl::isCurrent() const throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  return native::GDI::wglGetCurrentContext() == (HGLRC)renderingContextHandle;
+#else // unix
+  return native::GLX::glXGetCurrentContext() == (GLXContext)renderingContextHandle;
+#endif // flavor
+}
+
+void OpenGLContextImpl::makeCurrent() throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)  
+  native::GDI::wglMakeCurrent((HDC)graphicsContextHandle, (HGLRC)renderingContextHandle);
+#else // unix
+  assert(
+    native::GLX::glXMakeCurrent(
+      (Display*)Backend<WindowImpl>::getDisplay(),
+      (GLXDrawable)drawableHandle,
+      (GLXContext)renderingContextHandle
+    ) == True,
+    OpenGLException(this)
+  );
+// GLX 1.3
+//   Bool result = native::GLX::glXMakeContextCurrent(
+//     (Display*)Backend<WindowImpl>::getDisplay(),
+//     (GLXDrawable)drawableHandle,
+//     (GLXDrawable)drawableHandle,
+//     (GLXContext)renderingContextHandle
+//   );
+#endif // flavor
+}
+
+void OpenGLContextImpl::deselect() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)  
+  native::GDI::wglMakeCurrent(0, 0);
+#else // unix
+  assert(
+    native::GLX::glXMakeCurrent(
+      (Display*)Backend<WindowImpl>::getDisplay(),
+      (GLXDrawable)None,
+      (GLXContext)0
+    ) == True,
+    OpenGLException(this)
+  );
+// GLX 1.3
+//   Bool result = native::GLX::glXMakeContextCurrent(
+//     (Display*)Backend<WindowImpl>::getDisplay(),
+//     None,
+//     None,
+//     0
+//   );
+#endif // flavor
+}
+
+void OpenGLContextImpl::swap() throw(OpenGLException) {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  // ::SwapBuffers((HDC)graphicsContextHandle); // ignore errors
+  assert(
+    ::SwapBuffers((HDC)graphicsContextHandle),
+    OpenGLException("Unable to swap buffers", this)
+  );
+#else // unix
+  native::GLX::glXSwapBuffers((Display*)Backend<WindowImpl>::getDisplay(), (GLXDrawable)drawableHandle);
+#endif // flavor
+}
+
+void OpenGLContextImpl::swap(int plane) throw(OutOfRange, OpenGLException) {
+  assert(
+    (plane >= -numberOfUnderlayPlanes) && (plane <= numberOfOverlayPlanes),
+    OutOfRange(this)
+  );
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)  
+  static const UINT NATIVE_PLANES[15 - (-15) + 1] = {
+    WGL_SWAP_UNDERLAY15,
+    WGL_SWAP_UNDERLAY14,
+    WGL_SWAP_UNDERLAY13,
+    WGL_SWAP_UNDERLAY12,
+    WGL_SWAP_UNDERLAY11,
+    WGL_SWAP_UNDERLAY10,
+    WGL_SWAP_UNDERLAY9,
+    WGL_SWAP_UNDERLAY8,
+    WGL_SWAP_UNDERLAY7,
+    WGL_SWAP_UNDERLAY6,
+    WGL_SWAP_UNDERLAY5,
+    WGL_SWAP_UNDERLAY4,
+    WGL_SWAP_UNDERLAY3,
+    WGL_SWAP_UNDERLAY2,
+    WGL_SWAP_UNDERLAY1,
+    WGL_SWAP_MAIN_PLANE,
+    WGL_SWAP_OVERLAY1,
+    WGL_SWAP_OVERLAY2,
+    WGL_SWAP_OVERLAY3,
+    WGL_SWAP_OVERLAY4,
+    WGL_SWAP_OVERLAY5,
+    WGL_SWAP_OVERLAY6,
+    WGL_SWAP_OVERLAY7,
+    WGL_SWAP_OVERLAY8,
+    WGL_SWAP_OVERLAY9,
+    WGL_SWAP_OVERLAY10,
+    WGL_SWAP_OVERLAY11,
+    WGL_SWAP_OVERLAY12,
+    WGL_SWAP_OVERLAY13,
+    WGL_SWAP_OVERLAY14,
+    WGL_SWAP_OVERLAY15
+  };
+  assert(
+    native::GDI::wglSwapLayerBuffers((HDC)graphicsContextHandle, NATIVE_PLANES[plane + 15]),
+    OpenGLException("Unable to swap plane", this)
+  );
+#else // unix
+  // TAG: fixme
+  // TAG: lookup context in lot
+#endif // flavor
+}
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
---
+++ base/opengl/OpenGLContextImpl.h
@@ -0,0 +1,204 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_OPENGL__OPEN_GL_CONTEXT_IMPL_H
+#define _DK_SDU_MIP__BASE_OPENGL__OPEN_GL_CONTEXT_IMPL_H
+
+#include <base/ui/Drawable.h>
+#include <base/opengl/OpenGL.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Base class of all OpenGL rendering contexts.
+
+  @short OpenGL rendering context.
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class OpenGLContextImpl : virtual public Drawable {
+protected:
+
+  /** Version of the OpenGL window management (e.g. 0x010300). */
+  unsigned int version;
+  void* graphicsContextHandle;
+  /** Opaque handle to OpenGL rendering context. */
+  void* renderingContextHandle;
+  /** The number of overlay planes. */
+  unsigned int numberOfOverlayPlanes;
+  /** The number of underlay planes. */
+  unsigned int numberOfUnderlayPlanes;
+  uint8 colorBits;
+  uint8 redBits;
+  uint8 redShift;
+  uint8 greenBits;
+  uint8 greenShift;
+  uint8 blueBits;
+  uint8 blueShift;
+  uint8 alphaBits;
+  uint8 alphaShift;
+  uint8 accumulatorBits;
+  uint8 accumulatorRedBits;
+  uint8 accumulatorGreenBits;
+  uint8 accumulatorBlueBits;
+  uint8 accumulatorAlphaBits;
+  uint8 depthBits;
+  uint8 stencilBits;
+  uint8 auxBuffers;
+
+  /**
+    Loads the OpenGL module.
+  */
+  static void loadModule() throw(OpenGLException);
+public:
+
+  /** OpenGL implementation. */
+  OpenGL openGL;
+
+  /** OpenGL support flags. */
+  enum Flag {
+    COLOR_INDEX = 1, /**< Color index. */
+    DOUBLE_BUFFERED = COLOR_INDEX << 1, /**< Request double buffered. */
+    ACCUMULATOR = DOUBLE_BUFFERED << 1, /**< Accumulator. */
+    ALPHA = ACCUMULATOR << 1, /**< Request alpha/transparency support. */
+    DEPTH = ALPHA << 1, /**< Depth buffer support. */
+    STENCIL = DEPTH << 1, /**< Stencil buffer support. */
+    AUX = STENCIL << 1, /**< Auxiliary buffer support. */
+    MULTI_SAMPLE = AUX << 1, /**< Multi sample support. */
+    STEREO = MULTI_SAMPLE << 1, /**< Stereo support. */
+    LUMINANCE = STEREO << 1, /**< Luminance support. */
+    RGB = LUMINANCE << 1,
+    RGB15 = RGB << 1,
+    RGB16 = RGB15 << 1,
+    RGB24 = RGB16 << 1,
+    RGBA32 = RGB24 << 1,
+    OVERLAY = RGBA32 << 1,
+    UNDERLAY = OVERLAY << 1,
+    DIRECT = UNDERLAY << 1 /**< Direct rendering support. */
+    // ACCELERATED (same as DIRECT?)
+    // GENERIC
+  };
+  
+  /**
+    Initializes an OpenGL rendering context.
+  */
+  OpenGLContextImpl() throw(OpenGLException);
+
+  /**
+    Returns the vendor of the client.
+  */
+  String getGLClientVendor() const throw(OpenGLException);
+
+  /**
+    Returns the release of the client.
+  */
+  String getGLClientRelease() const throw(OpenGLException);
+
+  /**
+    Returns the client extensions.
+  */
+  String getGLClientExtensions() const throw(OpenGLException);
+
+  /**
+    Returns the vendor of the server.
+  */
+  String getGLServerVendor() const throw(OpenGLException);
+
+  /**
+    Returns the release of the server.
+  */
+  String getGLServerRelease() const throw(OpenGLException);
+
+  /**
+    Returns the server extensions.
+  */
+  String getGLServerExtensions() const throw(OpenGLException);
+  
+  /**
+    Returns the number of overlay planes.
+  */
+  inline unsigned int getNumberOfOverlayPlanes() const throw(OpenGLException) {
+    return numberOfOverlayPlanes;
+  }
+
+  /**
+    Returns the number of underlay planes.
+  */
+  inline unsigned int getNumberOfUnderlayPlanes() const throw(OpenGLException) {
+    return numberOfUnderlayPlanes;
+  }
+
+  inline bool isDoubleBuffered() const throw() {
+    return false;
+  }
+
+  inline bool isRGBA() const throw() {
+    return false;
+  }
+  
+  inline bool isHasAccumulatorBuffer() const throw() {
+    return false;
+  }
+  
+  inline bool isHasDepthBuffer() const throw() {
+    return false;
+  }
+  
+  inline bool isHasStencilBuffer() const throw() {
+    return false;
+  }
+
+  // ...
+
+  /**
+    Returns true if context is a direct rendering context.
+  */
+  bool isDirect() const throw(OpenGLException);
+  
+  /**
+    Returns true if the executing thread has an associated OpenGL context.
+  */
+  bool hasCurrent() const throw(OpenGLException);
+  
+  /**
+    Returns true if the OpenGL context is the current context of the executing thread.
+  */
+  bool isCurrent() const throw(OpenGLException);
+  
+  /**
+    Selects this OpenGL context as the current context of the executing thread.
+  */
+  void makeCurrent() throw(OpenGLException);
+
+  /**
+    Deselects the OpenGL context of the executing thread.
+  */
+  void deselect() throw();
+
+  /**
+    Swaps all the buffers.
+  */
+  void swap() throw(OpenGLException);
+
+  /**
+    Swaps the specified layer.
+
+    @param plane The layer to swap.
+  */
+  void swap(int plane) throw(OutOfRange, OpenGLException);
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

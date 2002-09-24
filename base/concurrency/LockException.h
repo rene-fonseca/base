5a78316f38de4ff8237ce4985f3e6df16e241cfe
René Fonseca <rene.m.fonseca@gmail.com>
2002-09-24 20:19:53 +0000

    Added LockException.

---
+++ base/concurrency/LockException.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_CONCURRENCY__LOCK_EXCEPTION_H
+#define _DK_SDU_MIP__BASE_CONCURRENCY__LOCK_EXCEPTION_H
+
+#include <base/Exception.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Base class of synchronization object exceptions.
+  
+  @ingroup exception
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class LockException : public Exception {
+public:
+
+  /**
+    Initializes the exception object with no message.
+  */
+  inline LockException() throw() {
+  }
+
+  /**
+    Initializes the exception object.
+
+    @param message The message.
+  */
+  inline LockException(const char* message) throw() {
+  }
+
+  /**
+    Initializes the exception object without an associated message.
+    
+    @param type The identity of the type.
+  */
+  inline LockException(Type type) throw() : Exception(type) {
+  }
+  
+  /**
+    Initializes the exception object.
+    
+    @param message An NULL-terminated string (ASCII).
+    @param type The identity of the type.
+  */
+  inline LockException(const char* message, Type type) throw() : Exception(message, type) {
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

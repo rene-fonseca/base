980bc1d77f34aa9b8103803633892e7a8a0fee69
René Fonseca <rene.m.fonseca@gmail.com>
2002-09-26 15:12:34 +0000

    Implementation.

--- base/ui/Bitmap.cpp
+++ base/ui/Bitmap.cpp
@@ -14,9 +14,110 @@
 #include <base/platforms/features.h>
 #include <base/ui/Bitmap.h>
 
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+#  include <windows.h>
+#  undef DELETE // yikes
+#else // unix (X11)
+#  include <X11/Xlib.h>
+#  include <X11/Xutil.h>
+#  include <X11/Xatom.h>
+#endif // flavor
+
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
-Bitmap::Bitmap() throw() {
+Bitmap::Handle::~Handle() throw() {
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  if (handle) {
+    ::DeleteObject((HGDIOBJ)handle);
+  }
+#else // unix
+  if (handle) {
+    // TAG: fixme
+  }
+#endif // flavor
+}
+
+Bitmap::Bitmap(const Dimension& dimension, Format format, Encoding encoding, const void* data) throw(UserInterfaceException)
+  : handle(0) {
+  if (!dimension.isProper()) {
+    return;
+  }
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  HDC h = ::GetDCEx(0, 0, 0); // TAG: destroy this?
+  HDC deviceContextHandle = ::CreateCompatibleDC(h);
+  assert(deviceContextHandle, UserInterfaceException("Unable to create bitmap", this));
+  HBITMAP bitmapHandle = ::CreateCompatibleBitmap((HDC)h, dimension.getWidth(), dimension.getHeight());
+  if (!bitmapHandle) {
+    ::DeleteDC((HDC)deviceContextHandle);
+    throw UserInterfaceException("Unable to create bitmap", this);
+  }
+  HGDIOBJ previous = ::SelectObject((HDC)deviceContextHandle, (HGDIOBJ)bitmapHandle);
+  if (!previous) {
+    ::DeleteObject(bitmapHandle);
+    ::DeleteDC(deviceContextHandle);
+    throw UserInterfaceException("Unable to create bitmap", this);    
+  } else {
+    ::DeleteObject(previous); // TAG: is this required
+  }
+  
+  assert((format == Bitmap::RGB) && (encoding == Bitmap::RGB_32), UserInterfaceException(this));
+  
+  BITMAPINFO info;
+  info.bmiHeader.biSize = sizeof(info.bmiHeader);
+  info.bmiHeader.biWidth = dimension.getWidth();
+  info.bmiHeader.biHeight = dimension.getHeight();
+  info.bmiHeader.biPlanes = 1;
+  info.bmiHeader.biBitCount = 32;
+  info.bmiHeader.biCompression = BI_RGB;
+  info.bmiHeader.biSizeImage = 0;
+  info.bmiHeader.biXPelsPerMeter = 1;
+  info.bmiHeader.biYPelsPerMeter = 1;
+  info.bmiHeader.biClrUsed = 0;
+  info.bmiHeader.biClrImportant = 0;
+  
+  assert(
+    ::SetDIBits(
+      (HDC)deviceContextHandle,
+      (HBITMAP)bitmapHandle,
+      0,
+      dimension.getHeight(),
+      data,
+      &info,
+      DIB_RGB_COLORS
+    ),
+    UserInterfaceException(this)
+  );
+  handle = new Handle(deviceContextHandle);
+#else // unix
+  // TAG: fixme
+#endif // flavor
+}
+
+#if 0
+void Bitmap::decode(const Dimension& dimension, Format format, Encoding encoding, const void* data) throw() {
+}
+
+void Bitmap::encode(Format format, Encoding encoding, void* data) throw() {
+}
+#endif
+
+Dimension Bitmap::getDimension() const throw(UserInterfaceException) {
+  if (!handle.isValid()) {
+    return Dimension(0, 0);
+  }
+#if (_DK_SDU_MIP__BASE__FLAVOR == _DK_SDU_MIP__BASE__WIN32)
+  HGDIOBJ bitmapHandle = ::GetCurrentObject((HDC)handle->getHandle(), OBJ_BITMAP);
+  BITMAP info;
+  ::GetObject(
+    (HBITMAP)bitmapHandle,
+    sizeof(info),
+    &info
+  );
+  return Dimension(info.bmWidth, info.bmHeight);
+#else // unix
+  return Dimension(0, 0);
+  // TAG: fixme
+#endif // flavor
 }
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE
--- base/ui/Bitmap.h
+++ base/ui/Bitmap.h
@@ -16,6 +16,7 @@
 
 #include <base/Dimension.h>
 #include <base/mem/ReferenceCountedObject.h>
+#include <base/ui/UserInterfaceException.h>
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
@@ -29,27 +30,96 @@ _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 */
 
 class Bitmap : public Object {
+  friend class GraphicsContext;
 private:
 
   class Handle : public ReferenceCountedObject {
+  private:
+    
+    void* handle;
   public:
+
+    inline Handle(void* _handle) throw() 
+      : handle(_handle) {
+    }
+
+    inline void* getHandle() throw() {
+      return handle;
+    }
+    
+    ~Handle() throw();
   };
 
+  /** Handle to the bitmap. */
   ReferenceCountedObjectPointer<Handle> handle;
 public:
 
-  Bitmap() throw();
+  /** Pixel format. */
+  enum Format {
+    GRAY, /**< Gray components. */
+    GRAY_ALPHA, /**< Gray and alpha components. */
+    RGB, /**< Red, green, and blue components. */
+    RGBA /**< Red, green, blue, and alpha components. */
+  };
 
+  /** Pixel encoding. */
+  enum Encoding {
+    GRAY_8, /**< Pixels are stored as */
+    RGB_15, /**< Pixels are stored in 16 bits (5 bit per component). */
+    RGB_16, /**< Pixels are stored in 16 bits (5 bit for red and blue and 6 for green component). */
+    RGB_24, /**< Pixels are stored in 24 bits (8 bits per component with the most significant octet not used). */
+    RGB_24_LE,
+    RGB_24_BE,
+    RGB_32, /**< Pixels are stored in 32 bits (8 bits per component). */
+    RGB_32_LE,
+    RGB_32_BE,
+    RGBA_32, /**< Each pixel take up 32 bit. */
+    RGBA_32_LE, /**< Each pixel take up 32 bit (red, green, blue, and alpha). */
+    RGBA_32_BE /**< Each pixel take up 32 bit (alpha, blue, green, and red). */
+  };
+
+  /**
+    Initializes bitmap as invalid.
+  */
+  inline Bitmap() throw() : handle(0) {
+  }
+
+  /**
+    Initializes bitmap.
+
+    @param dimension The dimension of the bitmap.
+    @param format The pixel format.
+    @param encoding The encoding of the pixels.
+    @param data The bitmap data.
+  */
+  Bitmap(const Dimension& dimension, Format format, Encoding encoding, const void* data) throw(UserInterfaceException);
+
+  /**
+    Initializes bitmap from other bitmap.
+  */
   inline Bitmap(const Bitmap& copy) throw()
     : handle(copy.handle) {
   }
 
+  /**
+    Assignment of bitmap by bitmap.
+  */
   inline Bitmap& operator=(const Bitmap& eq) throw() {
     handle = eq.handle;
     return *this;
   }
+
+  /**
+    Returns true if the bitmap is valid.
+  */
+  inline bool isValid() const throw() {
+    return handle.isValid();
+  }
   
-  Dimension getDimension() const throw();
+  /**
+    Returns the dimension of the bitmap.
+  */
+  Dimension getDimension() const throw(UserInterfaceException);
 };
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

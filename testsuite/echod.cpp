936a7d5d6d3d26b1a5db2a3fd6590ad48e41f75e
René Fonseca <rene.m.fonseca@gmail.com>
2002-10-21 03:16:48 +0000

    Added echod and ping applications.

---
+++ testsuite/echod.cpp
@@ -0,0 +1,230 @@
+/***************************************************************************
+    The Base Framework (Test Suite)
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/Application.h>
+#include <base/UnsignedInteger.h>
+#include <base/collection/List.h>
+#include <base/collection/Queue.h>
+#include <base/concurrency/Semaphore.h>
+#include <base/concurrency/Thread.h>
+#include <base/net/InetEndPoint.h>
+#include <base/net/InetService.h>
+#include <base/net/ServerSocket.h>
+#include <base/string/FormatOutputStream.h>
+#include <base/io/EndOfFile.h>
+
+using namespace base;
+
+Queue<StreamSocket*> jobs;
+Semaphore semaphore;
+
+class EchoThread : public Runnable {
+private:
+
+  static const unsigned int BUFFER_SIZE = 4096;
+  Allocator<char> buffer;
+public:
+  
+  EchoThread() throw()
+    : buffer(BUFFER_SIZE) {
+  }
+  
+  void run() throw() {
+    Thread* thread = Thread::getThread();
+    while (true) {
+      semaphore.wait(); // wait for job
+      
+      StreamSocket* job = jobs.pop();
+      if (!job) { // should we terminate
+        break;
+      }
+      StreamSocket socket = *job; // dereference for convenience
+      
+      try {
+        while (!thread->isTerminated()) {
+          socket.wait(250);
+          unsigned int availableBytes = socket.available();
+          while (availableBytes) {
+            unsigned int bytesToRead = minimum(availableBytes, buffer.getSize());
+            unsigned int bytesRead = socket.read(buffer.getElements(), bytesToRead);
+            availableBytes -= bytesRead;
+            socket.write(buffer.getElements(), bytesRead);
+          }
+        }
+      } catch (EndOfFile& e) {
+        socket.shutdownOutputStream();
+      } catch (IOException& e) {
+      }
+      try {
+        socket.close();
+      } catch (IOException& e) {
+      }
+    }
+  }
+
+  ~EchoThread() throw() {
+  }
+};
+
+class ContextBinder : public Object {
+private:
+  
+  EchoThread runnable; // must be initialized before context
+  Thread context;
+public:
+  
+  ContextBinder() throw()
+    : runnable(),
+      context(&runnable) {
+  }
+  
+  void start() {
+    context.start();
+  }
+  
+  ~ContextBinder() throw() {
+    context.terminate(); // ask context to terminate
+    context.join(); // wait for context to complete
+  }
+};
+
+class EchoServiceApplication : public Application {
+private:
+  
+  static const unsigned int MAJOR_VERSION = 1;
+  static const unsigned int MINOR_VERSION = 0;
+  /** The echo service port. */
+  static const unsigned short ECHO_SERVICE_PORT = 7;
+  static const unsigned int CONCURRENCY = 16;
+  /** The server port. */
+  unsigned short port;
+  
+  enum Command {
+    HELP,
+    ECHO_SERVICE
+  };
+public:
+  
+  EchoServiceApplication(int numberOfArguments, const char* arguments[], const char* environment[]) throw()
+    : Application(MESSAGE("echod"), numberOfArguments, arguments, environment) {
+  }
+  
+  void help() throw() {
+    fout << getFormalName() << MESSAGE(" version ")
+         << MAJOR_VERSION << '.' << MINOR_VERSION << EOL
+         << MESSAGE("The Base Framework (Test Suite)") << EOL
+         << MESSAGE("http://www.mip.sdu.dk/~fonseca/base") << EOL
+         << MESSAGE("Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>") << EOL
+         << ENDL;
+    fout << getFormalName() << MESSAGE(" [--help] [--port PORT]") << ENDL;
+  }
+  
+  bool accept(const InetEndPoint& endPoint) throw() {
+    // fout << MESSAGE("Incomming connection: ") << endPoint << ENDL;
+    return true;
+  }
+  
+  void echod() throw() {
+    List<ContextBinder*> threadPool;
+    for (unsigned int i = 0; i < CONCURRENCY; ++i) {
+      ContextBinder* temp = new ContextBinder();
+      threadPool.add(temp);
+      temp->start();
+    }
+    
+    try {
+      InetAddress address;
+      ServerSocket serverSocket(address, port, 1);
+      
+      Thread* thread = Thread::getThread();
+      while (!thread->isTerminated()) {
+        if (serverSocket.wait(250)) {
+          StreamSocket clientSocket = serverSocket.accept(100);
+          if (clientSocket.isValid()) {
+            // TAG: add thread to pool if required?
+            InetEndPoint endPoint(
+              clientSocket.getAddress(),
+              clientSocket.getPort()
+            );
+            if (accept(endPoint)) {
+              jobs.push(new StreamSocket(clientSocket));
+              semaphore.post(); // notify one thread
+            } else {
+              clientSocket.shutdownOutputStream();
+              clientSocket.close();
+            }
+          }
+        }
+      }
+    } catch (IOException& e) {
+      ferr << MESSAGE("Error: ") << e.getMessage() << ENDL;
+      setExitCode(EXIT_CODE_ERROR);
+    }
+    
+    for (unsigned int i = 0; i < threadPool.getSize(); ++i) {
+      jobs.push(0); // queue dummy job to terminate context
+      semaphore.post(); // notify
+    }
+    List<ContextBinder*>::Enumerator enu = threadPool.getEnumerator();
+    while (enu.hasNext()) {
+      delete *enu.next();
+    }
+  }
+  
+  void main() throw() {
+    String service;
+    Command command = ECHO_SERVICE;
+    
+    const Array<String> arguments = getArguments();
+    if (arguments.getSize() > 0) {
+      Array<String>::ReadEnumerator enu = arguments.getReadEnumerator();
+      while (enu.hasNext()) {
+        String argument = *enu.next();
+        if (argument == "--help") {
+          command = HELP;
+        } else if (argument == "--port") {
+          String temp = *enu.next();
+          try {
+            UnsignedInteger value(temp);
+            if (value > 0xffff) {
+              ferr << MESSAGE("Error: ") << MESSAGE("Invalid port") << ENDL;
+              setExitCode(EXIT_CODE_ERROR);
+              return;
+            }
+            port = value;
+          } catch (InvalidFormat& e) {
+            try {
+              InetService service(temp);
+              port = service.getPort();
+            } catch (ServiceNotFound& e) {
+              ferr << MESSAGE("Error: ") << e.getMessage() << ENDL;
+              setExitCode(EXIT_CODE_ERROR);
+              return;
+            }
+          }
+        }
+      }
+    }
+    
+    switch (command) {
+    case HELP:
+      help();
+      break;
+    case ECHO_SERVICE:
+      echod();
+      break;
+    }
+  }
+};
+
+STUB(EchoServiceApplication);
---
+++ testsuite/ping.cpp
@@ -0,0 +1,244 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/Application.h>
+#include <base/Timer.h>
+#include <base/UnsignedInteger.h>
+#include <base/mathematics/Math.h>
+#include <base/net/ClientSocket.h>
+#include <base/net/InetInterface.h>
+#include <base/net/InetService.h>
+#include <base/net/InetEndPoint.h>
+#include <base/concurrency/Thread.h>
+#include <base/string/FormatOutputStream.h>
+#include <base/string/StringOutputStream.h>
+
+using namespace base;
+
+class PingApplication : public Application {
+private:
+
+  static const unsigned int MAJOR_VERSION = 1;
+  static const unsigned int MINOR_VERSION = 0;
+  /** The echo service port. */
+  static const unsigned short ECHO_SERVICE_PORT = 7;
+  /** The server port. */
+  unsigned short port;
+  /** The bytes to send. */
+  unsigned int dataSize;
+  unsigned int timeout;
+  unsigned int packetsToTransmit;
+  
+  enum Command {
+    HELP,
+    PING
+  };
+public:
+  
+  PingApplication(int numberOfArguments, const char* arguments[], const char* environment[]) throw()
+    : Application(MESSAGE("ping"), numberOfArguments, arguments, environment) {
+    port = ECHO_SERVICE_PORT;
+    dataSize = 32;
+    timeout = 1000000;
+    packetsToTransmit = 0;
+  }
+  
+  void onTermination() throw() {
+  }
+  
+  String getTimeAsString(long long microseconds) throw() {
+    StringOutputStream stream;
+    if (microseconds < 1000) {
+      stream << microseconds << MESSAGE("us");
+    } else if (microseconds < 1000000) {
+      stream << FIXED << setPrecision(3) << microseconds/1000.0 << MESSAGE("ms");
+    } else {
+      stream << FIXED << setPrecision(3) << microseconds/1000000.0 << MESSAGE("s");
+    }
+    stream << FLUSH;
+    return stream.getString();
+  }
+  
+  void ping(const String& host) throw() {
+    bool byName = true;
+    InetAddress address;
+    
+//     if (InetAddress.isIPAddress(host)) {
+//       address = InetAddress(host);
+//       byName = false;
+//     }
+    
+    try {
+      address = InetAddress::getAddressByName(host); // the address of the remote host
+    } catch (HostNotFound& e) {
+      ferr << MESSAGE("Error: ") << MESSAGE("Unable to resolve host") << ENDL;
+      setExitCode(EXIT_CODE_ERROR);
+      return;
+    }
+
+    String name;
+    try {
+      name = address.getHostName(true);
+    } catch (...) {
+      name = host;
+    }
+    
+    if (byName) {
+      fout << MESSAGE("Pinging ") << name << ' ' << '(' << address << ')' << MESSAGE(" with ")
+           << dataSize << MESSAGE(" bytes of data") << EOL
+           << ENDL;
+    } else {
+      fout << MESSAGE("Pinging ") << ' ' << '(' << address << ')' << MESSAGE(" with ")
+           << dataSize << MESSAGE(" bytes of data") << EOL
+           << ENDL;
+    }
+    
+    InetEndPoint endPoint(address, port);
+    StreamSocket socket;
+    socket.connect(endPoint.getAddress(), endPoint.getPort());
+    
+    socket.setReceiveBufferSize(dataSize);
+    socket.setSendBufferSize(dataSize);
+    socket.setTcpNoDelay(true);    
+    
+    Timer timeoutTimer;
+    Timer timer;
+    
+    unsigned int packetsTransmitted = 0;
+    unsigned int packetsReceived = 0;
+    long long minimumTime = PrimitiveTraits<long long>::MAXIMUM;
+    long long maximumTime = 0;
+    unsigned long long totalTime = 0;
+    
+    Allocator<char> outgoing(dataSize);
+    Allocator<char> incomming(dataSize);
+    fill<char>(outgoing.getElements(), outgoing.getSize(), 0);
+    
+    while (!isTerminated() && ((packetsToTransmit == 0) || (packetsTransmitted < packetsToTransmit))) {
+      char* dest = outgoing.getElements();
+      *dest++ = packetsTransmitted;
+      timeoutTimer.start();
+      timer.start();
+      socket.write(outgoing.getElements(), outgoing.getSize());
+      ++packetsTransmitted;
+      socket.wait(timeout); // minimum(250, timeout)
+      // TAG: use timer to check for timeout
+      unsigned int bytesAvailable = socket.available();
+      if (bytesAvailable < incomming.getSize()) {
+        fout << address << ':' << MESSAGE(" request timed out") << ENDL;
+      } else {
+        timer.stop();
+        minimumTime = minimum(minimumTime, timer.getMicroseconds());
+        maximumTime = maximum(maximumTime, timer.getMicroseconds());
+        totalTime += timer.getMicroseconds();
+        while (bytesAvailable >= incomming.getSize()) {
+          socket.read(incomming.getElements(), incomming.getSize());
+          if (compare(incomming.getElements(), outgoing.getElements(), dataSize) == 0) {
+            ++packetsReceived;
+            fout << incomming.getSize() << MESSAGE(" bytes from ") << name << ' '
+                 << '(' << address << ')' << ':' << ' '
+                 << MESSAGE("n=") << packetsReceived << ' '
+                 << MESSAGE("time=") << getTimeAsString(timer.getMicroseconds())
+                 << ENDL;
+            break;
+          }
+        }
+      }
+      timeoutTimer.stop();
+      if (timeoutTimer.getMicroseconds() < timeout) {
+        Thread::microsleep(timeout - timeoutTimer.getMicroseconds());
+      }
+    }
+    
+    socket.close();
+    
+    unsigned int packetsLost = packetsTransmitted - packetsReceived;
+    long double meanTime = totalTime/packetsReceived;
+    fout << EOL
+         << MESSAGE("--- statistics for ") << name << MESSAGE(" ---") << EOL
+         << MESSAGE("Packets transmitted: ") << packetsTransmitted << EOL
+         << MESSAGE("Packets received: ") << packetsReceived << EOL
+         << MESSAGE("Packets lost: ") << packetsLost << ' '
+         << '(' << static_cast<int>(100*packetsLost/packetsTransmitted) << '%' << ')' << EOL
+         << MESSAGE("Time minimum/maximum/mean: ")
+         << getTimeAsString(minimumTime) << '/'
+         << getTimeAsString(maximumTime) << '/'
+         << getTimeAsString(static_cast<long long>(meanTime)) << EOL
+         << ENDL;
+  }
+  
+  void help() throw() {
+    fout << getFormalName() << MESSAGE(" version ")
+         << MAJOR_VERSION << '.' << MINOR_VERSION << EOL
+         << MESSAGE("The Base Framework (Test Suite)") << EOL
+         << MESSAGE("http://www.mip.sdu.dk/~fonseca/base") << EOL
+         << MESSAGE("Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>") << EOL
+         << ENDL;
+    fout << getFormalName() << MESSAGE(" [--help] [--port PORT] [--data SIZE] [--time MS] host") << ENDL;
+  }
+  
+  void main() throw() {
+    Command command = PING;
+    String host;
+    
+    const Array<String> arguments = getArguments();
+    if (arguments.getSize() > 0) {
+      Array<String>::ReadEnumerator enu = arguments.getReadEnumerator();
+      while (enu.hasNext()) {
+        String argument = *enu.next();
+        if (argument == "--help") {
+          command = HELP;
+        } else if (argument == "--port") {
+          String temp = *enu.next();
+          try {
+            UnsignedInteger value(temp);
+            if (value > 0xffff) {
+              ferr << MESSAGE("Error: ") << MESSAGE("Invalid port") << ENDL;
+              setExitCode(EXIT_CODE_ERROR);
+              return;
+            }
+            port = value;
+          } catch (InvalidFormat& e) {
+            try {
+              InetService service(temp);
+              port = service.getPort();
+            } catch (ServiceNotFound& e) {
+              ferr << MESSAGE("Error: ") << e.getMessage() << ENDL;
+              setExitCode(EXIT_CODE_ERROR);
+              return;
+            }
+          }
+        } else {
+          host = argument;
+        }
+      }
+    }
+
+    if (host.isEmpty()) {
+      ferr << MESSAGE("Error: ") << MESSAGE("Host not specified") << ENDL;
+      setExitCode(EXIT_CODE_ERROR);
+      return;
+    }
+    
+    switch (command) {
+    case HELP:
+      help();
+      break;
+    case PING:
+      ping(host);
+      break;
+    }
+  }
+};
+
+STUB(PingApplication);

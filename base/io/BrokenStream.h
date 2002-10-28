d0ca38b0123374d1408b6070c1c25c1071c3a3d9
René Fonseca <rene.m.fonseca@gmail.com>
2002-10-28 02:54:22 +0000

    Added broken stream exception.

---
+++ base/io/BrokenStream.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2000-2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_IO__BROKEN_STREAM_H
+#define _DK_SDU_MIP__BASE_IO__BROKEN_STREAM_H
+
+#include <base/io/IOException.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Specifies that the stream has been broken.
+  
+  @short Broken stream exception
+  @ingroup exceptions
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class BrokenStream : public IOException {
+public:
+  
+  /**
+    Initializes the exception object with no message.
+  */
+  BrokenStream();
+
+  /**
+    Initializes the exception object.
+
+    @param message The message.
+  */
+  
+  BrokenStream(const char* message);
+  
+  /**
+    Initializes the exception object without an associated message.
+    
+    @param type The identity of the type.
+  */
+  inline BrokenStream(Type type) throw() : IOException(type) {
+  }
+  
+  /**
+    Initializes the exception object.
+    
+    @param message An NULL-terminated string (ASCII).
+    @param type The identity of the type.
+  */
+  inline BrokenStream(const char* message, Type type) throw()
+    : IOException(message, type) {
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

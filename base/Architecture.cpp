7fb8a44ad0b5e3447afef2ebe383140920fe4123
René Fonseca <rene.m.fonseca@gmail.com>
2002-11-09 15:23:47 +0000

    Added processor information support.

--- base/Architecture.cpp
+++ base/Architecture.cpp
@@ -15,4 +15,183 @@
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
+unsigned int Architecture::getTechonologies() throw() {
+  unsigned int result = 0;
+#if (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__X86)
+  unsigned int eflags;
+
+  enum Flag {
+    CF = 1 << 0,
+    PF = 1 << 2,
+    AF = 1 << 4,
+    ZF = 1 << 6,
+    SF = 1 << 7,
+    TF = 1 << 8,
+    IF = 1 << 9,
+    DF = 1 << 10,
+    OF = 1 << 11,
+    IOPL = 3 << 12,
+    NT = 1 << 14,
+    RF = 1 << 16,
+    VM = 1 << 17,
+    AC = 1 << 18,
+    VIF = 1 << 19,
+    VIP = 1 << 20,
+    ID = 1 << 21 // identification flag
+  };
+  
+  asm (
+    "        pushfl\n"
+    "        pop %0"
+    : "=m" (eflags) // output
+  );
+  if (eflags & ID) { // is CPUID supported
+    unsigned int eax, ebx, ecx, edx; // result
+    asm (
+      "        cpuid\n\t"
+      : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx) // output
+      : "a" (0) // input // ask for vendor string
+    );
+
+    const bool genuineIntel = (ebx == 0x756e6547) && (edx == 0x49656e69) && (ecx == 0x6c65746e);
+    if (genuineIntel) {
+      asm (
+        "        cpuid\n\t"
+        : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx) // output
+        : "a" (1) // input // ask for feature flags
+      );
+      
+      result |= ((edx >> 28) & 1) ? HYPER_THREADING : 0;
+      result |= ((edx >> 26) & 1) ? SSE2 : 0;
+      result |= ((edx >> 25) & 1) ? SSE : 0;
+      result |= ((edx >> 23) & 1) ? MMX : 0;
+      result |= ((edx >> 0) & 1) ? FPU : 0;
+    }
+  }
+#endif
+  return result;
+}
+  
+Architecture::Major Architecture::getArchitecture() throw() {
+#if (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__X86)
+  return X86;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__SPARC)
+  return SPARC;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__MIPS)
+  return MIPS;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__ITANIUM)
+  return ITANIUM;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__ALPHA)
+  return ALPHA;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__PPC)
+  return POWER_PC;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__RS)
+  return RS;
+#elif (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__X86_64)
+  return X86_64;
+#else
+#  error Unsupported architecture
+#endif
+}
+  
+Architecture::Minor Architecture::getMinorArchitecture() throw() {
+  Architecture::Minor result = GENERIC;
+#if (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__X86)
+  result = X86_386;
+  // TAG: add check for X86_486
+  
+  unsigned int eflags;
+
+  enum Flag {
+    CF = 1 << 0,
+    PF = 1 << 2,
+    AF = 1 << 4,
+    ZF = 1 << 6,
+    SF = 1 << 7,
+    TF = 1 << 8,
+    IF = 1 << 9,
+    DF = 1 << 10,
+    OF = 1 << 11,
+    IOPL = 3 << 12,
+    NT = 1 << 14,
+    RF = 1 << 16,
+    VM = 1 << 17,
+    AC = 1 << 18,
+    VIF = 1 << 19,
+    VIP = 1 << 20,
+    ID = 1 << 21 // identification flag
+  };
+  
+  asm (
+    "        pushfl\n"
+    "        pop %0"
+    : "=m" (eflags) // output
+  );
+  if (eflags & ID) { // is CPUID supported
+    unsigned int eax, ebx, ecx, edx; // result
+    asm (
+      "        cpuid\n\t"
+      : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx) // output
+      : "a" (0) // input // ask for maximum input value and vendor string
+    );
+
+    const bool genuineIntel = (ebx == 0x756e6547) && (edx == 0x49656e69) && (ecx == 0x6c65746e);
+    if (genuineIntel) {
+
+      if (eax > 0) {
+        asm (
+          "        cpuid\n\t"
+          : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx) // output
+          : "a" (1) // input // ask for version information
+        );
+        
+        const unsigned int type = (eax >> 12) & 0x3;
+        const unsigned int family = (eax >> 8) & 0xf;
+        const unsigned int model = (eax >> 4) & 0xf;
+        const unsigned int step = (eax >> 0) & 0xf;
+
+        switch (family) {
+        case 0x4:
+          result = X86_486;
+          break;
+        case 0x5:
+          result = X86_PENTIUM;
+          break;
+        case 0x6:
+          switch (model) {
+          case 0x1:
+            result = X86_PENTIUM_PRO;
+            break;
+          case 0x3:
+          case 0x5:
+            result = X86_PENTIUM_II;
+            break;
+          case 0x7:
+          case 0x8:
+          case 0x10:
+          case 0x11:
+            result = X86_PENTIUM_III;
+            break;
+          }
+          break;
+        case 0xf:
+          result = X86_PENTIUM_IV;
+          break;
+        }
+      }
+    }
+  }
+#endif
+  return result;
+}
+
+// TAG: need support for multiple processors (and hyper-threading)
+// struct ProcessorInformation {major, minor, #logical processors, technologies};
+
+// Array is no good in Architecture header
+// Array<Architecture::Minor> Architecture::getProcessors() throw() {
+//   Array<Minor> result;
+//   return result;
+// }
+
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE
--- base/Architecture.h
+++ base/Architecture.h
@@ -41,6 +41,43 @@ public:
 #endif
   };
 
+  /** Technology flags. */
+  enum Technology {
+    FPU = 1U << 0, /** Floating Point Unit (FPU). */
+    MMX = 1U << 1, /** Multi-Media Extentions (MMX) */
+    SSE = 1U << 2, /** Streaming SIMD Extensions (SSE). */
+    SSE2 = 1U << 3, /** Streaming SIMD Extensions (SSE) 2. */
+    SSE3 = 1U << 4, /** Streaming SIMD Extensions (SSE) 3. */
+    NOW3D = 1U << 5, /** 3DNow. */
+    NOW3D2 = 1U << 6, /** 3DNow 2. */
+    HYPER_THREADING = 1U << 7 /** Hyper-threading. */
+  };
+
+  /** The major architecture. */
+  enum Major {
+    ALPHA, /** Alpha. */
+    X86, /** Intel 32 bit. */
+    ITANIUM, /** Itanium. */
+    MIPS, /** MIPS. */
+    POWER_PC, /** Power PC. */
+    RS, /** RS. */
+    SPARC, /** Sparc. */
+    X86_64 /** X86 64 bit. */
+  };
+  
+  /** The minor architecture. */
+  enum Minor {
+    GENERIC, /** Unspecified processor. */
+    X86_386, /** 386. */
+    X86_486, /** 486. */
+    X86_586, /** 586. */
+    X86_PENTIUM, /** Pentimum. */
+    X86_PENTIUM_PRO, /** Pentimum Pro. */
+    X86_PENTIUM_II, /** Pentimum II. */
+    X86_PENTIUM_III, /** Pentimum III. */
+    X86_PENTIUM_IV, /** Pentimum IV. */
+  };
+  
   /**
     Returns true if the native byte order is big endian.
   */
@@ -54,6 +91,21 @@ public:
   static bool isLittleEndian() throw() {
     return NATIVE == LE;
   }
+
+  /**
+    Returns the supported technologies.
+  */
+  static unsigned int getTechonologies() throw();
+  
+  /**
+    Returns the major architecture.
+  */
+  static Major getArchitecture() throw();
+  
+  /**
+    Returns the processor (lowest common denominator is multiple are present).
+  */
+  static Minor getMinorArchitecture() throw();
 };
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

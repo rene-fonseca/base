9a119161857f7a7356efe1bb3a04bf86a697bbd3
René Fonseca <rene.m.fonseca@gmail.com>
2002-11-12 18:09:39 +0000

    Added security exception.

---
+++ base/security/SecurityException.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2000-2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_SECURITY__SECURITY_EXCEPTION_H
+#define _DK_SDU_MIP__BASE_SECURITY__SECURITY_EXCEPTION_H
+
+#include <base/Exception.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Security exception.
+  
+  @short Security exception.
+  @ingroup exceptions security
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class SecurityException : public Exception {
+public:
+  
+  /**
+    Initializes the exception object with no message.
+  */
+  inline SecurityException() throw() {
+  }
+
+  /**
+    Initializes exception object as unspecified.
+
+    @param message The message.
+  */
+  inline SecurityException(const char* message) throw()
+    : Exception(message) {
+  }
+  
+  /**
+    Initializes the exception object without an associated message.
+    
+    @param type The identity of the type.
+  */
+  inline SecurityException(Type type) throw() : Exception(type) {
+  }
+  
+  /**
+    Initializes the exception object.
+    
+    @param message An NULL-terminated string (ASCII).
+    @param type The identity of the type.
+  */
+  inline SecurityException(const char* message, Type type) throw()
+    : Exception(message, type) {
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

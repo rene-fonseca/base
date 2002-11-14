13ffce8b465fbb66e9ce5a9e83e27125e58fca72
René Fonseca <rene.m.fonseca@gmail.com>
2002-11-14 04:52:59 +0000

    Added ORB decoder and encoder.

---
+++ base/rmi/OrbDecoder.h
@@ -0,0 +1,182 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_RMI__ORB_DECODER_H
+#define _DK_SDU_MIP__BASE_RMI__ORB_DECODER_H
+
+#include <base/Object.h>
+#include <base/io/IOException.h>
+#include <base/mem/Reference.h>
+#include <base/string/String.h>
+#include <base/string/WideString.h>
+#include <base/rmi/OrbReference.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Object Request Broker decoder.
+  
+  @short Object Request Broker decoder.
+  @ingroup rmi
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class OrbDecoder : public Object, public virtual ReferenceCountedObject {
+private:
+  
+  // OrbInputStream stream;
+public:
+  
+  /** The default buffer size. */
+  static const unsigned int BUFFER_SIZE = 4096;
+  
+  /**
+    Decodes a reference.
+  */
+  virtual void read(OrbReference& value) throw(IOException) = 0;
+  
+  /**
+    Decodes a string.
+  */
+  virtual void read(String& value) throw(IOException) = 0;
+  
+  /**
+    Decodes a wide string.
+  */
+  virtual void read(WideString& value) throw(IOException) = 0;  
+  
+  /**
+    Decodes an array of chars.
+  */
+  virtual void read(char* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes a char.
+  */
+  virtual void read(char& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of signed short integers.
+  */
+  virtual void read(short* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes a signed short integer.
+  */
+  virtual void read(short& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of unsigned short integers.
+  */
+  virtual void read(unsigned short* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes a unsigned short integer.
+  */
+  virtual void read(unsigned short&  value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of signed integers.
+  */
+  virtual void read(int* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Decodes a signed integer.
+  */
+  virtual void read(int& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of unsigned integers.
+  */
+  virtual void read(unsigned int* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Decodes an unsigned integer.
+  */
+  virtual void read(unsigned int& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of signed long integers.
+  */
+  virtual void read(long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Decodes a signed long integer.
+  */
+  virtual void read(long& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of unsigned long integers.
+  */
+  virtual void read(unsigned long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Decodes an unsigned long integer.
+  */
+  virtual void read(unsigned long& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of signed long long integers.
+  */
+  virtual void read(long long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Decodes a signed long long integer.
+  */
+  virtual void read(long long& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of unsigned long long integers.
+  */
+  virtual void read(unsigned long long* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes an unsigned long long interger.
+  */
+  virtual void read(unsigned long long& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of float.
+  */
+  virtual void read(float* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes a float.
+  */
+  virtual void read(float& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of double.
+  */
+  virtual void read(double* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes a double.
+  */
+  virtual void read(double& value) throw(IOException) = 0;
+  
+  /**
+    Decodes an array of long double.
+  */
+  virtual void read(long double* buffer, unsigned int size) throw(IOException) = 0;
+  
+  /**
+    Decodes a long double.
+  */
+  virtual void read(long double& value) throw(IOException) = 0;
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif
---
+++ base/rmi/OrbEncoder.h
@@ -0,0 +1,182 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_RMI__ORB_ENCODER_H
+#define _DK_SDU_MIP__BASE_RMI__ORB_ENCODER_H
+
+#include <base/Object.h>
+#include <base/io/IOException.h>
+#include <base/mem/Reference.h>
+#include <base/string/String.h>
+#include <base/string/WideString.h>
+#include <base/rmi/OrbReference.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Object Request Broker encoder.
+  
+  @short Object Request Broker encoder.
+  @ingroup rmi
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class OrbEncoder : public Object, public virtual ReferenceCountedObject {
+private:
+  
+  // OrbOutputStream stream;
+public:
+  
+  /** The default buffer size. */
+  static const unsigned int BUFFER_SIZE = 4096;
+  
+  /**
+    Encodes the specified reference.
+  */
+  virtual void write(const OrbReference& value) throw(IOException) = 0;
+  
+  /**
+    Encodes the specified string.
+  */
+  virtual void write(const String& value) throw(IOException) = 0;
+  
+  /**
+    Encodes the specified wide string.
+  */
+  virtual void write(const WideString& value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const char* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(char value) throw(IOException) = 0;  
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const short* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(short value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const unsigned short* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(unsigned short value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const int* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(int value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const unsigned int* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(unsigned int value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(long value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const unsigned long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(unsigned long value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const long long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(long long value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const unsigned long long* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(unsigned long long value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const float* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(float value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const double* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(double value) throw(IOException) = 0;
+  
+  /**
+    Encodes the elements of the specified array.
+  */
+  virtual void write(const long double* buffer, unsigned int size) throw(IOException) = 0;
+
+  /**
+    Encodes the specified value.
+  */
+  virtual void write(long double value) throw(IOException) = 0;
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

f760a2b909e74d0d1563b12863b0857e859161c8
René Fonseca <rene.m.fonseca@gmail.com>
2002-11-14 02:55:11 +0000

    Added virtual.

--- base/rmi/OrbSkeleton.h
+++ base/rmi/OrbSkeleton.h
@@ -29,7 +29,7 @@ _DK_SDU_MIP__BASE__ENTER_NAMESPACE
   @version 1.0
 */
 
-class OrbSkeleton : public Object, public ReferenceCountedObject {
+class OrbSkeleton : public Object, public virtual ReferenceCountedObject {
 private:
 
   /** The fully qualified name of the skeleton. */

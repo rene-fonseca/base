c20ebe4e4f689905c671b6b618ffabb2e7d08a98
René Fonseca <rene.m.fonseca@gmail.com>
2002-11-21 09:21:21 +0000

    Added CheckedPointer and ProtectedPointer.

---
+++ base/mem/CheckedPointer.h
@@ -0,0 +1,166 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_MEM__CHECKED_POINTER_H
+#define _DK_SDU_MIP__BASE_MEM__CHECKED_POINTER_H
+
+#include <base/mem/NullPointer.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Automation pointer which checks the pointer value before use.
+  
+  @short Checked pointer.
+  @ingroup memory
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+template<class TYPE>
+class CheckedPointer {
+private:
+
+  /** Pointer to object. */
+  TYPE* value;
+public:
+  
+  /**
+    Initializes the automation pointer as 0.
+  */
+  inline CheckedPointer() throw() : value(0) {
+  }
+  
+  /**
+    Initializes the automation pointer.
+    
+    @param value The object pointer to be automated.
+  */
+  inline CheckedPointer(TYPE* _value) throw() : value(_value) {
+  }
+
+  /**
+    Initialization of automation pointer from other automation pointer.
+  */
+  inline CheckedPointer(const CheckedPointer& copy) throw() : value(copy.value) {
+  }
+
+  /**
+    Initialization of automation pointer from other automation pointer using
+    compile time polymorphism.
+  */
+  template<class POLY>
+  inline CheckedPointer(const CheckedPointer<POLY>& copy) throw()
+    : value(copy.getValue()) {
+  }
+  
+  /**
+    Assignment of automation pointer to this automation pointer.
+  */
+  inline CheckedPointer& operator=(const CheckedPointer& eq) throw() {
+    value = eq.value;
+    return *this;
+  }
+
+  /**
+    Assignment of automation pointer to this automation pointer using compile
+    time polymorphism.
+  */
+  template<class POLY>
+  inline CheckedPointer& operator=(const CheckedPointer<POLY>& eq) throw() {
+    value = eq.value;
+    return *this;
+  }
+  
+  /**
+    Returns true if the automation pointer is valid (i.e. it is pointing to an
+    object).
+  */
+  inline bool isValid() const throw() {
+    return value != 0;
+  }
+  
+  /**
+    Returns true if the pointers are equal.
+  */
+  inline bool operator==(const CheckedPointer& eq) const throw() {
+    return value == eq.value;
+  }
+  
+  /**
+    Returns true if the pointers are non-equal.
+  */
+  inline bool operator!=(const CheckedPointer& eq) const throw() {
+    return value != eq.value;
+  }
+  
+  /**
+    Returns mutable object.
+  */
+  inline TYPE& operator*() throw(NullPointer) {
+    assert(value, NullPointer(this));
+    return *value;
+  }
+  
+  /**
+    Returns constant object.
+  */
+  inline const TYPE& operator*() const throw(NullPointer) {
+    assert(value, NullPointer(this));
+    return *value;
+  }
+  
+  /**
+    Returns object for modifying access.
+  */
+  inline TYPE* operator->() throw(NullPointer) {
+    assert(value, NullPointer(this));
+    return value;
+  }
+  
+  /**
+    Returns object for non-modifying access.
+  */
+  inline const TYPE* operator->() const throw(NullPointer) {
+    assert(value, NullPointer(this));
+    return value;
+  }
+  
+  /**
+    Returns true if valid pointer.
+  */
+  inline operator bool() const throw() {
+    return value != 0;
+  }
+};
+
+template<class TYPE>
+class Relocateable<CheckedPointer<TYPE> > {
+public:
+
+  static const bool IS_RELOCATEABLE = Relocateable<void*>::IS_RELOCATEABLE;
+};
+
+template<class TYPE>
+class Hash<CheckedPointer<TYPE> > {
+public:
+
+  inline unsigned long operator()(const CheckedPointer<TYPE>& value) throw() {
+    Hash<void*> hash;
+    return hash(value.getValue());
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif
---
+++ base/mem/ProtectedPointer.h
@@ -0,0 +1,129 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE_MEM__PROTECTED_POINTER_H
+#define _DK_SDU_MIP__BASE_MEM__PROTECTED_POINTER_H
+
+#include <base/mem/NullPointer.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  Automation pointer which protects the pointer value from direct access. The
+  automation pointer may only be used with valid pointers (i.e. not 0).
+  
+  @short Protected pointer.
+  @ingroup memory
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+template<class TYPE>
+class ProtectedPointer {
+private:
+
+  /** Pointer to object. */
+  TYPE* value;
+public:
+  
+  /**
+    Initializes the automation pointer.
+    
+    @param value The object pointer to be automated.
+  */
+  inline ProtectedPointer(TYPE* _value) throw(NullPointer) : value(_value) {
+    assert(value, NullPointer(this));
+  }
+  
+  /**
+    Initialization of automation pointer from other automation pointer.
+  */
+  inline ProtectedPointer(const ProtectedPointer& copy) throw() : value(copy.value) {
+  }
+
+  /**
+    Initialization of automation pointer from other automation pointer using
+    compile time polymorphism.
+  */
+  template<class POLY>
+  inline ProtectedPointer(const ProtectedPointer<POLY>& copy) throw()
+    : value(copy.getValue()) {
+  }
+  
+  /**
+    Assignment of automation pointer to this automation pointer.
+  */
+  inline ProtectedPointer& operator=(const ProtectedPointer& eq) throw() {
+    value = eq.value;
+    return *this;
+  }
+
+  /**
+    Assignment of automation pointer to this automation pointer using compile
+    time polymorphism.
+  */
+  template<class POLY>
+  inline ProtectedPointer& operator=(const ProtectedPointer<POLY>& eq) throw() {
+    value = eq.value;
+    return *this;
+  }
+  
+  /**
+    Returns true if the pointers are equal.
+  */
+  inline bool operator==(const ProtectedPointer& eq) const throw() {
+    return value == eq.value;
+  }
+  
+  /**
+    Returns true if the pointers are non-equal.
+  */
+  inline bool operator!=(const ProtectedPointer& eq) const throw() {
+    return value != eq.value;
+  }
+  
+  /**
+    Returns object for modifying access.
+  */
+  inline TYPE* operator->() throw() {
+    return value;
+  }
+  
+  /**
+    Returns object for non-modifying access.
+  */
+  inline const TYPE* operator->() const throw() {
+    return value;
+  }
+};
+
+template<class TYPE>
+class Relocateable<ProtectedPointer<TYPE> > {
+public:
+
+  static const bool IS_RELOCATEABLE = Relocateable<void*>::IS_RELOCATEABLE;
+};
+
+template<class TYPE>
+class Hash<ProtectedPointer<TYPE> > {
+public:
+
+  inline unsigned long operator()(const ProtectedPointer<TYPE>& value) throw() {
+    Hash<void*> hash;
+    return hash(value.getValue());
+  }
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

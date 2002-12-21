8ddf3156fc6fd5649fcb6fb25e11c24d51ff326e
René Fonseca <rene.m.fonseca@gmail.com>
2002-12-21 15:51:59 +0000

    Added NotCopyable.

---
+++ base/NotCopyable.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__NOT_COPYABLE_H
+#define _DK_SDU_MIP__BASE__NOT_COPYABLE_H
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  This class prevents copy construction and default assignment.
+  
+  @short Not copyable.
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class NotCopyable {
+private:
+
+  /**
+    Default copy construction is not permitted.
+  */
+  NotCopyable(const NotCopyable& copy) throw();
+  
+  /**
+    Default assignment is not permitted.
+  */
+  NotCopyable& operator=(const NotCopyable& eq) throw();
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

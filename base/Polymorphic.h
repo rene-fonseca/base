f23993c474a85926dfc69904d38dd55a9f8a4e16
René Fonseca <rene.m.fonseca@gmail.com>
2003-01-05 07:55:08 +0000

    Added Polymorphic base class.

---
+++ base/Polymorphic.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__POLYMORPHIC_H
+#define _DK_SDU_MIP__BASE__POLYMORPHIC_H
+
+#include <base/features.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  The base class of polymorphic objects. Objects are not copyable by default.
+  
+  @short Polymorphic object.
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class Polymorphic {
+private:
+
+  /* Disable the default copy constructor. */
+  Polymorphic(const Polymorphic& copy) throw();
+  /* Disable the default assignment operator. */
+  Polymorphic& operator=(const Polymorphic& eq) throw();
+public:
+
+  /**
+    Initializes object.
+  */
+  inline Polymorphic() throw() {
+  }
+  
+  /**
+    Destroys the object.
+  */
+  virtual ~Polymorphic() throw();
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

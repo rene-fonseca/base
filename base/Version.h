06d06bdf04545a04242bee4cba1d3e4f4f6092c3
René Fonseca <rene.m.fonseca@gmail.com>
2003-02-10 11:58:48 +0000

    Style.

--- base/Version.h
+++ base/Version.h
@@ -2,7 +2,7 @@
     The Base Framework
     A framework for developing platform independent applications
 
-    Copyright (C) 2002 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+    Copyright (C) 2003 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
 
     This framework is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,8 +19,9 @@
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
 /**
-  This class returns information concerning some component (default is this framework).
-
+  This class returns information concerning some component (default is this
+  framework).
+  
   @short Component information
   @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
   @version 1.0

b12c7946a2850021b75d39251758577118fc8463
René Fonseca <rene.m.fonseca@gmail.com>
2003-02-10 12:01:34 +0000

    Added unspecified type.

---
+++ base/Unspecified.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2002-2003 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#ifndef _DK_SDU_MIP__BASE__UNSPECIFIED_H
+#define _DK_SDU_MIP__BASE__UNSPECIFIED_H
+
+#include <base/features.h>
+
+_DK_SDU_MIP__BASE__ENTER_NAMESPACE
+
+/**
+  @short Unspecified type.
+  @author Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+  @version 1.0
+*/
+
+class Unspecified {
+};
+
+_DK_SDU_MIP__BASE__LEAVE_NAMESPACE
+
+#endif

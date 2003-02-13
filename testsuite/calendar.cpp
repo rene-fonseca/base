101bd17a2563009ad56bc4bb4e26e875dca78b9d
René Fonseca <rene.m.fonseca@gmail.com>
2003-02-13 21:33:40 +0000

    Added testing apps.

---
+++ testsuite/calendar.cpp
@@ -0,0 +1,159 @@
+/***************************************************************************
+    The Base Framework (Test Suite)
+    A framework for developing platform independent applications
+
+    Copyright (C) 2003 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/Application.h>
+#include <base/string/FormatOutputStream.h>
+#include <base/string/StringOutputStream.h>
+#include <base/Date.h>
+#include <base/Literal.h>
+
+using namespace dk::sdu::mip::base;
+
+class CalendarApplication : public Application {
+private:
+
+  static const unsigned int MAJOR_VERSION = 1;
+  static const unsigned int MINOR_VERSION = 0;
+  static const unsigned int FIELD_WIDTH = 4;
+public:
+  
+  CalendarApplication(
+    int numberOfArguments,
+    const char* arguments[],
+    const char* environment[]) throw()
+    : Application(
+        "calendar",
+        numberOfArguments,
+        arguments,
+        environment) {
+  }
+  
+  inline int getSucceedingDayOfWeek(int day) throw() {
+    int temp = day + 1;
+    return (temp > Date::SATURDAY) ? Date::SUNDAY : temp;
+  }
+  
+  void main() throw() {
+    fout << getFormalName() << " version "
+         << MAJOR_VERSION << '.' << MINOR_VERSION << EOL
+         << "The Base Framework (Test Suite)" << EOL
+         << "http://www.mip.sdu.dk/~fonseca/base" << EOL
+         << "Copyright (C) 2003 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>" << EOL
+         << ENDL;
+    
+    Date now = Date::getNow();
+    unsigned int month = now.getMonth();
+    Date::DateTime decomposed;
+    now.split(decomposed, true);
+    
+    // TAG: use locale
+    static const Literal MONTH_NAMES[] = {
+      Literal("January"),
+      Literal("February"),
+      Literal("March"),
+      Literal("April"),
+      Literal("May"),
+      Literal("June"),
+      Literal("July"),
+      Literal("August"),
+      Literal("September"),
+      Literal("October"),
+      Literal("November"),
+      Literal("December")
+    };
+
+    static const Literal DAY_NAMES[] = {
+      Literal("Sunday"),
+      Literal("Monday"),
+      Literal("Tuesday"),
+      Literal("Wednesday"),
+      Literal("Thursday"),
+      Literal("Friday"),
+      Literal("Saturday")
+    };
+    
+    static const Literal DAY_NAMES_MEDIUM[] = {
+      Literal("Sun"),
+      Literal("Mon"),
+      Literal("Tue"),
+      Literal("Wed"),
+      Literal("Thu"),
+      Literal("Fri"),
+      Literal("Sat")
+    };
+    
+    static const Literal DAY_NAMES_SHORT[] = {
+      Literal("Su"),
+      Literal("M"),
+      Literal("Tu"),
+      Literal("W"),
+      Literal("Th"),
+      Literal("F"),
+      Literal("Sa")
+    };
+
+    const int firstDayOfWeekOfLocale = Date::SUNDAY; // TAG: locale specific
+    const int lastDayOfWeekOfLocale =
+      (firstDayOfWeekOfLocale + Date::DAYS_PER_WEEK - 1)%Date::DAYS_PER_WEEK;
+    
+    {
+      StringOutputStream stream;
+      stream << MONTH_NAMES[month] << SP
+             << decomposed.year << FLUSH;
+      String header = stream.getString();
+      int headerIndent =
+        (FIELD_WIDTH * Date::DAYS_PER_WEEK - header.getLength())/2;
+      fout << indent(maximum(0, headerIndent)) << header << ENDL;
+      int dayOfWeek = firstDayOfWeekOfLocale;
+      fout << LEFT << setWidth(6) << "week";
+      for (
+        int day = 0;
+        day < Date::DAYS_PER_WEEK;
+        ++day, dayOfWeek = getSucceedingDayOfWeek(dayOfWeek)
+      ) {
+        fout << RIGHT << setWidth(FIELD_WIDTH)
+             << DAY_NAMES_MEDIUM[dayOfWeek];
+      }
+      fout << ENDL;
+    }
+
+    int firstWeek = Date::getWeek(0, decomposed.month, decomposed.year);
+    
+    int firstDayOfWeek =
+      Date::getDayOfWeek(0, decomposed.month, decomposed.year);
+    
+    fout << LEFT << setWidth(6) << firstWeek;
+    int dayOfWeek = firstDayOfWeekOfLocale;
+    while (dayOfWeek != firstDayOfWeek) {
+      dayOfWeek = getSucceedingDayOfWeek(dayOfWeek);
+      fout << indent(FIELD_WIDTH);
+    }
+    
+    const unsigned int daysOfMonth =
+      Date::getDaysOfMonth(decomposed.month, decomposed.year);
+
+    // TAG: 4 to 6 weeks per month
+    for (int day = 0; day < daysOfMonth; ++day) {
+      fout << RIGHT << setWidth(FIELD_WIDTH) << (day + 1);
+      dayOfWeek = getSucceedingDayOfWeek(dayOfWeek);
+      if (dayOfWeek == firstDayOfWeekOfLocale) {
+        fout << EOL;
+        fout << LEFT << setWidth(6)
+             << Date::getWeek(day, decomposed.month, decomposed.year);
+      }
+    }
+    fout << ENDL;
+  }
+};
+
+STUB(CalendarApplication);
---
+++ testsuite/demangle.cpp
@@ -0,0 +1,180 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    Copyright (C) 2003 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/Application.h>
+#include <base/TypeInfo.h>
+#include <base/collection/Array.h>
+
+using namespace dk::sdu::mip::base;
+
+class Hello {
+public:
+
+  int myFunction(long long) {
+    return 0;
+  }
+};
+
+class World {
+};
+
+class foo {
+public:
+
+template<class A, class B>
+class bar {
+public:
+
+void test() {
+}
+
+template<class TYPE>
+static void myFunction(foo, bar, Hello, World, World, Hello, void*) {
+}
+
+};
+
+};
+
+typedef int (Hello::*MyMemberType)(long long);
+typedef long Hello::*MyDataMemberType;
+
+MyMemberType myFunction(MyMemberType) {
+  return &Hello::myFunction;
+}
+
+void myFunction2(MyDataMemberType) {
+}
+
+typedef int (MyFunctionType)(int);
+
+template foo::bar<int, MyMemberType>;
+template foo::bar<int, MyFunctionType>;
+
+int test(MyFunctionType, MyFunctionType, MyFunctionType* const) {
+  return 0;
+}
+
+
+class DemangleApplication : public Application {
+private:
+
+  static const unsigned int MAJOR_VERSION = 1;
+  static const unsigned int MINOR_VERSION = 0;
+public:
+
+  DemangleApplication(
+    int numberOfArguments,
+    const char* arguments[],
+    const char* environment[]) throw()
+    : Application("demangle", numberOfArguments, arguments, environment) {
+  }
+
+  void testDemangling(
+    const StringLiteral& mangled, const String& expected) throw() {
+    String demangled = TypeInfo::demangleName(mangled);
+    if (demangled == expected) {
+      fout << "Demangling " << mangled << ": " << demangled << " <OK>" << ENDL;
+    } else {
+      fout << "Demangling " << mangled << ": " << demangled << " <ERROR>: " << expected << ENDL;
+    }
+  }
+
+  void testDemangling() throw() {
+    foo::bar<int, unsigned int>::myFunction<long long>(foo(), foo::bar<int, unsigned int>(), Hello(), World(), World(), Hello(), 0);
+
+    testDemangling(
+      MESSAGE("N3gip7ConvertINS_10ArrayImageIfEENS1_INS_8RGBPixelIhEEEENS_10RGBToFloatEEE"),
+      MESSAGE("gip::Convert<gip::ArrayImage<float>, gip::ArrayImage<gip::RGBPixel<unsigned char> >, gip::RGBToFloat>")
+    );
+
+    // member type
+    testDemangling(MESSAGE("10myFunctionM5HelloFixE"), MESSAGE("myFunction(int (Hello::*)(long long))"));
+    testDemangling(MESSAGE("10myFunctionM5Hellol"), MESSAGE("myFunction(long Hello::*)"));
+    testDemangling(MESSAGE("N7MyClassC1E"), MESSAGE("MyClass::MyClass"));
+    testDemangling(MESSAGE("7MyClassPRrK7MyClass"), MESSAGE("MyClass(MyClass restrict const&*)"));
+
+    testDemangling(MESSAGE("rm1XS_"), MESSAGE("operator%(X, X)"));
+    testDemangling(MESSAGE("plR1XS0_"), MESSAGE("operator+(X&, X&)"));
+    testDemangling(MESSAGE("lsRK1XS1_"), MESSAGE("operator<<(X const&, X const&)"));
+    testDemangling(MESSAGE("N3FooIA4_iE3barE"), MESSAGE("Foo<int[4]>::bar"));
+    testDemangling(MESSAGE("1fIiEvi"), MESSAGE("void f<int>(int)"));
+
+    testDemangling(MESSAGE("5firstI3DuoEvS0_"), MESSAGE("void first<Duo>(Duo)"));
+    testDemangling(MESSAGE("5firstI3DuoEvT_"), MESSAGE("void first<Duo>(Duo)"));
+    testDemangling(MESSAGE("3fooIiFidEiEvv"), MESSAGE("void foo<int, int (*)(double), int>()")); // changed from 3fooIiPFidEiEvv
+    testDemangling(MESSAGE("N1N1fE"), MESSAGE("N::f"));
+    testDemangling(MESSAGE("N6System5Sound4beepEv"), MESSAGE("System::Sound::beep()"));
+    testDemangling(MESSAGE("N5Arena5levelE"), MESSAGE("Arena::level"));
+    testDemangling(MESSAGE("N5StackIiiE5levelE"), MESSAGE("Stack<int, int>::level"));
+    testDemangling(MESSAGE("1fI1XEvPVN1AIT_E1TE"), MESSAGE("void f<X>(A<X>::T volatile*)"));
+    //testDemangling(MESSAGE("ngILi42EEvN1AIXplT_Li2EEE1TE"), MESSAGE("void operator-<42>(A<42+2>::T)"));
+    testDemangling(MESSAGE("4makeI7FactoryiET_IT0_Ev"), MESSAGE("Factory<int> make<Factory, int>()"));
+    testDemangling(MESSAGE("3foo5Hello5WorldS0_S_"), MESSAGE("foo(Hello, World, World, Hello)"));
+    testDemangling(MESSAGE("3fooPM2ABi"), MESSAGE("foo(int AB::**)"));
+    testDemangling(MESSAGE("lsRSoRKSs"), MESSAGE("operator<<(std::ostream&, std::string const&)"));
+
+    testDemangling(MESSAGE("TI7a_class"), MESSAGE("typeinfo for a_class"));
+    testDemangling(
+      MESSAGE("NSt6vectorIS_IiSaIiEESaIS1_EEC1ERKS2_"),
+      MESSAGE("std::vector<std::vector<int, std::allocator<int> >, std::allocator<std::vector<int, std::allocator<int> > > >::vector(std::allocator<std::vector<int, std::allocator<int> > > const&)")
+    );
+    testDemangling(MESSAGE("N1N1TIiiE2mfES0_IddE"), MESSAGE("N::T<int, int>::mf(N::T<double, double>)"));
+    testDemangling(MESSAGE("3foo3bar"), MESSAGE("foo(bar)"));
+    testDemangling(MESSAGE("3foov"), MESSAGE("foo()"));
+    testDemangling(MESSAGE("NSt3_In4wardE"), MESSAGE("std::_In::ward"));
+    testDemangling(MESSAGE("St5state"), MESSAGE("std::state"));
+
+    fout << "Demangling of String: "
+         << TypeInfo::getTypename<String>() << ENDL;
+    fout << "Demangling of Array<int>: "
+         << TypeInfo::getTypename<Array<int> >() << ENDL;
+    fout << "Demangling of Map<String, long long>: "
+         << TypeInfo::getTypename<Map<String, long long> >() << ENDL;
+  }
+
+  void main() throw() {
+    fout << getFormalName() << " version "
+         << MAJOR_VERSION << '.' << MINOR_VERSION << EOL
+         << "The Base Framework (Test Suite)" << EOL
+         << "http://www.mip.sdu.dk/~fonseca/base" << EOL
+         << "Copyright (C) 2003 by Rene Moeller Fonseca <fonseca@mip.sdu.dk>" << EOL
+         << ENDL;
+    
+    testDemangling();
+    
+    fout << Type::getType<char>() << ENDL;
+    fout << Type::getType<wchar>() << ENDL;
+    fout << Type::getType<short>() << ENDL;
+    fout << Type::getType<unsigned short>() << ENDL;
+    fout << Type::getType<int>() << ENDL;
+    fout << Type::getType<unsigned int>() << ENDL;
+    fout << Type::getType<long>() << ENDL;
+    fout << Type::getType<unsigned long>() << ENDL;
+    fout << Type::getType<long long>() << ENDL;
+    fout << Type::getType<unsigned long long>() << ENDL;
+
+    fout << Type::getType<const char*>() << ENDL;
+    fout << Type::getType<const wchar*>() << ENDL;
+    fout << Type::getType<const short*>() << ENDL;
+    fout << Type::getType<const unsigned short*>() << ENDL;
+    fout << Type::getType<const int*>() << ENDL;
+    fout << Type::getType<const unsigned int*>() << ENDL;
+    fout << Type::getType<const long*>() << ENDL;
+    fout << Type::getType<const unsigned long*>() << ENDL;
+    fout << Type::getType<const long long*>() << ENDL;
+    fout << Type::getType<const unsigned long long*>() << ENDL;
+  }
+};
+
+STUB(DemangleApplication);

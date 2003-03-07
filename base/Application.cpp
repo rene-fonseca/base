a7da37ee269a911b88e46acc98124c71a9c5466e
René Fonseca <rene.m.fonseca@gmail.com>
2003-03-07 13:29:33 +0000

    Added context info.

--- base/Application.cpp
+++ base/Application.cpp
@@ -24,8 +24,18 @@
 #  include <windows.h>
 #  undef ERROR // protect against the evil programmers
 #else // unix
+#  if (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__S390)
+#    define __thread // TAG: temp. fix for s390-ibm-linux-gnu
+#  endif
 #  include <sys/signal.h> // defines SIG_ERR on IRIX65
 #  include <signal.h>
+
+#  if (defined(_DK_SDU_MIP__BASE__HAVE_SIGACTION))
+#    include <ucontext.h> // TAG: is this required
+#    if (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__SOLARIS)
+#      include <sys/frame.h>
+#    endif
+#  endif
 #endif
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
@@ -207,7 +217,11 @@ public:
     return FALSE;
   }
 
-  static LRESULT CALLBACK messageHandler(HWND window, UINT message, WPARAM primaryParameter, LPARAM secondaryParameter) throw() {
+  static LRESULT CALLBACK messageHandler(
+    HWND window,
+    UINT message,
+    WPARAM primaryParameter,
+    LPARAM secondaryParameter) throw() {
     // TAG: we should destroy window in destructor
     StringOutputStream stream;
     //stream << "messageHandler: message=" << message << " primary="
@@ -229,8 +243,285 @@ public:
   
 #else // unix
 
-  static void signalHandler(int signal) /*throw(...)*/ {
-    switch (signal) {      
+#  if (defined(_DK_SDU_MIP__BASE__HAVE_SIGACTION))
+  static void actionHandler(
+    int signal, siginfo_t* info, void* opaque) throw() {
+    const char* error = 0;
+
+    const ucontext_t* context = Cast::pointer<const ucontext_t*>(opaque);
+    const mcontext_t* m = &context->uc_mcontext;
+
+    const void* instructionAddress = 0;
+#if ((_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__SOLARIS) && \
+     (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__SPARC))
+    instructionAddress = (void*)(m->gregs[REG_PC]);
+#endif
+    
+    switch (signal) {
+    case SIGFPE:
+      switch (info->si_code) {
+      case FPE_INTDIV:
+        error = "integer divide by zero";
+        break;
+      case FPE_INTOVF:
+        error = "integer overflow";
+        break;
+      case FPE_FLTDIV:
+        error = "floating-point divide by zero";
+        break;
+      case FPE_FLTOVF:
+        error = "floating-point overflow";
+        break;
+      case FPE_FLTUND:
+        error = "floating-point underflow";
+        break;
+      case FPE_FLTRES:
+        error = "floating-point inexact result";
+        break;
+      case FPE_FLTINV:
+        error = "invalid floating-point operation";
+        break;
+      case FPE_FLTSUB:
+        error = "subscript out of range";
+        break;
+      default:
+        break;
+      }
+      ferr << "Internal error: floating-point exception";
+      if (error) {
+        ferr << SP << '(' << NativeString(error) << ')';
+      }
+      ferr << " at address " << setWidth(2 + 2 * sizeof(void*))
+           << instructionAddress << '.' << ENDL;
+      break;
+    case SIGBUS:
+      switch (info->si_code) {
+      case BUS_ADRALN:
+        error = "address alignment";
+        break;
+      case BUS_ADRERR:
+        error = "invalid physical address";
+        break;
+      case BUS_OBJERR:
+        error = "hardware error";
+        break;
+      default:
+        break;
+      }
+      ferr << "Internal error: bus error ";
+      if (error) {
+        ferr << '(' << NativeString(error) << ')';
+      }
+      ferr << ENDL;
+      break;
+    case SIGILL:
+      switch (info->si_code) {
+      case ILL_ILLOPC:
+        error = "illegal opcode";
+        break;
+      case ILL_ILLOPN:
+        error = "illegal operand";
+        break;
+      case ILL_ILLADR:
+        error = "illegal addressing mode";
+        break;
+      case ILL_ILLTRP:
+        error = "illegal trap";
+        break;
+      case ILL_PRVOPC:
+        error = "privileged opcode";
+        break;
+      case ILL_PRVREG:
+        error = "privileged register";
+        break;
+      case ILL_BADSTK:
+        error = "stack error";
+        break;
+      default:
+        break;
+      }
+      ferr << "Internal error: illegal instruction";
+      if (error) {
+        ferr << SP << '(' << NativeString(error) << ')';
+      }
+      ferr << " at address " << setWidth(2 + 2 * sizeof(void*))
+           << info->si_addr << '.' << ENDL;
+      break;
+    case SIGSEGV:
+      switch (info->si_code) {
+      case SEGV_MAPERR:
+        ferr << "Internal error: Segmentation fault due to " \
+                "unmapped memory for memory reference "
+             << setWidth(2 + 2 * sizeof(void*)) << info->si_addr
+             << " at address " << setWidth(2 + 2 * sizeof(void*))
+             << instructionAddress << '.' << ENDL;
+        break;
+      case SEGV_ACCERR:
+        ferr << "Internal error: Segmentation fault due to " \
+                "invalid permissions for memory reference "
+             << setWidth(2 + 2 * sizeof(void*)) << info->si_addr
+             << " at address " << setWidth(2 + 2 * sizeof(void*))
+             << instructionAddress << '.' << ENDL;
+        break;
+      default:
+        ferr << "Internal error: Segmentation fault for memory reference "
+             << setWidth(2 + 2 * sizeof(void*)) << info->si_addr
+             << " at address " << setWidth(2 + 2 * sizeof(void*))
+             << instructionAddress << '.' << ENDL;
+        break;
+      }
+      break;
+    default:
+      ferr << "Internal error: System exception " << signal << '.' << ENDL;
+      exit(Application::EXIT_CODE_INTERNAL_ERROR); // TAG: need other function?
+    }
+    
+#if ((_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__SOLARIS) && \
+     (_DK_SDU_MIP__BASE__ARCH == _DK_SDU_MIP__BASE__SPARC))
+    
+    static const Literal REG_NAMES[8 + 8 + 8 + 8 + 8 + 32] = {
+      Literal("g0"), Literal("g1"), Literal("g2"), Literal("g3"),
+      Literal("g4"), Literal("g5"), Literal("g6"), Literal("g7"),
+      Literal("o0"), Literal("o1"), Literal("o2"), Literal("o3"),
+      Literal("o4"), Literal("o5"), Literal("sp"), Literal("o7"),
+      Literal("l0"), Literal("l1"), Literal("l2"), Literal("l3"),
+      Literal("l4"), Literal("l5"), Literal("l6"), Literal("l7"),
+      Literal("i0"), Literal("i1"), Literal("i2"), Literal("i3"),
+      Literal("i4"), Literal("i5"), Literal("fp"), Literal("i7"),
+      Literal("y"), Literal("psr"), Literal("wim"), Literal("tbr"),
+      Literal("pc"), Literal("npc"), Literal("fpsr"), Literal("cpsr"),
+      Literal("f0"), Literal("f1"), Literal("f2"), Literal("f3"),
+      Literal("f4"), Literal("f5"), Literal("f6"), Literal("f7"),
+      Literal("f8"), Literal("f9"), Literal("f10"), Literal("f11"),
+      Literal("f12"), Literal("f13"), Literal("f14"), Literal("f15"),
+      Literal("f16"), Literal("f17"), Literal("f18"), Literal("f19"),
+      Literal("f20"), Literal("f21"), Literal("f22"), Literal("f23"),
+      Literal("f24"), Literal("f25"), Literal("f26"), Literal("f27"),
+      Literal("f28"), Literal("f29"), Literal("f30"), Literal("f31")
+    };
+    
+    ferr << EOL;
+    ferr << RIGHT << setWidth(4) << REG_NAMES[36] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_PC] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[37] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_nPC] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[32] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_Y] << EOL
+      
+         << RIGHT << setWidth(4) << REG_NAMES[0] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[0] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[1] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G1] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[2] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G2] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[3] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G3] << EOL
+      
+         << RIGHT << setWidth(4) << REG_NAMES[4] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G4] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[5] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G5] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[6] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G6] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[7] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_G7] << EOL
+      
+         << RIGHT << setWidth(4) << REG_NAMES[8] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O0] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[9] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O1] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[10] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O2] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[11] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O3] << EOL
+      
+         << RIGHT << setWidth(4) << REG_NAMES[12] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O4] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[13] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O5] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[14] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O6] << indent(4)
+         << RIGHT << setWidth(4) << REG_NAMES[15] << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->gregs[REG_O7] << EOL
+         << EOL;
+    
+    void* framePointer = 0;
+    // TAG: check if valid pointer - what constraints
+    if (framePointer) {
+      const uint32* window = Cast::pointer<const uint32*>(framePointer);
+      for (unsigned int i = 0; i < 16; ++i) {
+        ferr << RIGHT << setWidth(4) << REG_NAMES[16+i] << ':'
+             << HEX << ZEROPAD << PREFIX << setWidth(10)
+             << window[i];
+        if ((i % 4) == 3) {
+          ferr << EOL;
+        } else {
+          ferr << indent(4);
+        }
+      }
+      ferr << EOL;
+    }
+    
+    ferr << RIGHT << setWidth(4) << "fpsr" << ':'
+         << HEX << ZEROPAD << PREFIX << setWidth(10)
+         << m->fpregs.fpu_fsr << indent(4)
+         << EOL;
+    
+    for (unsigned int i = 0; i < 32; ++i) {
+      ferr << RIGHT << setWidth(4) << REG_NAMES[40 + i] << ':'
+           << HEX << ZEROPAD << PREFIX << setWidth(10)
+           << m->fpregs.fpu_fr.fpu_regs[i];
+      if ((i % 4) == 3) {
+        ferr << EOL;
+      } else {
+        ferr << indent(4);
+      }
+    }
+    
+#if (_DK_SDU_MIP__BASE__ARCH_MINOR == _DK_SDU_MIP__BASE__SPARCV9)
+#endif // SPARCV9
+    
+    ferr << EOL << "stack frames:" << EOL;
+
+    const struct frame* f = (const struct frame*)(framePointer);
+    while (f && ((((uint32)f) & 7) == 0)) {
+      ferr << (void*)(f->fr_savfp) << SP << (void*)(f->fr_savpc) << SP << ENDL;
+      f = f->fr_savfp;
+      // TAG: must be ascending
+    }
+    // TAG: must end with 0
+    f = Cast::pointer<const struct frame*>(framePointer);
+    
+    ferr << EOL << "stack content:" << EOL;
+    
+    ferr << FLUSH;
+    
+#endif // Solaris && SPARC
+    exit(Application::EXIT_CODE_INTERNAL_ERROR); // TAG: need other function?
+  }
+#  endif
+  
+  static void signalHandler(int signal) throw() {
+    switch (signal) {
     case SIGHUP: // hangup
       if (Thread::getThread()->isMainThread()) {
         SystemLogger::write(SystemLogger::INFORMATION, "Hangup signal.");
@@ -257,7 +548,8 @@ public:
       break;
     case SIGSEGV:
       SystemLogger::write(SystemLogger::INFORMATION, "Segmentation fault.");
-      throw MemoryException("Invalid memory access");
+      throw MemoryException("Invalid memory access"); // TAG: remove
+      abort();
     case SIGILL:
       SystemLogger::write(SystemLogger::INFORMATION, "Invalid instruction.");
       throw Exception("Invalid instruction");
@@ -305,7 +597,10 @@ public:
 
 void Application::initialize() throw() {
   static unsigned int singleton = 0;
-  assert(singleton == 0, SingletonException("Application has been instantiated", this));
+  assert(
+    singleton == 0,
+    SingletonException("Application has been instantiated", this)
+  );
   ++singleton;
 
   // install signal handler
@@ -325,7 +620,9 @@ void Application::initialize() throw() {
   OSVERSIONINFO versionInfo;
   versionInfo.dwOSVersionInfoSize = sizeof(versionInfo);
   ::GetVersionEx(&versionInfo); // never fails
-  bool isWindows2000OrLater = (versionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT) && (versionInfo.dwMajorVersion >= 5);
+  bool isWindows2000OrLater =
+    (versionInfo.dwPlatformId == VER_PLATFORM_WIN32_NT) &&
+    (versionInfo.dwMajorVersion >= 5);
 
   HWND messageWindow = ::CreateWindowEx(
     0, // no extended window style
@@ -343,7 +640,23 @@ void Application::initialize() throw() {
   );
   
 #else // unix
-#  if (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__IRIX65)
+#  if (defined(_DK_SDU_MIP__BASE__HAVE_SIGACTION))
+  static const int SIGNALS[] = {
+    SIGHUP, SIGPIPE, SIGTERM, SIGCHLD, SIGQUIT, SIGINT, SIGABRT, SIGSEGV,
+    SIGILL, SIGFPE, SIGBUS
+  };
+  
+  struct sigaction action;
+  clear(action);
+  action.sa_flags = SA_SIGINFO;
+  action.sa_sigaction = ApplicationImpl::actionHandler;
+  for (unsigned int i = 0; i < getArraySize(SIGNALS); ++i) {
+    assert(
+      sigaction(SIGNALS[i], &action, 0) == 0,
+      UnexpectedFailure("Unable to register signal handler", this)
+    );
+  }
+#  elif (_DK_SDU_MIP__BASE__OS == _DK_SDU_MIP__BASE__IRIX65)
     if (!((bsd_signal(SIGHUP, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (bsd_signal(SIGPIPE, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (bsd_signal(SIGTERM, ApplicationImpl::signalHandler) != SIG_ERR) &&
@@ -354,11 +667,9 @@ void Application::initialize() throw() {
           (bsd_signal(SIGILL, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (bsd_signal(SIGFPE, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (bsd_signal(SIGABRT, ApplicationImpl::signalHandler) != SIG_ERR))) {
-      throw UnexpectedFailure("Unable to install signal handler", this);
+      throw UnexpectedFailure("Unable to register signal handler", this);
     }
 #  else
-    // TAG: need handler for segfault...
-    // TAG: new applications should use sigaction
     if (!((signal(SIGHUP, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (signal(SIGPIPE, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (signal(SIGTERM, ApplicationImpl::signalHandler) != SIG_ERR) &&
@@ -369,7 +680,7 @@ void Application::initialize() throw() {
           (signal(SIGILL, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (signal(SIGFPE, ApplicationImpl::signalHandler) != SIG_ERR) &&
           (signal(SIGABRT, ApplicationImpl::signalHandler) != SIG_ERR))) {
-      throw UnexpectedFailure("Unable to install signal handler", this);
+      throw UnexpectedFailure("Unable to register signal handler", this);
     }
 #  endif
 #endif

eb8ddfd73fe10f5cc293363a6d6939f580f1fa13
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-07 19:27:29 +0200

    Fixed Set template instances.

--- base/collection/Set.cpp
+++ base/collection/Set.cpp
@@ -18,24 +18,21 @@ _DK_SDU_MIP__BASE__DUMMY_SYMBOL
 
 _DK_SDU_MIP__BASE__ENTER_NAMESPACE
 
-namespace {
+// instantiate common types
+// TAG: defined common types for containers: bool, char, uint8, wchar, short, unsigned short, int, unsigned int, int64, uint64, std::string, std::wstring, float, double, long double
 
-  // instantiate common types
-  // TAG: defined common types for containers: bool, char, uint8, wchar, short, unsigned short, int, unsigned int, int64, uint64, std::string, std::wstring, float, double, long double
+template class Set<int>;
+template class Set<unsigned int>;
 
-  template class Set<int>;
-  template class Set<unsigned int>;
+template class Set<char>;
+template class Set<wchar>;
+template class Set<void*>;
 
-  template class Set<char>;
-  template class Set<wchar>;
-  template class Set<void*>;
+template class Set<float>;
+template class Set<double>;
+template class Set<long double>;
 
-  template class Set<float>;
-  template class Set<double>;
-  template class Set<long double>;
-
-  template class Set<std::string>;
-  template class Set<std::wstring>;
-}
+template class Set<std::string>;
+template class Set<std::wstring>;
 
 _DK_SDU_MIP__BASE__LEAVE_NAMESPACE

177ed27b0ff131dc70962f23f9a50d8528e70551
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-16 13:53:15 +0200

    Added Parser.

---
+++ base/string/Parser.h
@@ -0,0 +1,158 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#pragma once
+
+#include <base/mem/NullPointer.h>
+#include <base/Primitives.h>
+#include <base/Object.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+/** Parse exception. */
+class _COM_AZURE_DEV__BASE__API ParseException : public Exception {
+public:
+  
+  inline ParseException() {
+  }
+  
+  inline ParseException(const char* message) : Exception(message) {
+  }
+};
+
+/** Parser. */
+class _COM_AZURE_DEV__BASE__API Parser /*: public Object*/ {
+private:
+  
+  // TAG: add support for skipping UTF-8 chars
+  const uint8* src = nullptr;
+  const uint8* end = nullptr;
+public:
+  
+  /** Constructs parser. */
+  Parser() noexcept
+  {
+  }
+
+  /** Constructs parser. */
+  Parser(const uint8* _src, const uint8* _end) noexcept
+    : src(_src), end(_end)
+  {
+    ASSERT(src <= end);
+  }
+  
+  /** Returns the current marker. */
+  inline const uint8* getCurrent() const noexcept
+  {
+    return src;
+  }
+  
+  /** Returns true if more chars available. */
+  inline bool hasMore() const noexcept
+  {
+    return src != end;
+  }
+  
+  /** Returns the next char without reading it. */
+  inline char peek() const
+  {
+    if (src == end) {
+      throw ParseException("Unexpected end reached.");
+    }
+    return *src;
+  }
+
+  /** Returns true if the next char is as expected without reading it. */
+  inline bool peek(char ch) const noexcept
+  {
+    return peek() == ch;
+  }
+
+  /** Skip next char. */
+  inline void skip()
+  {
+    if (src == end) {
+      throw ParseException("Unexpected end reached.");
+    }
+    char result = *src;
+    ++src;
+  }
+
+  /** Returns the next char. */
+  inline char read()
+  {
+    if (src == end) {
+      throw ParseException("Unexpected end reached.");
+    }
+    const char result = *src;
+    ++src;
+    return result;
+  }
+
+  /** Reads the given char. */
+  inline void read(char ch)
+  {
+    if (read() != ch) {
+      throw ParseException("Unexpected char.");
+    }
+  }
+
+  /** Reads the given text. */
+  inline void read(const char* text)
+  {
+    if (!text) {
+      throw NullPointer();
+    }
+    while (*text) {
+      read(*text++);
+    }
+  }
+  
+  /** Skips JSON space. */
+  inline void skipJSONSpaces() noexcept
+  {
+    while (src != end) {
+      switch (peek()) {
+      case ' ':
+      case '\n':
+      case '\r':
+      case '\t':
+        break;
+      default:
+        return;
+      }
+      ++src;
+    }
+  }
+  
+  /** Skip any of the given chars until no more. */
+  inline void skipAny(const char* chars) noexcept
+  {
+    while (src != end) {
+      const char ch = peek();
+      bool skip = false;
+      for (const char* s = chars; *s; ++s) {
+        if (ch == *s) {
+          skip = true;
+          break;
+        }
+      }
+      if (!skip) {
+        return;
+      }
+      ++src;
+    }
+  }
+};
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

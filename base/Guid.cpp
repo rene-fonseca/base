005eb28bec8c2196dee010163426d8fa4725f34c
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-18 15:55:31 +0200

    Added Guid class.

---
+++ base/Guid.cpp
@@ -0,0 +1,151 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/Guid.h>
+#include <base/string/StringOutputStream.h>
+
+#if (_COM_AZURE_DEV__BASE__FLAVOR == _COM_AZURE_DEV__BASE__WIN32)
+#  include <windows.h>
+#endif
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+Guid::Guid() throw() {
+  clear(guid);
+}
+
+Guid::Guid(const uint8 (&value)[16]) throw()
+{
+  copy<uint8>(guid, value, sizeof(guid));
+}
+
+namespace {
+
+  uint8 getGuidByte(const uint8* src)
+  {
+    if (!ASCIITraits::isHexDigit(src[0]) || !ASCIITraits::isHexDigit(src[1])) {
+      throw InvalidFormat("Invalid guid.");
+    }
+    return (ASCIITraits::digitToValue(src[0]) << 4) | ASCIITraits::digitToValue(src[1]);
+  }
+}
+
+Guid Guid::getGuid(const char* text)
+{
+  Guid guid;
+  // {B429A864-CDFC-4044-BCC1-BDDE18EAF8DE}
+  if (NativeString(text).getLength() != 38) { // handles nullptr
+    throw InvalidFormat("Invalid guid.");
+  }
+  if ((text[0] != '{') || (text[37] != '}') ||
+      (text[9] != '-') || (text[14] != '-') || (text[19] != '-') || (text[26] != '-')) {
+    throw InvalidFormat("Invalid guid.");
+  }
+
+  static const uint8 OFFSETS[16] = {1, 3, 5, 7, 10, 12, 15, 17, 20, 22, 25, 27, 29, 31, 33, 35};
+  for (unsigned int i = 0; i < SIZE; ++i) {
+    guid.guid[i] = getGuidByte(reinterpret_cast<const uint8*>(text) + OFFSETS[i]);
+  }
+
+  return guid;
+}
+
+Guid::Guid(const Guid& _copy) noexcept
+{
+  copy<uint8>(guid, _copy.guid, sizeof(guid));
+}
+
+Guid& Guid::operator=(const Guid& eq) noexcept
+{
+  if (&eq != this) {
+    copy<uint8>(guid, eq.guid, sizeof(guid));
+  }
+  return *this;
+}
+
+bool Guid::operator==(const Guid& eq) const noexcept
+{
+  return compare<uint8>(guid, eq.guid, sizeof(guid)) == 0;
+}
+
+bool Guid::operator!=(const Guid& eq) const noexcept
+{
+  return compare<uint8>(guid, eq.guid, sizeof(guid)) != 0;
+}
+
+bool Guid::isNull() const noexcept
+{
+  for (MemorySize i = 0; i < sizeof(guid); ++i) {
+    if (guid[i] != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+String Guid::createGuidAsString()
+{
+  return createGuid().toString();
+}
+
+Guid Guid::createGuid() noexcept
+{
+  Guid result;
+
+#if (_COM_AZURE_DEV__BASE__FLAVOR == _COM_AZURE_DEV__BASE__WIN32)
+  GUID _guid;
+  HRESULT status = CoCreateGuid(&_guid);
+  if (status != S_OK) {
+    return result;
+  }
+  ASSERT(sizeof(_guid) == sizeof(result.guid)); // for C++17 static_assert();
+  copy<uint8>(result.guid, reinterpret_cast<const uint8*>(&_guid), sizeof(result.guid));
+#else
+  ASSERT(!"Not implemented.");
+#endif
+  return result;
+}
+
+String Guid::toString() const
+{
+  // {B429A864-CDFC-4044-BCC1-BDDE18EAF8DE}
+  // TAG: optimize
+  StringOutputStream s;
+  s << HEX << setWidth(2) << ZEROPAD << NOPREFIX
+    << '{'
+    << guid[0]
+    << guid[1]
+    << guid[2]
+    << guid[3] << '-'
+    << guid[4]
+    << guid[5] << '-'
+    << guid[6]
+    << guid[7] << '-'
+    << guid[8]
+    << guid[9] << '-'
+    << guid[10]
+    << guid[11]
+    << guid[12]
+    << guid[13]
+    << guid[14]
+    << guid[15]
+    << '}' << FLUSH;
+  return s.getString();
+}
+
+FormatOutputStream& operator<<(FormatOutputStream& stream, const Guid& guid)
+{
+  return stream << guid.toString();
+}
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
---
+++ base/Guid.h
@@ -0,0 +1,84 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#pragma once
+
+#include <base/OutOfDomain.h>
+#include <base/string/FormatOutputStream.h>
+#include <base/string/InvalidFormat.h>
+#include <base/Primitives.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+/**
+  Guid. Used as unique identifier.
+*/
+
+class _COM_AZURE_DEV__BASE__API Guid {
+public:
+
+  enum {
+    SIZE = 16
+  };
+private:
+
+  /** The guid. */
+  uint8 guid[SIZE];
+public:
+
+  /** Returns a likely unique guid. */
+  static Guid createGuid() noexcept;
+
+  static String createGuidAsString();
+
+  static Guid getGuid(const char* _guid);
+
+  /** Initializes as null. */
+  Guid() noexcept;
+  
+  Guid(const uint8 (&_guid)[SIZE]) noexcept;
+
+  Guid(const Guid& _copy) noexcept;
+  
+  Guid& operator=(const Guid& eq) noexcept;
+
+  /**
+    Returns true if the identifiers are identical.
+  */
+  bool operator==(const Guid& eq) const throw();
+
+  /**
+    Returns true if the identifiers are different.
+  */
+  bool operator!=(const Guid& eq) const throw();
+
+  /** Returns true if the guid is null. */
+  bool isNull() const noexcept;
+
+  /** Converts the guid to a string. E.g. "{B429A864-CDFC-4044-BCC1-BDDE18EAF8DE}" */
+  String toString() const;
+
+  /**
+    Returns the identifier.
+  */
+  inline const uint8* getBytes() const throw() {
+    return guid;
+  }
+};
+
+/**
+  Writes the guid to the format output stream. E.g. {B429A864-CDFC-4044-BCC1-BDDE18EAF8DE}.
+*/
+_COM_AZURE_DEV__BASE__API FormatOutputStream& operator<<(FormatOutputStream& stream, const Guid& value);
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

477f57fb65a5158e5f13cad8c711393523fbc333
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-25 14:17:17 +0200

    Added UTF-8 iterator and enumerator.

---
+++ base/iterator/UTF8Iterator.cpp
@@ -0,0 +1,20 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/iterator/UTF8Iterator.h>
+
+_COM_AZURE_DEV__BASE__DUMMY_SYMBOL
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
---
+++ base/iterator/UTF8Iterator.h
@@ -0,0 +1,208 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#pragma once
+
+#include <base/Iterator.h>
+#include <base/Primitives.h>
+#include <base/string/Unicode.h>
+#include <base/string/MultibyteException.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+// TAG: only uint8 is required - char can be cast
+
+/**
+  Iterator used to traverse UTF-8.
+*/
+class UTF8Iterator {
+protected:
+
+  typedef const uint8* Pointer;
+  typedef MemoryDiff Distance;
+
+  /** The position of the iterator. */
+  Pointer element = nullptr;
+
+  /** Reads UCS4 (doesnt check if UCS4 code is disallowed). */
+  static Pointer next(Pointer src, ucs4& ch)
+  {
+    const int status = Unicode::readUCS4(src, ch);
+    if (status > 0) {
+      src += status;
+    } else {
+      switch (status) {
+      case Unicode::UTF8_ERROR_INCOMPLETE:
+        throw MultibyteException(MultibyteException::INCOMPLETE_CHARACTER_CODE);
+      case Unicode::UTF8_ERROR_BAD_ENCODING:
+        throw MultibyteException(MultibyteException::INVALID_CHARACTER_CODE);
+      default:
+        throw MultibyteException();
+      }
+    }
+    return src;
+  }
+public:
+
+  /**
+    Initializes iterator.
+
+    @param value The initial value of the iterator.
+  */
+  inline UTF8Iterator(Pointer value) noexcept
+    : element(value) {
+  }
+
+  /**
+    Initializes iterator from other iterator.
+  */
+  inline UTF8Iterator(const UTF8Iterator& copy) noexcept
+    : element(copy.element)
+  {
+  }
+  
+  /**
+    Initializes iterator from other iterator.
+  */
+  inline UTF8Iterator& operator=(const UTF8Iterator& eq) noexcept
+  {
+    element = eq.element;
+    return *this;
+  }
+
+  /**
+    Prefix increment.
+  */
+  UTF8Iterator& operator++()
+  {
+    ucs4 ch = 0;
+    element = next(element, ch);
+    return *this;
+  }
+
+  /**
+    Postfix increment.
+  */
+  UTF8Iterator operator++(int) noexcept
+  {
+    UTF8Iterator result(*this);
+    ucs4 ch = 0;
+    element = next(element, ch);
+    return result;
+  }
+
+  /**
+    Moves the specified distance forward.
+  */
+  UTF8Iterator& operator+=(Distance distance) noexcept
+  {
+    ASSERT(distance >= 0);
+    while (distance--) {
+      ++element;
+    }
+    return *this;
+  }
+  
+  /**
+    Returns true if the iterators are equal.
+  */
+  inline bool operator==(const UTF8Iterator& eq) const noexcept
+  {
+    return element == eq.element;
+  }
+
+  /**
+    Returns true if the iterators aren't equal.
+  */
+  inline bool operator!=(const UTF8Iterator& eq) const noexcept
+  {
+    return element != eq.element;
+  }
+
+  /**
+    Returns true if this iterator is less than the specified iterator.
+  */
+  inline bool operator<(const UTF8Iterator& eq) const noexcept
+  {
+    return element < eq.element;
+  }
+
+  inline bool operator<=(const UTF8Iterator& eq) const noexcept
+  {
+    return element <= eq.element;
+  }
+  
+  /**
+    Returns true if this iterator is greater than or equal to the specified
+    iterator.
+  */
+  inline bool operator>=(const UTF8Iterator& eq) const noexcept
+  {
+    return element >= eq.element;
+  }
+
+  inline bool operator>(const UTF8Iterator& eq) const noexcept
+  {
+    return element > eq.element;
+  }
+  
+  /**
+    Access the element.
+  */
+  ucs4 operator*() const
+  {
+    ucs4 ch = 0;
+    next(element, ch);
+    return ch;
+  }
+
+  /**
+    Returns the element at the specified index from this element.
+  */
+  ucs4 operator[](Distance index) const
+  {
+    auto copy = *this;
+    copy += index;
+    return *copy;
+  }
+};
+
+/** UTF-8 enumerator. */
+class UTF8Enumerator {
+private:
+
+  UTF8Iterator src = nullptr;
+  UTF8Iterator end = nullptr;
+public:
+
+  inline UTF8Enumerator() noexcept
+  {
+  }
+
+  inline UTF8Enumerator(const UTF8Iterator& _begin, const UTF8Iterator& _end) noexcept
+    : src(_begin), end(_end)
+  {
+  }
+
+  inline bool hasNext() const noexcept
+  {
+    return src != end;
+  }
+
+  inline ucs4 next()
+  {
+    *src++;
+  }
+};
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

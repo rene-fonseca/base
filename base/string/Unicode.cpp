efea363be18f35c283d6630d5cd54f3ea1aa6513
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-25 14:16:49 +0200

    Extended Unicode class.

--- base/string/Unicode.cpp
+++ base/string/Unicode.cpp
@@ -13,8 +13,41 @@
 
 #include <base/string/Unicode.h>
 
-_COM_AZURE_DEV__BASE__DUMMY_SYMBOL
-
 _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+MemoryDiff Unicode::getUTF8StringLength(const uint8* src, const uint8* end) noexcept
+{
+  MemoryDiff length = 0;
+  while (src != end) {
+    ucs4 ch = 0;
+    auto status = readUCS4(src, end, ch);
+    if (status > 0) {
+      src += status;
+      ++length;
+      continue;
+    }
+    return status;
+  }
+  return length;
+}
+
+MemoryDiff Unicode::getUTF8StringLength(const uint8* src) noexcept
+{
+  if (!src || !*src) {
+    return 0;
+  }
+  MemoryDiff length = 0;
+  while (*src) {
+    ucs4 ch = 0;
+    auto status = readUCS4(src, ch);
+    if (status > 0) {
+      src += status;
+      ++length;
+      continue;
+    }
+    return status;
+  }
+  return length;
+}
 
 _COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
--- base/string/Unicode.h
+++ base/string/Unicode.h
@@ -25,7 +25,8 @@ class _COM_AZURE_DEV__BASE__API Unicode {
 public:
 
   /** Returns true if the code is a valid UCS4 code. */
-  static inline bool isUCS4(unsigned int value) throw() {
+  static inline bool isUCS4(unsigned int value) noexcept
+  {
     if (value < 0xd800) {
       return true;
     } else if (value < 0x10000) {
@@ -50,7 +51,7 @@ public:
 
     @return Bytes read. If negative this is the error code.
   */
-  static inline int readUCS4(const uint8* src, const uint8* end, ucs4& ch)
+  static inline int readUCS4(const uint8* src, const uint8* end, ucs4& ch) noexcept
   {
     if (src == end) {
       return UTF8_ERROR_EMPTY;
@@ -141,6 +142,105 @@ public:
     }
     return UTF8_ERROR_BAD_ENCODING;
   }
+
+  /** Read UCS4 from null terminated sequence. */
+  static inline int readUCS4(const uint8* src, ucs4& ch) noexcept
+  {
+    if (!src || !*src) {
+      return UTF8_ERROR_EMPTY;
+    }
+
+    uint8 value = *src++;
+    if ((value & 0x80) == 0x00) {
+      ch = value;
+      return 1;
+    }
+
+    if ((value & 0xe0) == 0xc0) {
+      ucs4 code = value & ~0xe0;
+      if (!*src) {
+        return UTF8_ERROR_INCOMPLETE;
+      }
+      uint8 temp = *src++;
+      if ((temp & 0xc0) != 0x80) {
+        return UTF8_ERROR_BAD_ENCODING;
+      }
+      code = (code << 6) | temp;
+      ch = code;
+      return 2;
+    }
+
+    if ((value & 0xf0) == 0xe0) {
+      ucs4 code = value & ~0xf0;
+      for (int i = 0; i < 2; ++i) {
+        if (!*src) {
+          return UTF8_ERROR_INCOMPLETE;
+        }
+        uint8 temp = *src++;
+        if ((temp & 0xc0) != 0x80) {
+          return UTF8_ERROR_BAD_ENCODING;
+        }
+        code = (code << 6) | temp;
+      }
+      ch = code;
+      return 3;
+    }
+
+    if ((value & 0xf8) == 0xf0) {
+      ucs4 code = value & ~0xf8;
+      for (int i = 0; i < 3; ++i) {
+        if (!*src) {
+          return UTF8_ERROR_INCOMPLETE;
+        }
+        uint8 temp = *src++;
+        if ((temp & 0xc0) != 0x80) {
+          return UTF8_ERROR_BAD_ENCODING;
+        }
+        code = (code << 6) | temp;
+      }
+      ch = code;
+      return 4;
+    }
+
+    if ((value & 0xfc) == 0xf8) {
+      ucs4 code = value & ~0xfc;
+      for (int i = 0; i < 4; ++i) {
+        if (!*src) {
+          return UTF8_ERROR_INCOMPLETE;
+        }
+        uint8 temp = *src++;
+        if ((temp & 0xc0) != 0x80) {
+          return UTF8_ERROR_BAD_ENCODING;
+        }
+        code = (code << 6) | temp;
+      }
+      ch = code;
+      return 5;
+    }
+
+    if ((value & 0xfe) == 0xfc) {
+      ucs4 code = value & ~0xfe;
+      for (int i = 0; i < 5; ++i) {
+        if (!*src) {
+          return UTF8_ERROR_INCOMPLETE;
+        }
+        uint8 temp = *src++;
+        if ((temp & 0xc0) != 0x80) {
+          return UTF8_ERROR_BAD_ENCODING;
+        }
+        code = (code << 6) | temp;
+      }
+      ch = code;
+      return 6;
+    }
+    return UTF8_ERROR_BAD_ENCODING;
+  }
+
+  /** Validates if the the given string is using valid UTF-8 encoding. Returns the number of characters if valid. Otherwise returns negative status. */
+  static MemoryDiff getUTF8StringLength(const uint8* src, const uint8* end) noexcept;
+
+  /** Validates if the the given null-terminated string is using valid UTF-8 encoding. Returns the number of characters if valid. Otherwise returns negative status. */
+  static MemoryDiff getUTF8StringLength(const uint8* src) noexcept;
 
   // TAG: add method for converting ucs4 to bytes
 };

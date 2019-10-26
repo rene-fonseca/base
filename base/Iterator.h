8c7e93a5fabcd9bf2f7abf5ada649d1fd9231049
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-26 17:32:32 +0200

    Std iterator traits compatibility.

--- base/Iterator.h
+++ base/Iterator.h
@@ -13,9 +13,25 @@
 
 #pragma once
 
-#include <base/features.h>
+#include <base/Primitives.h>
+#include <iterator>
 
 _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+/** Imports typedefs Value, Pointer, Reference, Distance, and Category from iterator traits into iterator. */
+#define _COM_AZURE_DEV__BASE__IMPORT_ITERATOR_TRAITS(TRAITS) \
+  typedef typename TRAITS::Value Value; \
+  typedef typename TRAITS::Distance Distance; \
+  typedef typename TRAITS::Reference Reference; \
+  typedef typename TRAITS::Pointer Pointer; \
+  typedef typename TRAITS::Category Category;
+
+class InputIterator {};
+class OutputIterator {};
+class ForwardIterator : public InputIterator {};
+class BidirectionalIterator : public ForwardIterator {};
+class RandomAccessIterator : public BidirectionalIterator {};
+class ContiguousIterator : public RandomAccessIterator {};
 
 /**
   Defines the types of a modifying (has write access to the elements) iterator.
@@ -30,7 +46,8 @@ public:
   typedef VALUE Value;
   typedef VALUE& Reference;
   typedef VALUE* Pointer;
-  typedef unsigned int Distance;
+  typedef MemoryDiff Distance;
+  typedef RandomAccessIterator Category; // TAG: how to handle
 };
 
 /**
@@ -47,7 +64,8 @@ public:
   typedef VALUE Value;
   typedef const VALUE& Reference;
   typedef const VALUE* Pointer;
-  typedef unsigned int Distance;
+  typedef MemoryDiff Distance;
+  typedef RandomAccessIterator Category; // TAG: how to handle
 };
 
 /**
@@ -74,6 +92,95 @@ public:
   typedef typename TRAITS::Reference Reference;
   /** The type of a pointer to an element. */
   typedef typename TRAITS::Pointer Pointer;
+  /** The iterator category. */
+  typedef typename TRAITS::Category Category;
 };
 
+/** Convert base to std iterator category. */
+template<class ITERATOR_CATEGORY>
+class GetStdIteratorCategory {
+public:
+
+  typedef void iterator_category;
+};
+
+template<>
+class GetStdIteratorCategory<InputIterator> {
+public:
+
+  typedef std::input_iterator_tag iterator_category;
+};
+
+template<>
+class GetStdIteratorCategory<OutputIterator> {
+public:
+
+  typedef std::output_iterator_tag iterator_category;
+};
+
+template<>
+class GetStdIteratorCategory<ForwardIterator> {
+public:
+
+  typedef std::forward_iterator_tag iterator_category;
+};
+
+template<>
+class GetStdIteratorCategory<BidirectionalIterator> {
+public:
+
+  typedef std::bidirectional_iterator_tag iterator_category;
+};
+
+template<>
+class GetStdIteratorCategory<RandomAccessIterator> {
+public:
+
+  typedef std::random_access_iterator_tag iterator_category;
+};
+
+#if 0
+template<>
+class GetStdIteratorCategory<ContiguousIterator> {
+public:
+
+  typedef std::contiguous_iterator_tag iterator_category;
+};
+#endif
+
 _COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
+
+/** Add std iterator traits for given base iterator. */
+#define _COM_AZURE_DEV__BASE__STD_ITERATOR_TRAITS(ITERATOR) \
+namespace std {\
+\
+  template<class TYPE> \
+  struct iterator_traits<typename ITERATOR<TYPE> > { \
+    typedef typename ITERATOR<TYPE>::Value value_type; \
+    typedef typename ITERATOR<TYPE>::Pointer pointer; \
+    typedef typename ITERATOR<TYPE>::Reference reference; \
+    typedef typename ITERATOR<TYPE>::Distance difference_type; \
+    typedef typename base::GetStdIteratorCategory<typename ITERATOR<TYPE>::Category>::iterator_category iterator_category; \
+  }; \
+}
+
+namespace std {
+
+  template<class TRAITS>
+  struct iterator_traits<typename base::IteratorTraits<TRAITS> > {
+    typedef typename TRAITS::Value value_type;
+    typedef typename TRAITS::Pointer pointer;
+    typedef typename TRAITS::Reference reference;
+    typedef typename TRAITS::Distance difference_type;
+    typedef typename base::GetStdIteratorCategory<typename TRAITS::Category>::iterator_category iterator_category;
+  };
+
+  template<class TRAITS>
+  struct iterator_traits<typename base::ReadIteratorTraits<TRAITS> > {
+    typedef typename TRAITS::Value value_type;
+    typedef typename TRAITS::Pointer pointer;
+    typedef typename TRAITS::Reference reference;
+    typedef typename TRAITS::Distance difference_type;
+    typedef typename base::GetStdIteratorCategory<typename TRAITS::Category>::iterator_category iterator_category;
+  };
+}

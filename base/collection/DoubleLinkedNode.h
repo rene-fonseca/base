1fe627d27cae3c1a29d278df2d3a63c34212ec9d
René Fonseca <rene.m.fonseca@gmail.com>
2019-10-27 18:26:08 +0100

    Added iterators for SingleLinkedNode and DoubleLinkedNode.

--- base/collection/DoubleLinkedNode.h
+++ base/collection/DoubleLinkedNode.h
@@ -13,7 +13,7 @@
 
 #pragma once
 
-#include <base/features.h>
+#include <base/Iterator.h>
 
 _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
 
@@ -116,6 +116,355 @@ public:
   {
     this->value = value;
   }
+
+  inline TYPE& operator*() noexcept
+  {
+    return *value;
+  }
+
+  inline const TYPE& operator*() const noexcept
+  {
+    return *value;
+  }
+};
+
+template<class TYPE>
+class DoubleLinkedNodeIterator {
+public:
+
+  typedef TYPE Value;
+  typedef TYPE* Pointer;
+  typedef TYPE& Reference;
+  typedef MemoryDiff Distance;
+  typedef BidirectionalIterator Category;
+protected:
+
+  DoubleLinkedNode<TYPE>* node = nullptr;
+  _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_ADD_CONTEXT();
+public:
+
+  /**
+    Initializes iterator.
+
+    @param value The initial value of the iterator.
+  */
+  explicit inline DoubleLinkedNodeIterator(DoubleLinkedNode<TYPE>* _node) noexcept
+    : node(_node)
+  {
+  }
+
+  /**
+    Initializes iterator from other iterator.
+  */
+  inline DoubleLinkedNodeIterator(const DoubleLinkedNodeIterator& copy) noexcept
+    : node(copy.node)
+  {
+  }
+
+  /**
+    Initializes iterator from other iterator.
+  */
+  inline DoubleLinkedNodeIterator& operator=(const DoubleLinkedNodeIterator& copy) noexcept
+  {
+    node = copy.node;
+    return *this;
+  }
+
+  /**
+    Prefix increment.
+  */
+  inline DoubleLinkedNodeIterator& operator++() noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    ASSERT(node);
+    node = node->getNext();
+    return *this;
+  }
+
+  /**
+    Postfix increment.
+  */
+  inline DoubleLinkedNodeIterator operator++(int) noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    DoubleLinkedNodeIterator result(*this);
+    ASSERT(node);
+    node = node->getNext();
+    return result;
+  }
+
+  /**
+    Prefix decrement.
+  */
+  inline DoubleLinkedNodeIterator& operator--() noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    ASSERT(node);
+    node = node->getPrevious();
+    return *this;
+  }
+
+  /**
+    Postfix decrement.
+  */
+  inline DoubleLinkedNodeIterator operator--(int) noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    DoubleLinkedNodeIterator result(*this);
+    ASSERT(node);
+    node = node->getPrevious();
+    return result;
+  }
+
+  /**
+    Moves the specified distance forward.
+  */
+  DoubleLinkedNodeIterator& operator+=(Distance distance) noexcept
+  {
+    if (distance < 0) {
+      while (distance++) {
+        --(*this);
+      }
+    } else {
+      while (distance--) {
+        ++(*this);
+      }
+    }
+    return *this;
+  }
+
+  /**
+    Moves the specified distance backwards.
+  */
+  DoubleLinkedNodeIterator& operator-=(Distance distance) noexcept
+  {
+    if (distance < 0) {
+      while (distance++) {
+        ++(*this);
+      }
+    } else {
+      while (distance--) {
+        --(*this);
+      }
+    }
+    return *this;
+  }
+
+  /**
+    Returns true if the iterators are equal.
+  */
+  inline bool operator==(const DoubleLinkedNodeIterator& eq) const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_COMPATIBLE(*this, eq);
+    return node == eq.node;
+  }
+
+  /**
+    Returns true if the iterators aren't equal.
+  */
+  inline bool operator!=(const DoubleLinkedNodeIterator& eq) const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_COMPATIBLE(*this, eq);
+    return node != eq.node;
+  }
+
+  /**
+    Access the element.
+  */
+  inline Reference operator*() const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    return *(node->getValue());
+  }
+
+  /**
+    Access the element.
+  */
+  inline Pointer operator->() const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    return node->getValue();
+  }
+
+  /**
+    Returns the pointer value of the iterator.
+  */
+  inline Pointer getValue() const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    return node->getValue();
+  }
+};
+
+template<class TYPE>
+class DoubleLinkedNodeReadIterator {
+public:
+
+  typedef TYPE Value;
+  typedef TYPE* Pointer;
+  typedef TYPE& Reference;
+  typedef MemoryDiff Distance;
+  typedef BidirectionalIterator Category;
+protected:
+
+  const DoubleLinkedNode<TYPE>* node = nullptr;
+  _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_ADD_CONTEXT();
+public:
+
+  /**
+    Initializes iterator.
+
+    @param value The initial value of the iterator.
+  */
+  explicit inline DoubleLinkedNodeReadIterator(const DoubleLinkedNode<TYPE>* _node) noexcept
+    : node(_node)
+  {
+  }
+
+  /**
+    Initializes iterator from other iterator.
+  */
+  inline DoubleLinkedNodeReadIterator(const DoubleLinkedNodeReadIterator& copy) noexcept
+    : node(copy.node)
+  {
+  }
+
+  /**
+    Initializes iterator from other iterator.
+  */
+  inline DoubleLinkedNodeReadIterator& operator=(const DoubleLinkedNodeReadIterator& copy) noexcept
+  {
+    node = copy.node;
+    return *this;
+  }
+
+  /**
+    Prefix increment.
+  */
+  inline DoubleLinkedNodeReadIterator& operator++() noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    ASSERT(node);
+    node = node->getNext();
+    return *this;
+  }
+
+  /**
+    Postfix increment.
+  */
+  inline DoubleLinkedNodeReadIterator operator++(int) noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    DoubleLinkedNodeReadIterator result(*this);
+    ASSERT(node);
+    node = node->getNext();
+    return result;
+  }
+
+  /**
+    Prefix decrement.
+  */
+  inline DoubleLinkedNodeReadIterator& operator--() noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    ASSERT(node);
+    node = node->getPrevious();
+    return *this;
+  }
+
+  /**
+    Postfix decrement.
+  */
+  inline DoubleLinkedNodeReadIterator operator--(int) noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    DoubleLinkedNodeReadIterator result(*this);
+    ASSERT(node);
+    node = node->getPrevious();
+    return result;
+  }
+
+  /**
+    Moves the specified distance forward.
+  */
+  DoubleLinkedNodeReadIterator& operator+=(Distance distance) noexcept
+  {
+    if (distance < 0) {
+      while (distance++) {
+        --(*this);
+      }
+    } else {
+      while (distance--) {
+        ++(*this);
+      }
+    }
+    return *this;
+  }
+
+  /**
+    Moves the specified distance backwards.
+  */
+  DoubleLinkedNodeReadIterator& operator-=(Distance distance) noexcept
+  {
+    if (distance < 0) {
+      while (distance++) {
+        ++(*this);
+      }
+    } else {
+      while (distance--) {
+        --(*this);
+      }
+    }
+    return *this;
+  }
+
+  /**
+    Returns true if the iterators are equal.
+  */
+  inline bool operator==(const DoubleLinkedNodeReadIterator& eq) const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_COMPATIBLE(*this, eq);
+    return node == eq.node;
+  }
+
+  /**
+    Returns true if the iterators aren't equal.
+  */
+  inline bool operator!=(const DoubleLinkedNodeReadIterator& eq) const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_COMPATIBLE(*this, eq);
+    return node != eq.node;
+  }
+
+  /**
+    Access the element.
+  */
+  inline Reference operator*() const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    return *node;
+  }
+
+  /**
+    Access the element.
+  */
+  inline Pointer operator->() const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    return node->getValue();
+  }
+
+  /**
+    Returns the pointer value of the iterator.
+  */
+  inline Pointer getValue() const noexcept
+  {
+    _COM_AZURE_DEV__BASE__PROTECT_ITERATORS_UNMODIFIED();
+    return node->getValue();
+  }
 };
 
 _COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
+
+_COM_AZURE_DEV__BASE__STD_ITERATOR_TRAITS(base::DoubleLinkedNodeIterator);
+_COM_AZURE_DEV__BASE__STD_ITERATOR_TRAITS(base::DoubleLinkedNodeReadIterator);

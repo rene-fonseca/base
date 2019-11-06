038d20195790ec1d4d2bcd45081bb0fcb946e54e
René Fonseca <rene.m.fonseca@gmail.com>
2019-11-06 16:00:31 +0100

    Added modules example.

---
+++ testsuite/modules.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+    The Base Framework (Test Suite)
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/Application.h>
+#include <base/string/FormatOutputStream.h>
+#include <base/Module.h>
+
+using namespace com::azure::dev::base;
+
+class ModulesApplication : public Application {
+private:
+
+  static const unsigned int MAJOR_VERSION = 1;
+  static const unsigned int MINOR_VERSION = 0;
+public:
+
+  ModulesApplication(
+    int numberOfArguments,
+    const char* arguments[],
+    const char* environment[]) throw()
+    : Application("modules", numberOfArguments, arguments, environment)
+  {
+  }
+
+  void main()
+  {
+    auto& manager = ModuleManager::getManager();
+    if (!manager.traverseModules()) {
+      setExitCode(1);
+      return;
+    }
+
+    // TAG: dump
+    // TAG: connect to online service to check if recalled
+    // TAG: dump graph
+  }
+};
+
+APPLICATION_STUB(ModulesApplication);

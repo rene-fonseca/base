2a92cbcc30f2aa6fd6aa35a4c98ac0ba7c4a3c2c
René Fonseca <rene.m.fonseca@gmail.com>
2019-11-11 12:35:53 +0100

    Enabled long double formatting for GNULINUX.

--- base/string/FormatOutputStream.cpp
+++ base/string/FormatOutputStream.cpp
@@ -1671,7 +1671,7 @@ FormatOutputStream& FormatOutputStream::operator<<(double _value) throw(IOExcept
 
 FormatOutputStream& FormatOutputStream::operator<<(long double _value) throw(IOException)
 {
-#if (_COM_AZURE_DEV__BASE__OS == _COM_AZURE_DEV__BASE__GNULINUX) // TAG: need to fix this
+#if 0 && (_COM_AZURE_DEV__BASE__OS == _COM_AZURE_DEV__BASE__GNULINUX)
   return operator<<(static_cast<double>(_value));
 #endif
 

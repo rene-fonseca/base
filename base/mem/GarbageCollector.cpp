c073031ae7acdc214924f2734df510db45378fb6
René Fonseca <rene.m.fonseca@gmail.com>
2019-11-16 14:31:16 +0100

    Added GarbageCollector.

---
+++ base/mem/GarbageCollector.cpp
@@ -0,0 +1,153 @@
+/***************************************************************************
+   The Base Framework
+   A framework for developing platform independent applications
+
+   See COPYRIGHT.txt for details.
+
+   This framework is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+   For the licensing terms refer to the file 'LICENSE'.
+***************************************************************************/
+
+#include <base/mem/GarbageCollector.h>
+#include <base/concurrency/Thread.h>
+#include <base/concurrency/MutualExclusion.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+namespace {
+
+  class GarbageCollectorImpl : public Thread {
+  public:
+  
+    bool stopped = true;
+    Thread thread;
+    MutualExclusion lock;
+    Event signal;
+    Reference<ReferenceCountedObject> references[4096];
+    MemorySize count = 0;
+
+    inline void release(const Reference<ReferenceCountedObject>& reference)
+    {
+      if (reference && !stopped) {
+        MutualExclusion::Sync _sync(lock);
+        if (count == getArraySize(references)) {
+          // TAG: need to wait
+        }
+        references[count++] = reference;
+        signal.signal();
+      }
+    }
+
+    inline void release(Reference<ReferenceCountedObject>& reference)
+    {
+      // TAG: atomic allocate write position - swap atomic
+      if (reference && !stopped) {
+        MutualExclusion::Sync _sync(lock);
+        if (count == getArraySize(references)) {
+          // TAG: need to wait
+        }
+        references[count++] = reference;
+        reference = nullptr;
+        signal.signal();
+      }
+    }
+    
+    void onException(std::exception_ptr e)
+    {
+      // TAG: send to Application - get type of object
+      ferr << "Error: Garbage collector exception." << ENDL;
+    }
+
+    void doRelease(MemorySize i)
+    {
+      auto& r = references[i];
+      try {
+        r = nullptr; // release
+      } catch (...) {
+        onException(std::current_exception());
+      }
+      if ((i + 1) < count) {
+        r = std::move(references[count - 1]);
+      }
+      --count;
+
+      static MemorySize total = 0;
+      ++total;
+    }
+
+    void releaseAll()
+    {
+      for (MemorySize i = count; i > 0; --i) {
+        doRelease(i - 1);
+      }
+    }
+    
+    void run()
+    {
+      while (!stopped && signal.wait(10)) {
+        signal.reset();
+        MutualExclusion::Sync _sync(lock);
+        for (MemorySize i = 0; i < count; ++i) {
+          auto& r = references[i];
+          if (!r.isMultiReferenced()) {
+            doRelease(i);
+          }
+        }
+      }
+      
+      releaseAll();
+      BASSERT(count == 0);
+    }
+  };
+
+  GarbageCollectorImpl collector;
+}
+
+void GarbageCollector::stop()
+{
+  collector.stopped = true;
+}
+
+bool GarbageCollector::isRunning()
+{
+  return collector.isAlive();
+}
+
+bool GarbageCollector::start()
+{
+  if (collector.isAlive()) {
+    return false;
+  }
+  collector.thread.start();
+  return true;
+}
+
+void GarbageCollector::release(const Reference<ReferenceCountedObject>& reference)
+{
+  if (reference) {
+    collector.release(reference);
+  }
+}
+
+void GarbageCollector::release(Reference<ReferenceCountedObject>& reference)
+{
+  if (reference) {
+    auto r = reference;
+    reference = nullptr;
+    collector.release(r);
+  }
+}
+
+void garbageCollect(Reference<ReferenceCountedObject>& reference)
+{
+  if (reference) {
+    auto r = reference;
+    reference = nullptr;
+    collector.release(r);
+  }
+}
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
---
+++ base/mem/GarbageCollector.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#pragma once
+
+#include <base/mem/Reference.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+/**
+  Garbage collector.
+ 
+  @ingroup memory
+*/
+
+class _COM_AZURE_DEV__BASE__API GarbageCollector {
+public:
+
+  /** Returns true if the garbage collector is running. */
+  static bool isRunning();
+
+  /** Stops the garbage collector. */
+  static void stop();
+
+  /** Starts the garbage collector. */
+  static bool start();
+
+  /** Tells garbage collector tor release the given object. The object cannot be destructed until all references are removed. */
+  static void release(const Reference<ReferenceCountedObject>& reference);
+
+  /** Tells garbage collector tor release the given object. The reference will be set to nullptr. */
+  static void release(Reference<ReferenceCountedObject>& reference);
+};
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

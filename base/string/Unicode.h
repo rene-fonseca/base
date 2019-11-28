d612d8269522ee15d7aad9a8ac6b69efbb0935e5
René Fonseca <rene.m.fonseca@gmail.com>
2019-11-28 20:06:22 +0100

    Updated Unicode.

--- base/string/Unicode.h
+++ base/string/Unicode.h
@@ -24,6 +24,8 @@ _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
 class _COM_AZURE_DEV__BASE__API Unicode {
 public:
 
+  // TAG: add BOM detection
+  
   /** Returns true if the code is a valid UCS4 code. */
   static inline bool isUCS4(unsigned int value) noexcept
   {
@@ -51,7 +53,7 @@ public:
 
     @return Bytes read. If negative this is the error code.
   */
-  static inline int readUCS4(const uint8* src, const uint8* end, ucs4& ch) noexcept
+  static int readUCS4(const uint8* src, const uint8* end, ucs4& ch) noexcept
   {
     if (src == end) {
       return UTF8_ERROR_EMPTY;
@@ -72,7 +74,7 @@ public:
       if ((temp & 0xc0) != 0x80) {
         return UTF8_ERROR_BAD_ENCODING;
       }
-      code = (code << 6) | temp;
+      code = (code << 6) | (temp & 0x3f);
       ch = code;
       return 2;
     }
@@ -87,7 +89,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 3;
@@ -103,7 +105,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 4;
@@ -119,7 +121,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 5;
@@ -135,7 +137,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 6;
@@ -144,7 +146,7 @@ public:
   }
 
   /** Read UCS4 from null terminated sequence. */
-  static inline int readUCS4(const uint8* src, ucs4& ch) noexcept
+  static int readUCS4(const uint8* src, ucs4& ch) noexcept
   {
     if (!src || !*src) {
       return UTF8_ERROR_EMPTY;
@@ -180,7 +182,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 3;
@@ -196,7 +198,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 4;
@@ -212,7 +214,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 5;
@@ -228,7 +230,7 @@ public:
         if ((temp & 0xc0) != 0x80) {
           return UTF8_ERROR_BAD_ENCODING;
         }
-        code = (code << 6) | temp;
+        code = (code << 6) | (temp & 0x3f);
       }
       ch = code;
       return 6;
@@ -242,7 +244,51 @@ public:
   /** Validates if the the given null-terminated string is using valid UTF-8 encoding. Returns the number of characters if valid. Otherwise returns negative status. */
   static MemoryDiff getUTF8StringLength(const uint8* src) noexcept;
 
-  // TAG: add method for converting ucs4 to bytes
+  /** Returns number of bytes required for UTF-8 encoding of the given ucs4 character. Returns 0 is invalid ucs4 character. */
+  static inline MemorySize getUTF8Bytes(ucs4 ch) noexcept
+  {
+    if (ch < ~0x7f) { // 7 bit
+      return 1;
+    }
+    if (ch < ~0x7ff) { // 11 bit
+      return 2;
+    }
+    if (ch <= 0xffff) { // 16 bit
+      return 3;
+    }
+    if (ch <= 0x10ffff) { // 21 bit
+      return 4;
+    }
+    return 0;
+  }
+
+  /** Converts UCS4 to UTF-8. Returns 0 is invalid ucs4 character. Buffer must have room for minimum 4 bytes. */
+  static inline MemorySize writeUTF8(char* dest, ucs4 ch) noexcept
+  {
+    if (ch < ~0x7f) { // 7 bit
+      *dest++ = static_cast<uint8>(ch);
+      return 1;
+    }
+    if (ch < ~0x7ff) { // 11 bit
+      *dest++ = static_cast<uint8>(((ch >> 6) & ((1 << 5) - 1)) | 0xc0);
+      *dest++ = static_cast<uint8>(((ch >> 0) & ((1 << 6) - 1)) | 0x80);
+      return 2;
+    }
+    if (ch <= 0xffff) { // 16 bit
+      *dest++ = static_cast<uint8>(((ch >> 12) & ((1 << 4) - 1)) | 0xc0);
+      *dest++ = static_cast<uint8>(((ch >> 6) & ((1 << 6) - 1)) | 0x80);
+      *dest++ = static_cast<uint8>(((ch >> 0) & ((1 << 6) - 1)) | 0x80);
+      return 3;
+    }
+    if (ch <= 0x10ffff) { // 21 bit
+      *dest++ = static_cast<uint8>(((ch >> 18) & ((1 << 3) - 1)) | 0xc0);
+      *dest++ = static_cast<uint8>(((ch >> 12) & ((1 << 6) - 1)) | 0x80);
+      *dest++ = static_cast<uint8>(((ch >> 6) & ((1 << 6) - 1)) | 0x80);
+      *dest++ = static_cast<uint8>(((ch >> 0) & ((1 << 6) - 1)) | 0x80);
+      return 4;
+    }
+    return 0;
+  }
 };
 
 _COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

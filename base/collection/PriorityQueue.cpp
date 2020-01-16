040c5ac5976dee30edcf4d6e89486305ee68ccf9
René Fonseca <rene.m.fonseca@gmail.com>
2020-01-16 21:28:39 +0100

    Updated PriorityQueue and added test.

--- base/collection/PriorityQueue.cpp
+++ base/collection/PriorityQueue.cpp
@@ -12,11 +12,46 @@
  ***************************************************************************/
 
 #include <base/collection/PriorityQueue.h>
+#include <base/UnitTest.h>
 
 _COM_AZURE_DEV__BASE__DUMMY_SYMBOL
 
 _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
 
 _COM_AZURE_DEV__BASE__INSTANTIATE_CONTAINER_COMMON_TYPE_LIMITED(PriorityQueue)
+
+#if defined(_COM_AZURE_DEV__BASE__TESTS)
+
+class TEST_CLASS(PriorityQueue) : public UnitTest {
+public:
+
+  TEST_PRIORITY(10);
+  TEST_PROJECT("base/collection");
+
+  void run() override
+  {
+    PriorityQueue<String> q1{};
+    PriorityQueue<String> q2{{10, "1"}};
+    PriorityQueue<String> q3{{5, "2"}, {10, "1"}, {7, "3"}};
+    TEST_ASSERT(q3.getSize() == 3);
+    TEST_ASSERT(q3.pop() == "1");
+    TEST_ASSERT(q3.getSize() == 2);
+    TEST_ASSERT(q3.pop() == "3");
+    TEST_ASSERT(q3.getSize() == 1);
+
+#if 0
+    for (auto value : q3) {
+      fout << value << ENDL;
+    }
+#endif
+    
+    q3.removeAll();
+    TEST_ASSERT(q3.isEmpty());
+  }
+};
+
+TEST_REGISTER(PriorityQueue);
+
+#endif
 
 _COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

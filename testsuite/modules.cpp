a8f8ff453d97f8e2f962c0c00797233eb65a7940
René Fonseca <rene.m.fonseca@gmail.com>
2020-02-04 16:41:04 +0100

    Updated modules app.

--- testsuite/modules.cpp
+++ testsuite/modules.cpp
@@ -13,6 +13,9 @@
 
 #include <base/Application.h>
 #include <base/string/FormatOutputStream.h>
+#include <base/string/Format.h>
+#include <base/io/FileDescriptor.h>
+#include <base/net/HTTPSRequest.h>
 #include <base/Module.h>
 
 using namespace com::azure::dev::base;
@@ -22,24 +25,139 @@ private:
 
   static const unsigned int MAJOR_VERSION = 1;
   static const unsigned int MINOR_VERSION = 0;
+
+  bool colorize = false;
+  String format = "text";
+  String url;
+  String token;
 public:
 
   ModulesApplication()
     : Application("modules")
   {
+    colorize = FileDescriptor::getStandardOutput().isANSITerminal();
+  }
+
+  bool parseArguments()
+  {
+    Array<String> arguments = getArguments();
+    Array<String>::ReadEnumerator enu = arguments.getReadEnumerator();
+    while (enu.hasNext()) {
+      String argument = *enu.next();
+      // TAG: need for split only first argument.split("=");
+      if (argument.startsWith("--")) {
+        // Pair<String, String> option = argument.split("=");
+      }
+      
+      if (argument == "--color") { // TAG: add support for =always,auto,never
+        colorize = true;
+      } else if (argument == "--nocolor") {
+        colorize = false;
+      } else if (argument == "--format") { // TAG: switch for = syntax?
+        if (!enu.hasNext()) {
+          Application::error("Expected format [text,json,xml,csv].", EXIT_CODE_ERROR);
+          return false;
+        }
+        format = *enu.next();
+      } else if (argument == "--token") {
+        if (!enu.hasNext()) {
+          Application::error("Expected token.", EXIT_CODE_ERROR);
+          return false;
+        }
+        token = *enu.next();
+      } else if (argument == "--submit") {
+        if (!enu.hasNext()) {
+          Application::error("Expected URL.", EXIT_CODE_ERROR);
+          return false;
+        }
+        url = *enu.next();
+      } else {
+        Application::error("Invalid argument.", EXIT_CODE_ERROR);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /** Submits JSON to given url. */
+  bool submit(const String& url, const String& payload)
+  {
+    if (!HTTPSRequest::isSupported()) {
+      Application::error("Runtime does not supported HTTPS.");
+      return false;
+    }
+    String response;
+    HTTPSRequest request;
+    try {
+      if (request.open(HTTPSRequest::METHOD_GET, url)) {
+        request.send(payload);
+        response = request.getResponse();
+        request.close();
+      }
+      // TAG: detect approved/rejected/other
+      if (request.getStatus() != 400) {
+      }
+      fout << "DEBUG: status=" << request.getStatus() << ENDL;
+    } catch (...) {
+      Application::error(Format::subst("Failed to submit request to '%1'.", url));
+    }
   }
 
   void main()
   {
-    auto& manager = ModuleManager::getManager();
-    if (!manager.traverseModules()) {
-      setExitCode(1);
+    if (!parseArguments()) {
       return;
     }
 
-    // TAG: dump
-    // TAG: connect to online service to check if recalled
-    // TAG: dump graph
+    auto& manager = ModuleManager::getManager();
+
+    Reference<ObjectModel::Value> om;
+
+    if (format == "text") {
+      if (!manager.traverseModules(fout, "*", true)) {
+        setExitCode(1);
+        return;
+      }
+    } else if ((format == "json") || (format == "xml")) {
+      auto report = manager.getModules();
+      if (!report) {
+        setExitCode(1);
+        return;
+      }
+
+      if (format == "json") {
+        fout << report << ENDL;
+      } else {
+        DOMImplementation impl;
+        String xml = impl.saveDocumentToMemory(ObjectModel::getXML(report, "modules"), true);
+        fout << xml << ENDL;
+      }
+      om = report;
+    } else if (format == "csv") {
+      auto report = manager.getModulesCSV();
+      if (!report) {
+        setExitCode(1);
+        return;
+      }
+      fout << report;
+    } else {
+      // ("%1", "value") // TAG: can we support substitution easily
+      Application::error(Format::subst("Unsupported format '%1'.", format));
+      return;
+    }
+
+    if (url) {
+      if (!om) {
+        om = manager.getModules();
+      }
+      if (!om) {
+        setExitCode(1);
+        return;
+      }
+      if (!submit(url, StringOutputStream() << om)) {
+        setExitCode(1);
+      }
+    }
   }
 };
 

619d88e24194f3c404579a11d385f9bd350f5afd
René Fonseca <rene.m.fonseca@gmail.com>
2020-02-22 12:58:39 +0100

    Refactored media blob.

--- base/media/Media.cpp
+++ base/media/Media.cpp
@@ -17,6 +17,66 @@
 
 _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
 
+const String& MediaBlob::getPlainType()
+{
+  static String result(MESSAGE("text/plain"));
+  return result;
+}
+
+const String& MediaBlob::getHTMLType()
+{
+  static String result(MESSAGE("text/html"));
+  return result;
+}
+
+MediaBlob::MediaBlob()
+{
+}
+
+MediaBlob::MediaBlob(const String& key, const String& bytes, bool encode)
+{
+  add(key, bytes, encode);
+}
+
+MediaBlob::MediaBlob(String&& key, String&& bytes, bool encode)
+{
+  add(moveObject(key), moveObject(bytes), encode);
+}
+
+MediaBlob& MediaBlob::add(const String& _key, const String& _bytes, bool encode)
+{
+  if (size >= getArraySize(fields)) {
+    _throw OutOfRange("Too many blobs.");
+  }
+  // we dont care if the same key is added more than once
+  fields[size].key = _key;
+  fields[size].bytes = encode ? base64encode(_bytes) : _bytes;
+  ++size;
+  return *this;
+}
+
+MediaBlob& MediaBlob::add(String&& _key, String&& _bytes, bool encode)
+{
+  if (size >= getArraySize(fields)) {
+    _throw OutOfRange("Too many blobs.");
+  }
+  // we dont care if the same key is added more than once
+  fields[size].key = moveObject(_key);
+  fields[size].bytes = encode ? base64encode(_bytes) : moveObject(_bytes);
+  ++size;
+  return *this;
+}
+
+bool MediaBlob::hasKey(const String& key) const noexcept
+{
+  for (const auto& nv : *this) {
+    if (nv.key == key) {
+      return true;
+    }
+  }
+  return false;
+}
+
 String Audio::getAudioControl(const String& bytes, const String& mimetype)
 {
   StringOutputStream stream;
--- base/media/Media.h
+++ base/media/Media.h
@@ -17,42 +17,86 @@
 
 _COM_AZURE_DEV__BASE__ENTER_NAMESPACE
 
-#if 1
-/** Jupyter media blob. */
-class _COM_AZURE_DEV__BASE__API JupyterMediaBlob {
+/** Media blob. Lower level object with minimum dependencies. */
+class _COM_AZURE_DEV__BASE__API MediaBlob {
 public:
   
-  class Blob {
+  /** Name/value pair. */
+  class KeyValue {
   public:
     
-    String mimetype;
+    // R<ReferenceCountedAllocator<char> > key;
+    // R<ReferenceCountedAllocator<char> > bytes;
+    
+    String key; // normally mimetype
     String bytes;
   };
+private:
   
-  // Blob fields[2];
-  // TAG: we could add support for up to 2 types
+  KeyValue fields[2];
+  unsigned int size = 0;
+public:
+  
+  /** Returns 'text/plain'. */
+  static const String& getPlainType();
 
-  String mimetype;
-  String bytes;
+  /** Returns 'text/html'. */
+  static const String& getHTMLType();
+
+  /** Initializes media blob. */
+  MediaBlob();
+
+  /** Initializes media blob. */
+  MediaBlob(const String& key, const String& bytes, bool encode = false);
   
-  inline JupyterMediaBlob()
+  /** Initializes media blob. */
+  MediaBlob(String&& key, String&& bytes, bool encode = false);
+  
+  /** Adds key. */
+  MediaBlob& add(const String& key, const String& bytes, bool encode = false);
+
+  /** Adds key. */
+  MediaBlob& add(String&& key, String&& bytes, bool encode = false);
+
+  /** Returns true if the key exists. */
+  bool hasKey(const String& key) const noexcept;
+
+  /** Returns the size. */
+  inline MemorySize getSize() const noexcept
   {
+    return size;
+  }
+  
+  /** Returns the begin iterator. */
+  inline const KeyValue* begin() const noexcept
+  {
+    return fields;
   }
 
-  inline JupyterMediaBlob(const String& _mimetype, const String& _bytes, bool encode = false)
-    : mimetype(_mimetype), bytes(encode ? base64encode(_bytes) : _bytes)
+  /** Returns the end iterator. */
+  inline const KeyValue* end() const noexcept
   {
+    return fields + size;
   }
-};
+  
+#if (_COM_AZURE_DEV__BASE__COMPILER == _COM_AZURE_DEV__BASE__COMPILER_CLING)
+  /** Returns bundle. */
+  inline ClingMimeBundle getBundle() const
+  {
+    ClingMimeBundleHandle handle;
+    for (const auto& nv : fields) {
+      handle.setMimeType(nv.key, nv.bytes);
+    }
+    return moveObject(handle.getBundle());
+  }
 #endif
+};
 
 #if (_COM_AZURE_DEV__BASE__COMPILER == _COM_AZURE_DEV__BASE__COMPILER_CLING)
-
-inline ClingMimeBundle _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_ID(const JupyterMediaBlob& blob) \
+inline ClingMimeBundle _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_ID(const MediaBlob& blob) \
 { \
-  return ClingMimeBundleHandle().setMimeType(blob.mimetype, blob.bytes); \
+  return blob.getBundle(); \
 }
-
 #endif
 
 
@@ -83,9 +127,10 @@ public:
   }
 
   // cling: would like cling to pick this up for display
-  inline operator JupyterMediaBlob() const
+  /** Returns media blob. */
+  inline MediaBlob getMediaBlob() const
   {
-    return JupyterMediaBlob(getMimeType(), getBytes(), true);
+    return MediaBlob(getMimeType(), getBytes(), true);
   }
 };
 
@@ -94,11 +139,6 @@ class _COM_AZURE_DEV__BASE__API Image : public Media {
 public:
   
   using Media::Media;
-  
-  inline operator JupyterMediaBlob() const
-  {
-    return JupyterMediaBlob(getMimeType(), getBytes(), true);
-  }
 };
 
 /** Audio. */
@@ -109,6 +149,15 @@ public:
   
   /** Returns HTML audio control. */
   static String getAudioControl(const String& bytes, const String& mimetype);
+
+  /** Returns media blob. */
+  inline MediaBlob getMediaBlob() const
+  {
+    return MediaBlob(
+      MediaBlob::getHTMLType(),
+      Audio::getAudioControl(getBytes(), getMimeType())
+    );
+  }
 };
 
 /** Video. */
@@ -142,44 +191,28 @@ public:
   {
     return text;
   }
+  
+  /** Returns media blob. */
+  inline MediaBlob getMediaBlob() const
+  {
+    return MediaBlob(getMimeType(), getText());
+  }
 };
 
-/** Display data in Jupyter. */
-inline DisplayObject display(const String& text, const String& mimetype = "text/html")
-{
-  return DisplayObject(mimetype, text);
-}
-
 #if (_COM_AZURE_DEV__BASE__COMPILER == _COM_AZURE_DEV__BASE__COMPILER_CLING)
-
 #  define _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(TYPE) \
 inline ClingMimeBundle _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_ID(const TYPE& media) \
 { \
-  return cling_getAnyMimeBundle(media.getMimeType(), Base64::encode(media.getBytes())); \
+  return moveObject(media.getMediaBlob().getBundle()); \
 }
-
-#  define _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_DISPLAY(TYPE) \
-inline ClingMimeBundle _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_ID(const TYPE& object) \
-{ \
-  return cling_getAnyMimeBundle(object.getMimeType(), object.getText()); \
-}
-
 #else
 #  define _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(TYPE)
-#  define _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_DISPLAY(TYPE)
-#endif
-
-#if (_COM_AZURE_DEV__BASE__COMPILER == _COM_AZURE_DEV__BASE__COMPILER_CLING)
-
-inline ClingMimeBundle _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_ID(const Audio& audio)
-{
-  return ClingMimeBundleHandle().setHTMLText(Audio::getAudioControl(audio.getBytes(), audio.getMimeType()));
-}
 #endif
 
 _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(Media)
 _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(Image)
+_COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(Audio)
 _COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(Video)
-_COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_DISPLAY(DisplayObject)
+_COM_AZURE_DEV__BASE__CLING_GET_MIME_BUNDLE_MEDIA(DisplayObject)
 
 _COM_AZURE_DEV__BASE__LEAVE_NAMESPACE

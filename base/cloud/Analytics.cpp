e7ca4c6fde556935160c5e3cb5b75f227d840bb1
René Fonseca <rene.m.fonseca@gmail.com>
2020-03-12 22:55:53 +0100

    Added initial analytics class.

---
+++ base/cloud/Analytics.cpp
@@ -0,0 +1,137 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#include <base/cloud/Analytics.h>
+#include <base/concurrency/Thread.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+namespace {
+
+  class AnalyticsWorker : public Thread {
+  public:
+  
+    void run()
+    {
+      Analytics::getAnalytics().worker();
+    }
+  };
+
+  AnalyticsWorker thread;
+}
+
+Analytics::Event Analytics::makeResource(const String& resource, const String& title)
+{
+  Event e;
+  e.type = "resource";
+  e.category = resource;
+  e.label = title;
+  return e;
+}
+
+Analytics::Event Analytics::makeEvent(const String& category, const String& action)
+{
+  Event e;
+  e.type = "event";
+  e.category = category;
+  e.action = action;
+  return e;
+}
+
+Analytics::Analytics()
+{
+}
+
+void Analytics::worker()
+{
+  while (true) {
+    event.wait();
+    MutualExclusion::Sync _sync(mutex);
+    if (!running) {
+      break;
+    }
+    const auto e = queue.getFirst();
+    queue.removeFirst();
+    for (auto consumer : consumers) {
+      if (consumer->isEnabled()) {
+        consumer->send(e);
+      }
+    }
+  }
+}
+
+Analytics& Analytics::getAnalytics()
+{
+  static Analytics analytics;
+  return analytics;
+}
+
+void Analytics::registerConsumer(Consumer* consumer)
+{
+  if (!consumer) {
+    _throw NullPointer();
+  }
+  MutualExclusion::Sync _sync(mutex);
+  consumers.append(consumer);
+}
+
+bool Analytics::isRunning() const noexcept
+{
+  return running;
+}
+
+void Analytics::start()
+{
+  if (!running) {
+    running = true;
+    thread.start();
+  }
+}
+
+void Analytics::stop()
+{
+  if (running) {
+    running = false;
+    event.signal();
+    thread.join();
+    // thread = Worker();
+  }
+}
+
+void Analytics::send(const Event& e)
+{
+  MutualExclusion::Sync _sync(mutex);
+  queue.append(e);
+  event.signal();
+}
+
+Analytics::~Analytics()
+{
+  for (auto consumer : consumers) {
+    delete consumer;
+  }
+  consumers.removeAll();
+}
+
+Analytics::Task::Task(const Event& _e)
+  : e(_e)
+{
+}
+
+Analytics::Task::~Task()
+{
+  e.endTime = Date::getNow().getValue();
+  getAnalytics().send(e);
+}
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
---
+++ base/cloud/Analytics.h
@@ -0,0 +1,175 @@
+/***************************************************************************
+    The Base Framework
+    A framework for developing platform independent applications
+
+    See COPYRIGHT.txt for details.
+
+    This framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    For the licensing terms refer to the file 'LICENSE'.
+ ***************************************************************************/
+
+#pragma once
+
+#include <base/string/String.h>
+#include <base/collection/Array.h>
+#include <base/collection/List.h>
+#include <base/collection/Pair.h>
+#include <base/Date.h>
+#include <base/concurrency/Event.h>
+#include <base/concurrency/MutualExclusion.h>
+
+_COM_AZURE_DEV__BASE__ENTER_NAMESPACE
+
+/**
+  Analytics.
+*/
+
+class _COM_AZURE_DEV__BASE__API Analytics {
+public:
+  
+  /** Event. */
+  class Event {
+  public:
+    
+    uint64 startTime = Date::getNow().getValue();
+    uint64 endTime = 0;
+    
+    String type; // 'event' or 'resource'
+
+    // event
+    String category; // resource id
+    String action;
+    String label; // resource title
+    String value;
+
+    inline Event& setCategory(const String& category) noexcept
+    {
+      this->category = category;
+      return *this;
+    }
+
+    inline Event& setAction(const String& action) noexcept
+    {
+      this->action = action;
+      return *this;
+    }
+
+    inline Event& setLabel(const String& label) noexcept
+    {
+      this->label = label;
+      return *this;
+    }
+
+    inline Event& setValue(const String& value) noexcept
+    {
+      this->value = value;
+      return *this;
+    }
+  };
+
+  /** Analytics consumer. */
+  class Consumer : public DynamicObject {
+  public:
+
+    /** Opens consumer. */
+    virtual void open(const Array<StringPair>& params = {}) = 0;
+
+    /** Returns true if enabled. */
+    virtual bool isEnabled() const = 0;
+
+    /** Sets enabled. */
+    virtual void setEnabled(bool enabled) = 0;
+
+    /** Sends event to consumer. */
+    virtual void send(const Event& e) = 0;
+
+    /** Closes consumer. */
+    virtual void close() = 0;
+  };
+private:
+  
+  Array<Consumer*> consumers;
+  List<Event> queue;
+  MutualExclusion mutex;
+  base::Event event;
+  bool running = false;
+  String userId;
+  String clientId;
+
+  /** Initializes analytics instance. */
+  Analytics();
+public:
+  
+  void worker();
+public:
+
+  /** Returns resource event. */
+  static Event makeResource(const String& resource, const String& title);
+  
+  /** Returns normal event. */
+  static Event makeEvent(const String& category, const String& action);
+  
+  /** Returns the analytics instance. */
+  static Analytics& getAnalytics();
+
+  /** Returns anonymous user id. */
+  inline const String& getUserId() const noexcept
+  {
+    return userId;
+  }
+  
+  /** Sets anonymous user id. */
+  inline void setUserId(const String& userId) noexcept
+  {
+    this->userId = userId;
+  }
+
+  /** Returns anonymous user id. */
+  inline const String& getClientId() const noexcept
+  {
+    return clientId;
+  }
+
+  /** Sets anonymous client id. */
+  inline void setClientId(const String& clientId) noexcept
+  {
+    this->clientId = clientId;
+  }
+
+  /** Registers analytics consumer. */
+  void registerConsumer(Consumer* consumer);
+  
+  /** Returns true if running. */
+  bool isRunning() const noexcept;
+  
+  /** Startss the analytics. */
+  void start();
+
+  /** Stops the analytics. */
+  void stop();
+
+  /** Send the given event. */
+  void send(const Event& e);
+
+  /** Destroys analytics. */
+  ~Analytics();
+
+  /** Task. */
+  class Task {
+  private:
+    
+    Event e;
+  public:
+    
+    /** Start task. */
+    Task(const Event& e);
+
+    /** End of task. */
+    ~Task();
+  };
+};
+
+_COM_AZURE_DEV__BASE__LEAVE_NAMESPACE
